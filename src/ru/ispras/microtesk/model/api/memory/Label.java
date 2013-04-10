248d4ed354a82a9998c034b006dc119b0c71b41e
Andrei Tatarnikov <andrewt@ispras.ru>
2013-04-10 12:49:15 +0000

    The Label class was added.

---
+++ src/ru/ispras/microtesk/model/api/memory/Label.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2013 ISPRAS
+ * 
+ * Institute for System Programming of Russian Academy of Sciences
+ * 
+ * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * 
+ * All rights reserved.
+ * 
+ * ModelMain.java, Apr 10, 2013 4:34:28 PM Andrei Tatarnikov
+ */
+
+package ru.ispras.microtesk.model.api.memory;
+
+public final class Label
+{
+    private final String name;
+    private final MemoryBase memory;
+    private final int index;
+
+    public Label(String name, MemoryBase memory)
+    {
+        this(name, memory, 0);
+    }
+
+    public Label(String name, MemoryBase memory, int index)
+    {
+        this.name   = name;
+        this.memory = memory;
+        this.index  = index;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Location access()
+    {
+        return memory.access(index);
+    }
+}
