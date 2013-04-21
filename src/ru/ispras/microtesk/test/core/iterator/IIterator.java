07fc490791084b19926f884b336752b2b1a878c5
Alexander Kamkin <kamkin@ispras.ru>
2013-04-21 05:09:44 +0000

    The generic interator interface has been added (simplified old code).

--- src/ru/ispras/microtesk/test/core/iterator/IIterator.java
+++ src/ru/ispras/microtesk/test/core/iterator/IIterator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 ISP RAS (http://www.ispras.ru), UniTESK Lab (http://www.unitesk.com)
+ * Copyright 2008-2013 ISP RAS (http://www.ispras.ru), UniTESK Lab (http://www.unitesk.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,8 +17,33 @@
 package ru.ispras.microtesk.test.core.iterator;
 
 /**
+ * This is a generic iterator interface.
+ * 
  * @author <a href="mailto:kamkin@ispras.ru">Alexander Kamkin</a>
  */
-public interface IIterator
+public interface IIterator<T>
 {
+    /** Initializes the iterator. */
+    public void init();
+
+    /**
+     * Checks whether the iterator is not exhausted (value is available).
+     * 
+     * @return <code>true</code> if the iterator is not exhausted;
+     *         <code>false</code> otherwise.
+     */
+    public boolean hasValue();
+        
+    /**
+     * Returns the current value of the iterator.
+     * 
+     * @return the current value of the iterator.
+     */
+    public T value();
+    
+    /** Makes the iteration. */
+    public void next();
+
+    /** Stops the iteration. */
+    public void stop();
 }
