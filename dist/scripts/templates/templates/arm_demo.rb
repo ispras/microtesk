91ae9781bd5b1cdc5e161632344105e033023513
Andrei Tatarnikov <andrewt@ispras.ru>
2013-05-22 14:22:31 +0000

    Added more test situations.

--- dist/scripts/templates/templates/arm_demo.rb
+++ dist/scripts/templates/templates/arm_demo.rb
@@ -12,17 +12,17 @@ class ArmDemo < DemoPrepost
   def run
 
     add equalcond, setson, reg({:r => 0}), reg({:r => 0}), register1
-
     add equalcond, setsoff, reg(1), reg(2), register0 do random end
 
     debug do
       puts "This is a debug message"
     end
-    
-    add equalcond, setsoff, reg(2), reg(2), register0
+
+    add equalcond, setsoff, reg(2), reg(2), register0 do overflow end
+
     add equalcond, setsoff, reg(3), reg(2), register0
 
-    add equalcond, setsoff, reg(4), reg(2), register0
+    add equalcond, setsoff, reg(4), reg(2), register0 do normal end
     add equalcond, setsoff, reg(5), reg(2), register0
 
     block {
@@ -34,7 +34,6 @@ class ArmDemo < DemoPrepost
     }
 
     add_immediate blank, setsoff, reg(2), reg(3), immediate(4, 5) do random end
-    
     b equalcond, 42 do random end
 
   end
