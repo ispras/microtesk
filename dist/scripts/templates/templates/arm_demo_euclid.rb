c0c4a72b561a1471763b1803cc3264f8db84178f
Andrei Tatarnikov <andrewt@ispras.ru>
2013-05-30 18:49:15 +0000

    Commented out calls to debug functions that are not implemented in the current version.

--- dist/scripts/templates/templates/arm_demo_euclid.rb
+++ dist/scripts/templates/templates/arm_demo_euclid.rb
@@ -11,9 +11,9 @@ class ArmDemo < Template
 
   def run
 
-    exec_debug {
-      puts "Euclidean algorithm - debug output\n"
-    }
+    # exec_debug {
+    #   puts "Euclidean algorithm - debug output\n"
+    # }
 
     (1..5).each do |ind|
 
@@ -21,13 +21,13 @@ class ArmDemo < Template
       j = Random.rand(64)
 
       # This is what we try to set into the registers - the arguments of the Euclidean algorithm (finding MCD)
-      exec_debug {
-        puts "Arguments: " + i.to_s + ", " + j.to_s
-      }
+      # exec_debug {
+      #   puts "Arguments: " + i.to_s + ", " + j.to_s
+      # }
 
-      newline
-      text "Setting up arguments"
-      newline
+      # newline
+      # text "Setting up arguments"
+      # newline
 
       SUB           blank, setsOff, REG(0), REG(0), register0 # Keep in mind - Ruby thinks a method with no parameters starting in caps is a constant
       ADD_IMMEDIATE blank, setsOff, REG(0), REG(0), IMMEDIATE(0, i)
@@ -36,9 +36,9 @@ class ArmDemo < Template
       ADD_IMMEDIATE blank, setsOff, REG(1), REG(1), IMMEDIATE(0, j)
 
       # This is what is set in the registers at the time of execution
-      exec_debug {
-        puts "INPUT: R0: " + get_loc_value("GPR", 0).to_s + ", R1: " + get_loc_value("GPR", 1).to_s
-      }
+      # exec_debug {
+      #   puts "INPUT: R0: " + get_loc_value("GPR", 0).to_s + ", R1: " + get_loc_value("GPR", 1).to_s
+      # }
 
       label ("cycle" + ind.to_s).to_sym
 
@@ -47,22 +47,22 @@ class ArmDemo < Template
       SUB lessThan,    setsOff, REG(1), REG(1), register0
 
       # This is what REG(0) and REG(1) contain during a single iteration of the cycle
-      exec_debug {
-        puts "DEBUG: R0: " + get_loc_value("GPR", 0).to_s + ", R1: " + get_loc_value("GPR", 1).to_s# + ", label code: " + self.send("cycle" + ind.to_s).to_s
-      }
+      # exec_debug {
+      #   puts "DEBUG: R0: " + get_loc_value("GPR", 0).to_s + ", R1: " + get_loc_value("GPR", 1).to_s# + ", label code: " + self.send("cycle" + ind.to_s).to_s
+      # }
 
       B   notEqual, ("cycle" + ind.to_s).to_sym
 
-      newline
-      exec_output {
-        "// Simulator heavily implies the result should be " + self.get_loc_value("GPR", 0).to_s
-      }
-      newline
+      # newline
+      # exec_output {
+      #  "// Simulator heavily implies the result should be " + self.get_loc_value("GPR", 0).to_s
+      # }
+      # newline
 
       # This is the result of the algorithm
-      exec_debug {
-        puts "Result: " + get_loc_value("GPR", 0).to_s
-      }
+      # exec_debug {
+      #   puts "Result: " + get_loc_value("GPR", 0).to_s
+      # }
 
     end
 
