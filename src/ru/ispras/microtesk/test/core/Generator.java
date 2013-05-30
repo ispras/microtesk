7643580758f495b1bbe5b4472fdc2543e28ff02b
Andrei Tatarnikov <andrewt@ispras.ru>
2013-05-30 18:35:38 +0000

    One more file.

---
+++ src/ru/ispras/microtesk/test/core/Generator.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2013 ISPRAS
+ * 
+ * Institute for System Programming of Russian Academy of Sciences
+ * 
+ * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * 
+ * All rights reserved.
+ * 
+ * Generator.java, May 29, 2013, 11:47:00 PM PM Andrei Tatarnikov
+ */
+
+package ru.ispras.microtesk.test.core;
+
+import ru.ispras.microtesk.test.core.iterator.IIterator;
+
+public interface Generator<T> extends IIterator<Sequence<T>>
+{
+
+}
