dbf9f21a2e8dda8f7199fab78427efdfae1f5077
Andrei Tatarnikov <andrewt@ispras.ru>
2013-06-01 19:51:00 +0000

    Scripts under testing/updating.

--- dist/scripts/templates/templates/arm_demo.rb
+++ dist/scripts/templates/templates/arm_demo.rb
@@ -11,18 +11,28 @@ class ArmDemo < DemoPrepost
 
   def run
 
+    print_all_registers
+
+    # add_immediate blank, setsoff, reg(0), reg(0), immediate(0, 1)
+    # add_immediate blank, setsoff, reg(0), reg(0), immediate(4, 1)
+    # add_immediate blank, setsoff, reg(0), reg(0), immediate(8, 1)
+    
+    add equalcond, setsoff, reg(2), reg(2), register2 
+
+    print_all_registers
+
     add equalcond, setson, reg({:r => 0}), reg({:r => 0}), register1
-    add equalcond, setsoff, reg(1), reg(2), register3 do random end
+    add equalcond, setsoff, reg(1), reg(2), register3  do random end
 
     debug do
       puts "This is a debug message"
     end
 
-    add equalcond, setsoff, reg(2), reg(2), register0 do overflow end
+    add equalcond, setsoff, reg(2), reg(2), register0 # do overflow end
 
-    add equalcond, setsoff, reg(3), reg(2), register0 do random end
+    add equalcond, setsoff, reg(3), reg(2), register0 # do random end
 
-    add equalcond, setsoff, reg(4), reg(2), register0 do normal end
+    add equalcond, setsoff, reg(4), reg(2), register0 # do normal end
     add equalcond, setsoff, reg(5), reg(2), register0
 
     block {
@@ -32,9 +42,22 @@ class ArmDemo < DemoPrepost
       add equalcond, setsoff, reg(4), reg(3), register0
       add equalcond, setsoff, reg(5), reg(3), register0
     }
-
+    
     add_immediate blank, setsoff, reg(2), reg(3), immediate(4, 5)
     b equalcond, 42
 
   end
+  
+  def print_all_registers
+
+    debug {
+      a = "DEBUG: GRP values: "
+      (0..15).each do |i|
+         a += sprintf("%032b, ", get_loc_value("GPR", i))
+      end
+      puts a
+    }
+ 
+  end
+
 end
--- dist/scripts/templates/templates/arm_demo_labels.rb
+++ dist/scripts/templates/templates/arm_demo_labels.rb
@@ -32,44 +32,41 @@ class ArmDemo < DemoPrepost
     b notEqual, :valiant
     
     block {
-      
+
       b blank, :compliant
-      
+
       sub blank, setsoff, reg(10), reg(10), register0
-      
+
       label :compliant
-      
+
       sub blank, setsoff, reg(11), reg(0), register0
-      
+
       sub blank, setsoff, reg(13), reg(0), register0
-      
+
       b blank, :valiant
-      
+
       sub blank, setsoff, reg(14), reg(0), register0
-      
+
       label :valiant
-      
+
       sub blank, setsoff, reg(15), reg(0), register0
 
       b blank, :defiant
-      
+
       sub blank, setsoff, reg(12), reg(0), register0
-      
     }
-    
+
     label :defiant
-    
+
     sub blank, setsoff, reg(1), reg(1), register1
     add_immediate blank, setsoff, reg(1), reg(1), immediate(0, 1)
 
     #b notequal, :defiant
-    b blank, :defiant
+    #b blank, :defiant
 
     #add_immediate blank, setsoff, reg(2), reg(2), immediate(0, 2)
     sub blank, setsoff, reg(0), reg(0), register0
 
-
-        
     sub blank, setsoff, reg(1), reg(1), register1
     #add_immediate blank, setsoff, reg(3), reg(3), immediate(0, 3)
 
