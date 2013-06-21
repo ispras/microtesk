5f1193fb1a7b9e4e76704dfb59b601da582a78ff
Andrei Tatarnikov <andrewt@ispras.ru>
2013-06-21 08:01:51 +0000

    Updated scripts that launch generation.

--- generate.bat
+++ generate.bat
@@ -1,2 +1,2 @@
-java -Xmx1024m -jar jars/jruby.jar dist/scripts/parse_templates.rb %*
+java -Xmx1024m -jar jars/jruby.jar dist/scripts/template_processor.rb %*
 
--- generate.sh
+++ generate.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -Xmx1024m -jar ./jars/jruby.jar ./dist/scripts/parse_templates.rb $*
+java -Xmx1024m -jar ./jars/jruby.jar ./dist/scripts/template_processor.rb $*
+
