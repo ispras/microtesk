f5e1586b53348c65ecef405bc10dc76128137e0a
Alexander Kamkin <kamkin@ispras.ru>
2013-06-26 07:08:20 +0000

    Some comments have been added.

--- src/ru/ispras/microtesk/model/api/mmu/buffer/Buffer.java
+++ src/ru/ispras/microtesk/model/api/mmu/buffer/Buffer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2013 ISP RAS (http://www.ispras.ru), UniTESK Lab (http://www.unitesk.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ru.ispras.microtesk.model.api.mmu.buffer;
 
 import java.util.HashMap;
@@ -5,8 +21,22 @@ import java.util.HashMap;
 import ru.ispras.microtesk.model.api.mmu.Policy;
 import ru.ispras.microtesk.model.api.rawdata.RawData;
 
-public class Buffer<T extends Line, P extends Policy>
+/**
+ * This class represents a partially associative buffer.
+ *
+ * @author <a href="mailto:leonsia@ispras.ru">Tatiana Sergeeva</a>
+ */
+public abstract class Buffer<L extends Line, P extends Policy>
 {
+    /// Internal representation of the buffer (Index -> Set).
+    protected HashMap<Integer, Set<L, P>> buffer = new HashMap<Integer, Set<L, P>>();
+
+    /**
+     * Constructs a buffer with the given number of sets and lines.
+     *
+     * @param number_of_sets the number of sets in the buffer.
+     * @param number_of_lines the number of lines in a set.
+     */
 	public Buffer(int number_of_sets, int number_of_lines)
 	{
 		for(int i = 0; i < number_of_sets; i++)
@@ -15,16 +45,28 @@ public class Buffer<T extends Line, P extends Policy>
 		}
 	}
 	
-	public int index(Address address) {
-		return 0;
-	} 
+    /**
+     * Returns the index of the buffer set for the given address.
+     *
+     * @return the index in the buffer set.
+     */
+	public abstract int index(final Address address);
 	
-	public boolean match(Address address)
+    /**
+     * Checks whether there is a buffer hit for the given address.
+     *
+     * @param address the address.
+     * @return true iff there is a buffer hit.
+     */
+	public boolean match(final Address address)
 	{
 		return getSet(index(address)).match(address);
 	}
 	
-	private Set<T, P> getSet(int index) { return buffer.get(index); }
+	private Set<T, P> getSet(int index)
+    {
+        return buffer.get(index);
+    }
 	
 	public RawData read(Address address)
 	{
@@ -35,11 +77,9 @@ public class Buffer<T extends Line, P extends Policy>
 
 	public RawData write (Address address, RawData data) 
 	{
-		Set<T, P> set = getSet(index(address));
-		
+		Set<L, P> set = getSet(index(address));
+
 		return set.write(address,data);
     }
 	
-	protected HashMap<Integer, Set<T, P>> buffer = new HashMap<Integer, Set<T, P>>();
-
 }
