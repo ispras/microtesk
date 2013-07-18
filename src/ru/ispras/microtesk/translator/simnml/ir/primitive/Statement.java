1bcbdf2e31047b79a539b7a647158af4116b7e5a
Andrei Tatarnikov <andrewt@ispras.ru>
2013-07-18 11:00:27 +0000

    IR refactoring: changes in statements is in progress...

--- src/ru/ispras/microtesk/translator/simnml/ir/primitive/Statement.java
+++ src/ru/ispras/microtesk/translator/simnml/ir/primitive/Statement.java
@@ -14,23 +14,156 @@ package ru.ispras.microtesk.translator.simnml.ir.primitive;
 
 import java.util.List;
 import ru.ispras.microtesk.translator.simnml.ir.expression.Expr;
+import ru.ispras.microtesk.translator.simnml.ir.expression.LocationExpr;
 
-public interface Statement
+public abstract class Statement
 {
-    public String getText();
+    public static enum Kind
+    {
+        CALL,
+        ASSIGN,
+        COND,
+        @Deprecated TEXT
+    }
+
+    private final Kind kind;
+
+    public Statement(Kind kind)
+    {
+        this.kind = kind;
+    }
+
+    public final Kind getKind()
+    {
+        return kind;
+    }
+
+    public abstract String getText();
 }
 
-class TextStatement implements Statement
+final class AttributeCallStatement extends Statement
+{
+    private final Attribute        callee;
+    private final String    calleeObjName;
+    private final Primitive calleeObjType;
+
+    public AttributeCallStatement(
+        Attribute callee, String calleeObjName, Primitive calleeObjType)
+    {
+        super(Kind.CALL);
+
+        this.callee        = callee;
+        this.calleeObjName = calleeObjName;
+        this.calleeObjType = calleeObjType;
+    }
+
+    public Attribute getCallee()
+    {
+        return callee; 
+    }
+
+    public String getCalleeObjectName()
+    {
+        return calleeObjName;
+    }
+
+    public Primitive getCalleeObjectType()
+    {
+        return calleeObjType;
+    }
+
+    @Override
+    public String getText()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
+
+final class AssignmentStatement extends Statement
+{
+    private final LocationExpr left;
+    private final Expr right;
+
+    public AssignmentStatement(LocationExpr left, Expr right)
+    {
+        super(Kind.ASSIGN);
+
+        this.left  = left;
+        this.right = right;
+    }
+
+    public LocationExpr getLeft()
+    {
+        return left;
+    }
+    
+    public Expr getRight()
+    {
+        return right;
+    }
+
+    @Override
+    public String getText()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
+
+final class ConditionalStatement extends Statement
+{
+    private final Expr                cond;
+    private final List<Statement>   ifSmts;
+    private final List<Statement> elseSmts;
+
+    public ConditionalStatement(
+        Expr cond, List<Statement> ifSmts, List<Statement> elseSmts)
+    {
+        super(Kind.COND);
+        
+        this.cond     = cond;
+        this.ifSmts   = ifSmts;
+        this.elseSmts = elseSmts;
+    }
+
+    public Expr getCondition()
+    {
+        return cond;
+    }
+
+    public List<Statement> getIfStatements()
+    {
+        return ifSmts;
+    }
+
+    public List<Statement> getElseStatements()
+    {
+        return elseSmts;
+    }
+
+    @Override
+    public String getText()
+    {
+        return null;
+    }
+}
+
+class TextStatement extends Statement
 {
     private final String text;
 
     public TextStatement()
     {
+        super(Kind.TEXT);
+        
         this.text = "//Default code\r\nreturn null";
     }
 
     public TextStatement(String text)
     {
+        super(Kind.TEXT);
+        
         this.text = text;
     }
 
@@ -40,7 +173,7 @@ class TextStatement implements Statement
     }
 }
 
-class IfElseStatement implements Statement
+class IfElseStatement extends Statement
 {
     private static int depth = 0;
 
@@ -50,6 +183,8 @@ class IfElseStatement implements Statement
 
     public IfElseStatement(Expr condition, List<Statement> ifSmts, List<Statement> elseSmts)
     {
+        super(Kind.TEXT);
+        
         this.condition = condition;
         this.ifSmts    = ifSmts;
         this.elseSmts  = elseSmts;
