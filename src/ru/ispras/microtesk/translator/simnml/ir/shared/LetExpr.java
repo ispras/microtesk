d7213280422ce9702c3b1d839d0f843b63b36ce4
Sergey Smolov <ssedai@ispras.ru>
2013-09-11 13:51:32 +0000

    LetExpr: coding style changed to the one, that is used in the whole project

--- src/ru/ispras/microtesk/translator/simnml/ir/shared/LetExpr.java
+++ src/ru/ispras/microtesk/translator/simnml/ir/shared/LetExpr.java
@@ -1,20 +1,32 @@
+/*
+ * Copyright (c) 2013 ISPRAS
+ * 
+ * Institute for System Programming of Russian Academy of Sciences
+ * 
+ * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * 
+ * All rights reserved.
+ * 
+ * LetExpr.java, Sep 11, 2013 5:50:19
+ */
+
 package ru.ispras.microtesk.translator.simnml.ir.shared;
 
 public class LetExpr {
 
-	public Class<?> getJavaType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Class<?> getJavaType() {
+        // TODO Auto-generated method stub
+        return null;
+        }
 
-	public Object getValue() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Object getValue() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	public Object getText() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Object getText() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
