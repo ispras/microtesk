815db1e2d8dcd66c65788eefc2d07d0215c39404
Tatyana Sergeeva <leonsia@ispras.ru>
2013-09-11 11:36:49 +0000

    Repairing compilation

---
+++ src/ru/ispras/microtesk/translator/simnml/ir/shared/LetExpr.java
@@ -0,0 +1,20 @@
+package ru.ispras.microtesk.translator.simnml.ir.shared;
+
+public class LetExpr {
+
+	public Class<?> getJavaType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
