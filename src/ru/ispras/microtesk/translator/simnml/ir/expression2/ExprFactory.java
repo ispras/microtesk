5d8fe2864bccb2be14e4cdd1b2aef1e730714629
Andrei Tatarnikov <andrewt@ispras.ru>
2013-10-11 11:54:35 +0000

    New implementation.

--- src/ru/ispras/microtesk/translator/simnml/ir/expression2/ExprFactory.java
+++ src/ru/ispras/microtesk/translator/simnml/ir/expression2/ExprFactory.java
@@ -12,54 +12,26 @@
 
 package ru.ispras.microtesk.translator.simnml.ir.expression2;
 
-import java.util.EnumMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import ru.ispras.microtesk.translator.antlrex.SemanticError;
 import ru.ispras.microtesk.translator.antlrex.SemanticException;
 import ru.ispras.microtesk.translator.antlrex.Where;
 import ru.ispras.microtesk.translator.simnml.antlrex.WalkerContext;
 import ru.ispras.microtesk.translator.simnml.antlrex.WalkerFactoryBase;
 import ru.ispras.microtesk.translator.simnml.errors.UndefinedConstant;
+import ru.ispras.microtesk.translator.simnml.errors.ValueParsingFailure;
 import ru.ispras.microtesk.translator.simnml.ir.expression.Location;
 import ru.ispras.microtesk.translator.simnml.ir.shared.LetConstant;
 import ru.ispras.microtesk.translator.simnml.ir.shared.Type;
 
-public abstract class ExprFactory extends WalkerFactoryBase
+public final class ExprFactory extends WalkerFactoryBase
 {
-    private final static Map<ValueInfo.ValueKind, ExprFactory> factories =
-        new EnumMap<ValueInfo.ValueKind, ExprFactory>(ValueInfo.ValueKind.class);
-
-    protected ExprFactory(WalkerContext context)
+    public ExprFactory(WalkerContext context)
     {
         super(context);
-    }
-    
-    public static ExprFactory getInstance(ValueInfo.ValueKind targetKind, WalkerContext context)
-    {
-        if (factories.containsKey(targetKind))
-            return factories.get(targetKind);
-
-        final ExprFactory result;
-        switch (targetKind)
-        {
-        case MODEL:
-            result = new ExprFactoryModel(context);
-            break;
-
-        case NATIVE:
-            result = new ExprFactoryNative(context);
-            break;
-
-        default:
-            assert false : "Unknown target value kind!";
-            result = null;
-            break;
-        }
-
-        if (null != result)
-            factories.put(targetKind, result);
-
-        return result;
     }
 
     public final Expr namedConstant(Where w, String name) throws SemanticException
@@ -71,15 +43,80 @@ public abstract class ExprFactory extends WalkerFactoryBase
         return new ExprNodeNamedConst(constant);
     }
 
-    public final Expr constant(Where w, String text, int radix) throws SemanticException
+    public Expr constant(Where w, String text, int radix) throws SemanticException
     {
-        final ExprFactoryCreator creator = new ConstantCreator(this, w, text, radix); 
-        return creator.create();
+        try
+        {
+            final Integer value = Integer.valueOf(text, radix);
+            return new ExprNodeConst(ValueInfo.createNative(value), radix);
+        }
+        catch (NumberFormatException e) {}
+
+        try
+        {
+            final Long value = Long.valueOf(text, radix);
+            return new ExprNodeConst(ValueInfo.createNative(value), radix);
+        }
+        catch (NumberFormatException e) {}
+
+        getReporter().raiseError(w, new ValueParsingFailure(text, "Java integer"));
+        return null;
     }
 
-    public abstract Expr location(Location location);
-    public abstract Expr binary(Where w, String opID, Expr arg1, Expr arg2) throws SemanticException;
-    public abstract Expr unary (Where w, String opID, Expr arg) throws SemanticException;
-    public abstract Expr coerce(Where w, Expr src, Type type) throws SemanticException;
-    public abstract Expr evaluate(Where w, Expr src) throws SemanticException;
+    public Expr location(Location location)
+    {
+        return new ExprNodeLocation(location);
+    }
+
+    public Expr coerce(
+        Where w, Expr src, Type type) throws SemanticException
+    {
+        return new ExprNodeCoercion(src, type);
+    }
+
+    public Expr operator(
+        Where w, ValueKind target, String id, Expr ... operands) throws SemanticException
+    {
+        final Operator op = Operator.forText(id);
+
+        if (null == op)
+            getReporter().raiseError(w, new UnsupportedOperator(id));
+
+        if (operands.length != op.operands())
+            getReporter().raiseError(w, new OperandNumberMismatch(id, op.operands()));
+
+        final List<ValueInfo> values =
+            new ArrayList<ValueInfo>(operands.length);
+
+        for(Expr operand : operands)
+            values.add(operand.getValueInfo());
+
+        final ValueInfo result = calculate(w, target, op, values);
+        return new ExprNodeOperator(op, Arrays.asList(operands), result);
+    }
+    
+    private ValueInfo calculate(Where w, ValueKind target, Operator op, List<ValueInfo> values)
+    {
+        return null;
+    }
+}
+
+class UnsupportedOperator extends SemanticError
+{
+    private static final String FORMAT = "The %s operator is not supported.";
+
+    public UnsupportedOperator(String op)
+    {
+        super(String.format(FORMAT, op));
+    }
+}
+
+class OperandNumberMismatch extends SemanticError
+{
+    private static final String FORMAT = "The %s operator requires %d operands.";
+
+    public OperandNumberMismatch(String op, int operands)
+    {
+        super(String.format(FORMAT, op, operands));
+    }
 }
