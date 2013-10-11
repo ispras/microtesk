d43939a98370743466398de708260f37d4f93ea1
Andrei Tatarnikov <andrewt@ispras.ru>
2013-10-11 11:31:26 +0000

    New enumeration.

---
+++ src/ru/ispras/microtesk/translator/simnml/ir/expression2/ValueKind.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2013 ISPRAS
+ * 
+ * Institute for System Programming of Russian Academy of Sciences
+ * 
+ * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * 
+ * All rights reserved.
+ * 
+ * ValueKind.java, Oct 11, 2013 9:02:22 AM Andrei Tatarnikov
+ */
+
+package ru.ispras.microtesk.translator.simnml.ir.expression2;
+
+public enum ValueKind
+{
+    MODEL,
+    NATIVE
+}
