b2247a00d7c08401d3a06274d42c160b63fab18a
Andrei Tatarnikov <andrewt@ispras.ru>
2013-10-25 10:52:48 +0000

    Updated.

--- src/ru/ispras/microtesk/model/api/memory/Location.java
+++ src/ru/ispras/microtesk/model/api/memory/Location.java
@@ -161,7 +161,7 @@ public final class Location
     public Location bitField(int start, int end)
     {
         assert (start >= 0) && (end >= 0);
-        assert start <= end : "Start must be >= end. Reverse order is currently not supported.";
+        assert start <= end : "Start must be <= end. Reverse order is currently not supported.";
 
         final int bitSize = end - start + 1;
         return new Location(
