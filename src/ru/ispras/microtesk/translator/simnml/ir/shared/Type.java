c5ce854e5031cd468d787b61fee1f199f795a0a4
Andrei Tatarnikov <andrewt@ispras.ru>
2013-10-28 13:57:09 +0000

    Updated.

--- src/ru/ispras/microtesk/translator/simnml/ir/shared/Type.java
+++ src/ru/ispras/microtesk/translator/simnml/ir/shared/Type.java
@@ -103,11 +103,8 @@ public final class Type
     @Override
     public boolean equals(Object obj)
     {
-        if (this == obj)
-            return true;
-
-        if (obj == null)
-            return false;
+        if (this == obj) return true;
+        if (obj == null) return false;
 
         if (getClass() != obj.getClass())
             return false;
