eed593c46dcd5a34ea54b93059f729bf3b99e8ca
Andrei Tatarnikov <andrewt@ispras.ru>
2014-02-26 09:55:02 +0000

    Moved Ruby files.

--- dist/scripts/templates/plugins/pipeline/pipeline_solver.rb
+++
@@ -1,7 +0,0 @@
-require_relative "../../mtruby"
-
-# One big TODO:
-
-class PipelineSolver
-
-end
--- dist/scripts/templates/templates/arm_pipeline.rb
+++
@@ -1,7 +0,0 @@
-require_relative "../../templates/plugins/pipeline/pipeline_definition"
-
-# One big TODO:
-
-class ArmPipeline < PipelineDefinition
-
-end
