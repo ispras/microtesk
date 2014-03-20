c0934d885991a474de100acbb63f3302c186bf1b
Andrei Tatarnikov <andrewt@ispras.ru>
2014-03-20 09:00:31 +0000

    No longer needed.

--- build.bat
+++
@@ -1,3 +0,0 @@
-set CLASSPATH=jars/ant-antlr3.jar;jars/antlr-3.4-complete.jar;jars/commons-cli-1.2.jar
-
-ant %1
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export CLASSPATH=./jars/ant-antlr3.jar:./jars/antlr-3.4-complete.jar:./jars/commons-cli-1.2.jar
-ant $1
