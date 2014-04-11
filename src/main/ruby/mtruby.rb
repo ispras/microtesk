4aaf2958b099c72b933ff645b67eb2c51caa2d5b
Andrei Tatarnikov <andrewt@ispras.ru>
2014-04-11 13:51:34 +0000

    mtruby.rb was updated (no redundant code).

--- src/main/ruby/mtruby.rb
+++ src/main/ruby/mtruby.rb
@@ -3,9 +3,5 @@ require_relative 'lib/constructs/argument'
 require_relative 'lib/constructs/instruction'
 require_relative 'lib/constructs/instruction_block'
 
-
-#require_relative 'lib/storage'
-#require_relative 'lib/situation'
-
 Kernel.send(:define_method, :yes, lambda do true end)
 Kernel.send(:define_method, :no, lambda do false end)
