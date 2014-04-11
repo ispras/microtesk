f7ef2ceb2d0f9ec65205b6d3dbe930d64cd142ea
Andrei Tatarnikov <andrewt@ispras.ru>
2014-04-11 14:03:55 +0000

    Updated 'config.rb'.

--- src/main/ruby/config.rb
+++ src/main/ruby/config.rb
@@ -1,12 +1,12 @@
-#                              #
-#   MicroTESK Ruby front-end   #
-#                              #
-#      Configuration file      #
-#                              #
+################################################################################
+#                                                                              #
+#                MicroTESK Test Templates (Ruby Libraries)                     #
+#                                                                              #
+#                         Configuration File                                   #
+#                                                                              #
+################################################################################
 
-# ---------- Start ----------- #
-
-# --- Folders and Packages --- #
+# Folders and Packages 
 
 WD               = Dir.pwd
 HOME             = ENV['MICROTESK_HOME']
@@ -20,34 +20,7 @@ MODELS_JAR       = File.join(JARS, "models.jar")
 
 MODEL_CLASS_FRMT = "ru.ispras.microtesk.model.%s.Model"
 
-##    Sim-nml CPU description location
-## TODO: this isn't really used yet
-#
-#$TEMPLATE_LOCATION = "../model/samples/simple/design.nml"
-#
-##    Template file/folder locations
-## TODO: make a separate one for recursive folder inclusion
-#
-#$TEMPLATE_FILES = [
-#    "#{File.dirname(__FILE__)}/templates/demo_template.rb",
-#]
-#
-##    Generated test file output locations
-##    (if fewer than template locations, the last one is used
-##     for the remaining templates)
-#
-#$OUTPUT_LOCATIONS = [
-#    "./bin"
-#]
+# Debugging features 
 
-# ---- Debugging features ---- #
-
-#    Write results to stdout?
-
-$TO_STDOUT = true
-
-#    Write results to files?
-
-$TO_FILES = true
-
-# ------------ End ----------- #
+$TO_STDOUT = true # Write results to stdout?
+$TO_FILES  = true # Write results to files?
