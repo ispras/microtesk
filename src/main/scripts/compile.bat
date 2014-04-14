34b376d1334670f497b2da3763772c1a56acb428
Andrei Tatarnikov <andrewt@ispras.ru>
2014-04-14 10:00:28 +0000

    Bug #4827 was fixed.

--- src/main/scripts/compile.bat
+++ src/main/scripts/compile.bat
@@ -1,3 +1,4 @@
 
+ant -f %MICROTESK_HOME%/bin/build.xml clean
 java -ea -jar "%MICROTESK_HOME%/lib/jars/microtesk.jar" -d "%MICROTESK_HOME%/gen/src" %*
 ant -f %MICROTESK_HOME%/bin/build.xml
--- src/main/scripts/compile.sh
+++ src/main/scripts/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ant -f $MICROTESK_HOME/bin/build.xml clean
 java -ea -jar $MICROTESK_HOME/lib/jars/microtesk.jar -d $MICROTESK_HOME/gen/src $*
 ant -f $MICROTESK_HOME/bin/build.xml
 
