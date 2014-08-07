d545fe4fe00e16d90521a8c24a3826d7b0f307f8
Andrei Tatarnikov <andrewt@ispras.ru>
2014-08-07 11:42:40 +0000

    New Java classes to describe test templates.

---
+++ src/main/java/ru/ispras/microtesk/test/template/BlockId.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2014 ISPRAS (www.ispras.ru)
+ * 
+ * Institute for System Programming of Russian Academy of Sciences
+ * 
+ * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * 
+ * All rights reserved.
+ * 
+ * BlockId.java, Aug 7, 2014 2:53:27 PM Andrei Tatarnikov
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.microtesk.test.template;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * The BlockId class describes unique identifiers for instruction call blocks.
+ * The identifiers help uniquely identify elements that belong to different
+ * blocks, but have the same name. The identifier reflects the hierarchical
+ * structure of instruction call blocks. 
+ * 
+ * @author Andrei Tatarnikov
+ */
+
+final class BlockId
+{
+    private final List<Integer> indexes;
+    private int childCount;
+
+    public BlockId()
+    {
+        this(Collections.singletonList(1));
+    }
+
+    private BlockId(List<Integer> indexes)
+    {
+        this.indexes = indexes;
+        this.childCount = 0;
+    }
+
+    public BlockId nextChildId()
+    {
+        childCount++;
+
+        final List<Integer> childIndexes =
+            new ArrayList<Integer>(indexes.size() + 1);
+
+        childIndexes.addAll(indexes);
+        childIndexes.add(childCount);
+
+        return new BlockId(childIndexes);
+    }
+
+    @Override
+    public String toString()
+    {
+        final StringBuilder sb = new StringBuilder();
+        for (Integer index : indexes)
+        {
+            sb.append('_');
+            sb.append(index);
+        }
+        return sb.toString();
+    }
+}
