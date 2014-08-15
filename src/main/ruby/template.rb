6d6dc065a9e25ea4edcef7115857804e08ea501c
Andrei Tatarnikov <andrewt@ispras.ru>
2014-08-15 13:16:48 +0000

    Added a header.

--- src/main/ruby/template.rb
+++ src/main/ruby/template.rb
@@ -1,3 +1,26 @@
+#
+# Copyright (c) 2014 ISPRAS (www.ispras.ru)
+#
+# Institute for System Programming of Russian Academy of Sciences
+#
+# 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+#
+# All rights reserved.
+#
+# template.rb, Aug 15, 2014 5:16:42 PM Andrei Tatarnikov
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # Mixins for the Template class
 require_relative "output"
