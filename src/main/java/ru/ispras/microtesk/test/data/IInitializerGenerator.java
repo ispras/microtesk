b8d96ab859e8362c1b6a664c8eabf2c2e4166744
Andrei Tatarnikov <andrewt@ispras.ru>
2014-08-31 14:56:19 +0000

    Uses new ConcreteCall.

--- src/main/java/ru/ispras/microtesk/test/data/IInitializerGenerator.java
+++ src/main/java/ru/ispras/microtesk/test/data/IInitializerGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 ISPRAS
+ * Copyright (c) 2013 ISPRAS (www.ispras.ru)
  * 
  * Institute for System Programming of Russian Academy of Sciences
  * 
@@ -8,6 +8,18 @@
  * All rights reserved.
  * 
  * IInitializerGenerator.java, May 21, 2013 2:19:19 PM Andrei Tatarnikov
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package ru.ispras.microtesk.test.data;
@@ -17,9 +29,12 @@ import java.util.List;
 import ru.ispras.microtesk.model.api.data.Data;
 import ru.ispras.microtesk.model.api.exception.ConfigurationException;
 import ru.ispras.microtesk.test.block.Argument;
+import ru.ispras.microtesk.test.template.ConcreteCall;
 
 public interface IInitializerGenerator
 {
     public boolean isCompatible(Argument dest);
-    public List<ConcreteCall> createInitializingCode(Argument dest, Data data) throws ConfigurationException; 
+
+    public List<ConcreteCall> createInitializingCode(
+        Argument dest, Data data) throws ConfigurationException; 
 }
