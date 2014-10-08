02ab8514146fe23b538d990d056d029e31fe1674
Andrei Tatarnikov <andrewt@ispras.ru>
2014-10-08 17:06:28 +0000

    Added LazyValue to be used in preparators to refer to the generated test data.

---
+++ src/main/java/ru/ispras/microtesk/test/template/LazyValue.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2014 ISPRAS (www.ispras.ru)
+ * 
+ * Institute for System Programming of Russian Academy of Sciences
+ * 
+ * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * 
+ * All rights reserved.
+ * 
+ * LazyValue.java, Oct 8, 2014 8:46:10 PM Andrei Tatarnikov
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.microtesk.test.template;
+
+import ru.ispras.fortress.data.types.bitvector.BitVector;
+
+public final class LazyValue
+{
+    private BitVector data = null;
+
+    private final int start;
+    private final int size;
+
+    public LazyValue()
+    {
+        this.start = 0;
+        this.size = 0; // This means that we will use all data.
+    }
+
+    public LazyValue(int start, int end)
+    {
+        if ((start < 0) || (end < 0))
+            throw new IllegalArgumentException();
+
+        this.start = Math.min(start, end);
+        this.size = Math.abs(end - start) + 1;
+    }
+
+    public void setData(BitVector data)
+    {
+        if (null == data)
+            throw new NullPointerException();
+
+        this.data = data;
+    }
+
+    public void resetData()
+    {
+        this.data = null;
+    }
+
+    public int getValue()
+    {
+        if (null == data)
+            throw new IllegalStateException("Data is not set.");
+
+        if (0 == start && 0 == size) // Use all data
+            return data.intValue();
+
+        final BitVector mapping =
+            BitVector.newMapping(data, start, size);
+
+        return mapping.intValue();
+    }
+}
