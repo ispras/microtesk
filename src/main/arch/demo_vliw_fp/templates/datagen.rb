1c841b631b45cef8650e94830fe77ed8aae05c9f
Andrei Tatarnikov <andrewt@ispras.ru>
2014-10-08 13:17:30 +0000

    Added a specification of the 'preparator' construct.

--- src/main/arch/demo_vliw_fp/templates/datagen.rb
+++ src/main/arch/demo_vliw_fp/templates/datagen.rb
@@ -53,6 +53,30 @@ class VLIWDemo < Template
     @is_executable = true
   end
 
+  def pre
+    #
+    # TODO: Will be implemented soon. This is how it should work:
+    #
+    # Creates an instruction sequence that writes a 32-bit value to
+    # the resource referenced via the 'r' addressing mode (register GPR).
+    #
+    # Format:
+    # - The ':target' and ':size' attributes specify the name of the target
+    #   addressing mode and the size of assigned data respectively.
+    # - The 'target' and 'value' methods specify the target addressing mode
+    #   with all its arguments set and the value passed to the preparator
+    #   respectively. The arguments of the 'value' method specify which part
+    #   of the value is used. 
+    # 
+    # preparator(:target => 'r', :size => 32) {
+    #   vliw(
+    #     (lui  target, value(0, 15)),
+    #     (addi target, target, value(15, 31))
+    #   )
+    # }
+    #
+  end
+
   def run
     trace 'Data Generation Example: Debug Output'
 
