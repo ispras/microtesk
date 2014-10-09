3b9963809793e3f70fc04c8b364ed7836f2a8f5c
Andrei Tatarnikov <andrewt@ispras.ru>
2014-10-09 13:33:56 +0000

    Google cidy style for class MicroTESK.

--- src/main/java/ru/ispras/microtesk/MicroTESK.java
+++ src/main/java/ru/ispras/microtesk/MicroTESK.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 ISPRAS
+ * Copyright (c) 2012 ISPRAS (www.ispras.ru)
  * 
  * Institute for System Programming of Russian Academy of Sciences
  * 
@@ -8,6 +8,16 @@
  * All rights reserved.
  * 
  * MicroTESK.java, Oct 19, 2012 3:50:30 PM Andrei Tatarnikov
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package ru.ispras.microtesk;
@@ -15,83 +25,67 @@ package ru.ispras.microtesk;
 import org.apache.commons.cli.*;
 import ru.ispras.microtesk.translator.simnml.SimnMLAnalyzer;
 
-public final class MicroTESK
-{
-    private MicroTESK() {}
+public final class MicroTESK {
+  private MicroTESK() {}
 
-    private static class Parameters
-    {
-        public static final String INCLUDE = "i";
-        public static final String HELP    = "h";
-        public static final String OUTDIR  = "d";
-        public static final String TESTSIT = "s";
+  private static class Parameters {
+    public static final String INCLUDE = "i";
+    public static final String HELP = "h";
+    public static final String OUTDIR = "d";
+    public static final String TESTSIT = "s";
 
-        private static Options options = new Options();
+    private static Options options = new Options();
 
-        static
-        {
-            options.addOption(INCLUDE, "include", true,  "Sets include files directories");
-            options.addOption(OUTDIR,  "dir",     true,  "Sets where to place generated Java files");
-            options.addOption(TESTSIT, "sit",     true,  "Sets the location of user-defined test situations");
-            options.addOption(HELP,    "help",    false, "Shows this message");
-        };
+    static {
+      options.addOption(INCLUDE, "include", true, "Sets include files directories");
+      options.addOption(OUTDIR, "dir", true, "Sets where to place generated Java files");
+      options.addOption(TESTSIT, "sit", true, "Sets the location of user-defined test situations");
+      options.addOption(HELP, "help", false, "Shows this message");
+    };
 
-        private Parameters() {}
+    private Parameters() {}
 
-        public static CommandLine parse(String[] args) throws ParseException
-        {
-            CommandLineParser parser = new GnuParser();
-            return parser.parse(options, args);
-        }
-
-        public static void help()
-        {
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp(80, "[options] Sim-nML-files", "", options, "");
-        }
+    public static CommandLine parse(String[] args) throws ParseException {
+      CommandLineParser parser = new GnuParser();
+      return parser.parse(options, args);
     }
 
-    public static void main(String[] args)
-    {
-        final SimnMLAnalyzer analyzer = new SimnMLAnalyzer();
-
-        try
-        {
-            final CommandLine params = Parameters.parse(args);
-
-            if(params.hasOption(Parameters.HELP))
-            {
-                Parameters.help();
-                return;
-            }
-
-            if(params.hasOption(Parameters.INCLUDE))
-            {
-                analyzer.addPath(params.getOptionValue(Parameters.INCLUDE));
-            }
-
-            if(params.hasOption(Parameters.OUTDIR))
-            {
-                analyzer.setOutDir(params.getOptionValue(Parameters.OUTDIR));
-            }
-
-            if(params.hasOption(Parameters.TESTSIT))
-            {
-                analyzer.setTestSitDir(params.getOptionValue(Parameters.TESTSIT));
-            }
-
-            try
-            {
-                analyzer.start(params.getArgs());
-            }
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
-        catch(ParseException e)
-        {
-            Parameters.help();
-        }
+    public static void help() {
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp(80, "[options] Sim-nML-files", "", options, "");
     }
+  }
+
+  public static void main(String[] args) {
+    final SimnMLAnalyzer analyzer = new SimnMLAnalyzer();
+
+    try {
+      final CommandLine params = Parameters.parse(args);
+
+      if (params.hasOption(Parameters.HELP)) {
+        Parameters.help();
+        return;
+      }
+
+      if (params.hasOption(Parameters.INCLUDE)) {
+        analyzer.addPath(params.getOptionValue(Parameters.INCLUDE));
+      }
+
+      if (params.hasOption(Parameters.OUTDIR)) {
+        analyzer.setOutDir(params.getOptionValue(Parameters.OUTDIR));
+      }
+
+      if (params.hasOption(Parameters.TESTSIT)) {
+        analyzer.setTestSitDir(params.getOptionValue(Parameters.TESTSIT));
+      }
+
+      try {
+        analyzer.start(params.getArgs());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    } catch (ParseException e) {
+      Parameters.help();
+    }
+  }
 }
