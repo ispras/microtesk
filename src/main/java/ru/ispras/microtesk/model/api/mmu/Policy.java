5a34f53a33bfcce1f85df42a9cbeb5a8889e865f
Andrei Tatarnikov <andrewt@ispras.ru>
2014-10-14 18:09:23 +0000

    New headers and Google code style for ru.ispras.microtesk.model.api.mmu

--- src/main/java/ru/ispras/microtesk/model/api/mmu/Policy.java
+++ src/main/java/ru/ispras/microtesk/model/api/mmu/Policy.java
@@ -1,24 +1,14 @@
 /*
- * Copyright (c) 2014 ISPRAS (www.ispras.ru)
+ * Copyright 2014 ISP RAS (http://www.ispras.ru)
  * 
- * Institute for System Programming of Russian Academy of Sciences
- * 
- * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
- * 
- * All rights reserved.
- * 
- * Policy.java, Aug 28, 2014 2:05:14 PM Tatiana Sergeeva
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 
@@ -32,145 +22,126 @@ import java.util.LinkedList;
  * @author Tatiana Sergeeva
  */
 
-public interface Policy
-{
-    /**
-     * Handles a buffer hit.
-     * 
-     * @param index the line being hit.
-     */
+public interface Policy {
+  /**
+   * Handles a buffer hit.
+   * 
+   * @param index the line being hit.
+   */
 
-    void accessLine(int index);
+  void accessLine(int index);
 
-    /**
-     * Handles a buffer miss.
-     * 
-     * @return the line to be replaced.
-     */
+  /**
+   * Handles a buffer miss.
+   * 
+   * @return the line to be replaced.
+   */
 
-    int chooseVictim();
+  int chooseVictim();
 }
+
 
 /**
- * The FIFO (First In - First Out) data replacement policy. 
+ * The FIFO (First In - First Out) data replacement policy.
  */
 
-final class PolicyFIFO implements Policy
-{
-    /** Keeps line indexes in the order of their usage. */
-    private LinkedList<Integer> fifo = new LinkedList<Integer>();
+final class PolicyFIFO implements Policy {
+  /** Keeps line indexes in the order of their usage. */
+  private LinkedList<Integer> fifo = new LinkedList<Integer>();
 
-    PolicyFIFO(int associativity)
-    {
-        for (int i = 0; i < associativity; i++)
-        {
-            fifo.add(i);
-        }
+  PolicyFIFO(int associativity) {
+    for (int i = 0; i < associativity; i++) {
+      fifo.add(i);
+    }
+  }
+
+  @Override
+  public void accessLine(int index) {
+    for (int i = 0; i < fifo.size(); i++) {
+      if (fifo.get(i) == index) {
+        fifo.remove(i);
+        fifo.add(index);
+
+        return;
+      }
     }
 
-    @Override
-    public void accessLine(int index)
-    {
-        for (int i = 0; i < fifo.size(); i++)
-        {
-            if (fifo.get(i) == index)
-            {
-                fifo.remove(i);
-                fifo.add(index);
+    throw new IllegalStateException(String.format("Index %d cannot be found.", index));
+  }
 
-                return;
-            }
-        }
-
-        throw new IllegalStateException(
-            String.format("Index %d cannot be found.", index));
-    }
-
-    @Override
-    public int chooseVictim()
-    {
-        return fifo.peek();
-    }
+  @Override
+  public int chooseVictim() {
+    return fifo.peek();
+  }
 }
+
 
 /**
- * The LRU (Least Recently Used) data replacement policy. 
+ * The LRU (Least Recently Used) data replacement policy.
  */
 
-final class PolicyLRU implements Policy
-{
-    PolicyLRU(int associativity)
-    {
-        // TODO:
-    }
+final class PolicyLRU implements Policy {
+  PolicyLRU(int associativity) {
+    // TODO:
+  }
 
-    @Override
-    public void accessLine(int index)
-    {
-        // TODO:
-    }
+  @Override
+  public void accessLine(int index) {
+    // TODO:
+  }
 
-    @Override
-    public int chooseVictim()
-    {
-        // TODO:
-        return 0;
-    }
+  @Override
+  public int chooseVictim() {
+    // TODO:
+    return 0;
+  }
 }
+
 
 /**
  * The PLRU (Pseudo Least Recently Used) data replacement policy.
  */
 
-final class PolicyPLRU implements Policy
-{
-    /** The associativity. */
-    private int associativity;
+final class PolicyPLRU implements Policy {
+  /** The associativity. */
+  private int associativity;
 
-    /** The PLRU bits. */
-    private int bits;
+  /** The PLRU bits. */
+  private int bits;
 
-    /**
-     * {@inheritDoc}
-     *
-     * The associativity should not exceed 32.
-     */
+  /**
+   * {@inheritDoc}
+   * 
+   * The associativity should not exceed 32.
+   */
 
-    PolicyPLRU(int associativity)
-    {
-        this.associativity = associativity;
+  PolicyPLRU(int associativity) {
+    this.associativity = associativity;
+  }
+
+  @Override
+  public void accessLine(int index) {
+    setBit(index);
+  }
+
+  @Override
+  public int chooseVictim() {
+    for (int i = 0; i < associativity; i++) {
+      if ((bits & (1 << i)) == 0) {
+        setBit(i);
+        return i;
+      }
     }
 
-    @Override
-    public void accessLine(int index)
-    {
-        setBit(index);
+    throw new IllegalStateException("All bits are set to 1");
+  }
+
+  private void setBit(int i) {
+    final int mask = (1 << i);
+
+    bits |= mask;
+    if (bits == ((1 << associativity) - 1)) {
+      bits = mask;
     }
-
-    @Override
-    public int chooseVictim()
-    {
-        for (int i = 0; i < associativity; i++)
-        {
-            if ((bits & (1 << i)) == 0)
-            {
-                setBit(i);
-                return i;
-            }
-        }
-
-        throw new IllegalStateException("All bits are set to 1");
-    }
-
-    private void setBit(int i)
-    {
-        final int mask = (1 << i);
-
-        bits |= mask;
-
-        if (bits == ((1 << associativity) - 1))
-            bits = mask;
-    }
+  }
 }
-
-
--- src/main/java/ru/ispras/microtesk/model/api/mmu/PolicyId.java
+++ src/main/java/ru/ispras/microtesk/model/api/mmu/PolicyId.java
@@ -1,24 +1,14 @@
 /*
- * Copyright (c) 2014 ISPRAS (www.ispras.ru)
+ * Copyright 2014 ISP RAS (http://www.ispras.ru)
  * 
- * Institute for System Programming of Russian Academy of Sciences
- * 
- * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
- * 
- * All rights reserved.
- * 
- * PolicyId.java, Aug 28, 2014 1:59:36 PM Tatiana Sergeeva
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 
@@ -26,50 +16,43 @@ package ru.ispras.microtesk.model.api.mmu;
 
 /**
  * This enumeration contains basic data replacement policies.
- *
+ * 
  * @author <a href="mailto:leonsia@ispras.ru">Tatiana Sergeeva</a>
  */
 
-public enum PolicyId
-{
-    /**
-     * The FIFO (First In - First Out) data replacement policy. 
-     */
+public enum PolicyId {
+  /**
+   * The FIFO (First In - First Out) data replacement policy.
+   */
 
-    FIFO
-    {
-        @Override
-        public Policy newPolicy(int associativity)
-        {
-            return new PolicyFIFO(associativity);
-        }
-    },
+  FIFO {
+    @Override
+    public Policy newPolicy(int associativity) {
+      return new PolicyFIFO(associativity);
+    }
+  },
 
-    /**
-     * The LRU (Least Recently Used) data replacement policy. 
-     */
+  /**
+   * The LRU (Least Recently Used) data replacement policy.
+   */
 
-    LRU
-    {
-        @Override
-        public Policy newPolicy(int associativity)
-        {
-            return new PolicyLRU(associativity);
-        }
-    },
+  LRU {
+    @Override
+    public Policy newPolicy(int associativity) {
+      return new PolicyLRU(associativity);
+    }
+  },
 
-    /**
-     * The PLRU (Pseudo Least Recently Used) data replacement policy. 
-     */
-    
-    PLRU
-    {
-        @Override
-        public Policy newPolicy(int associativity)
-        {
-            return new PolicyPLRU(associativity);
-        }
-    };
+  /**
+   * The PLRU (Pseudo Least Recently Used) data replacement policy.
+   */
 
-    public abstract Policy newPolicy(int associativity);
+  PLRU {
+    @Override
+    public Policy newPolicy(int associativity) {
+      return new PolicyPLRU(associativity);
+    }
+  };
+
+  public abstract Policy newPolicy(int associativity);
 }
