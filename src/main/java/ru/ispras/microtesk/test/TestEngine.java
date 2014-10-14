2a3c033ce67260192c08f93cc8a6bca7e520f0cb
Andrei Tatarnikov <andrewt@ispras.ru>
2014-10-14 18:05:47 +0000

    New headers for ru.ispras.microtesk.test

--- src/main/java/ru/ispras/microtesk/test/TestEngine.java
+++ src/main/java/ru/ispras/microtesk/test/TestEngine.java
@@ -1,13 +1,5 @@
 /*
- * Copyright (c) 2013 ISPRAS (www.ispras.ru)
- * 
- * Institute for System Programming of Russian Academy of Sciences
- * 
- * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
- * 
- * All rights reserved.
- * 
- * TestEngine.java, May 8, 2013 11:00:02 AM Andrei Tatarnikov
+ * Copyright 2013-2014 ISP RAS (http://www.ispras.ru)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
