c87f738cc5bd2c54a674aec26317d6db81b154b5
Andrei Tatarnikov <andrewt@ispras.ru>
2014-10-14 17:04:46 +0000

    New headers and Google code style for ru.ispras.microtesk.translator.generation.

--- src/main/java/ru/ispras/microtesk/translator/generation/IClassGenerator.java
+++ src/main/java/ru/ispras/microtesk/translator/generation/IClassGenerator.java
@@ -1,13 +1,15 @@
 /*
- * Copyright (c) 2012 ISPRAS
+ * Copyright 2012-2014 ISP RAS (http://www.ispras.ru)
  * 
- * Institute for System Programming of Russian Academy of Sciences
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * All rights reserved.
- * 
- * IClassGenerator.java, Jul 10, 2012 10:44:34 AM Andrei Tatarnikov
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package ru.ispras.microtesk.translator.generation;
@@ -15,20 +17,18 @@ package ru.ispras.microtesk.translator.generation;
 import java.io.IOException;
 
 /**
- * The IClassGenerator interface is a base interface to be implemented by all
- * class file generators. 
+ * The IClassGenerator interface is a base interface to be implemented by all class file generators.
  * 
  * @author Andrei Tatarnikov
  */
 
-public interface IClassGenerator
-{
-    /**
-     * Runs generation of a class file.
-     * 
-     * @throws Exception Raised if the generator failed to generate the needed file
-     * due to an I/O problem.
-     */
+public interface IClassGenerator {
+  /**
+   * Runs generation of a class file.
+   * 
+   * @throws Exception Raised if the generator failed to generate the needed file due to an I/O
+   *         problem.
+   */
 
-    public void generate() throws IOException;
+  public void generate() throws IOException;
 }
--- src/main/java/ru/ispras/microtesk/translator/generation/ITemplateBuilder.java
+++ src/main/java/ru/ispras/microtesk/translator/generation/ITemplateBuilder.java
@@ -1,13 +1,15 @@
 /*
- * Copyright (c) 2012 ISPRAS
+ * Copyright 2012-2014 ISP RAS (http://www.ispras.ru)
  * 
- * Institute for System Programming of Russian Academy of Sciences
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * All rights reserved.
- * 
- * $Id: ITemplateBuilder.java, Jul 10, 2012 11:58:57 AM Andrei Tatarnikov Exp $
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package ru.ispras.microtesk.translator.generation;
@@ -16,25 +18,22 @@ import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroup;
 
 /**
- * The ITemplateBuilder interface is a base interface for all objects
- * that are responsible for initialization of class file templates. 
+ * The ITemplateBuilder interface is a base interface for all objects that are responsible for
+ * initialization of class file templates.
  * 
  * @author Andrei Tatarnikov
  */
 
-public interface ITemplateBuilder
-{
-    /**
-     * Performs initialization of the template of the target class based
-     * on templates described in the corresponding template group and
-     * information extracted from the intermediate representation of
-     * the target classes.     
-     * 
-     * @param group A template group that stores templates required to
-     * build generated representation.
-     *    
-     * @return Fully initialized template object. 
-     */
+public interface ITemplateBuilder {
+  /**
+   * Performs initialization of the template of the target class based on templates described in the
+   * corresponding template group and information extracted from the intermediate representation of
+   * the target classes.
+   * 
+   * @param group A template group that stores templates required to build generated representation.
+   * 
+   * @return Fully initialized template object.
+   */
 
-    public ST build(STGroup group);
+  public ST build(STGroup group);
 }
