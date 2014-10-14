229a45090db0401d02ca65339bc02f6ea9786536
Andrei Tatarnikov <andrewt@ispras.ru>
2014-10-14 17:27:51 +0000

    New headers and Google code style for ru.ispras.microtesk.translator.antlrex.errors

--- src/main/java/ru/ispras/microtesk/translator/antlrex/errors/SymbolTypeMismatch.java
+++ src/main/java/ru/ispras/microtesk/translator/antlrex/errors/SymbolTypeMismatch.java
@@ -1,13 +1,15 @@
 /*
- * Copyright (c) 2012 ISPRAS
+ * Copyright 2012-2014 ISP RAS (http://www.ispras.ru)
  * 
- * Institute for System Programming of Russian Academy of Sciences
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * All rights reserved.
- * 
- * SymbolTypeMismatch.java, Oct 22, 2012 1:22:33 PM Andrei Tatarnikov
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package ru.ispras.microtesk.translator.antlrex.errors;
@@ -17,45 +19,47 @@ import java.util.List;
 
 import ru.ispras.microtesk.translator.antlrex.ISemanticError;
 
-public final class SymbolTypeMismatch<Kind extends Enum<Kind>> implements ISemanticError
-{
-    public static final String FORMAT =
-        "The '%s' symbol uses a wrong type. It is %s while %s is expected in this expression.";
+public final class SymbolTypeMismatch<Kind extends Enum<Kind>> implements ISemanticError {
+  public static final String FORMAT =
+    "The '%s' symbol uses a wrong type. It is %s while %s is expected in this expression.";
 
-    private final String   symbolName;
-    private final Kind           kind;
-    private final List<Kind> expected;
+  private final String symbolName;
+  private final Kind kind;
+  private final List<Kind> expected;
 
-    public SymbolTypeMismatch(String symbolName, Kind kind, Kind expected)
-    {
-        this(symbolName, kind, Collections.singletonList(expected));
+  public SymbolTypeMismatch(String symbolName, Kind kind, Kind expected) {
+    this(symbolName, kind, Collections.singletonList(expected));
+  }
+
+  public SymbolTypeMismatch(String symbolName, Kind kind, List<Kind> expected) {
+    this.symbolName = symbolName;
+    this.kind = kind;
+    this.expected = expected;
+  }
+
+  private static <Kind extends Enum<Kind>> String kindsToString(List<Kind> kinds) {
+    final StringBuffer sb = new StringBuffer();
+
+    if (kinds.size() > 1) {
+      sb.append('{');
     }
 
-    public SymbolTypeMismatch(String symbolName, Kind kind, List<Kind> expected)
-    {
-        this.symbolName = symbolName;
-        this.kind       = kind;
-        this.expected   = expected;
+    for (Kind k : kinds) {
+      if (sb.length() > 0) {
+        sb.append(", ");
+      }
+      sb.append(k.name());
     }
 
-    private static <Kind extends Enum<Kind>> String kindsToString(List<Kind> kinds)
-    {
-        final StringBuffer sb = new StringBuffer(); 
-
-        if (kinds.size() > 1) sb.append('{');
-        for (Kind k : kinds)
-        {
-            if (sb.length() > 0) sb.append(", ");
-            sb.append(k.name());
-        }
-        if (kinds.size() > 1) sb.append('}');
-
-        return sb.toString();
+    if (kinds.size() > 1) {
+      sb.append('}');
     }
 
-    @Override
-    public String getMessage()
-    {
-        return String.format(FORMAT, symbolName, kind, kindsToString(expected));
-    }
+    return sb.toString();
+  }
+
+  @Override
+  public String getMessage() {
+    return String.format(FORMAT, symbolName, kind, kindsToString(expected));
+  }
 }
--- src/main/java/ru/ispras/microtesk/translator/antlrex/errors/UndeclaredSymbol.java
+++ src/main/java/ru/ispras/microtesk/translator/antlrex/errors/UndeclaredSymbol.java
@@ -1,33 +1,32 @@
 /*
- * Copyright (c) 2012 ISPRAS
+ * Copyright 2012-2014 ISP RAS (http://www.ispras.ru)
  * 
- * Institute for System Programming of Russian Academy of Sciences
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * All rights reserved.
- * 
- * UndeclaredSymbol.java, Oct 22, 2012 1:22:46 PM Andrei Tatarnikov
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package ru.ispras.microtesk.translator.antlrex.errors;
 
 import ru.ispras.microtesk.translator.antlrex.ISemanticError;
 
-public class UndeclaredSymbol implements ISemanticError
-{
-    private static final String FORMAT = "The '%s' symbol is not declared.";
+public class UndeclaredSymbol implements ISemanticError {
+  private static final String FORMAT = "The '%s' symbol is not declared.";
 
-    private final String symbolName; 
+  private final String symbolName;
 
-    public UndeclaredSymbol(String symbolName)
-    {
-        this.symbolName = symbolName;
-    }
+  public UndeclaredSymbol(String symbolName) {
+    this.symbolName = symbolName;
+  }
 
-    @Override
-    public String getMessage()
-    {
-        return String.format(FORMAT, symbolName);
-    }
+  @Override
+  public String getMessage() {
+    return String.format(FORMAT, symbolName);
+  }
 }
