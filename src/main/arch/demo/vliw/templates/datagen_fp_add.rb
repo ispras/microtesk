d6118bd33898bc3cfaa1495a3f68306581d265f4
Andrei Tatarnikov <andrewt@ispras.ru>
2014-10-14 12:03:53 +0000

    Updated the example.

--- src/main/arch/demo/vliw/templates/datagen_fp_add.rb
+++ src/main/arch/demo/vliw/templates/datagen_fp_add.rb
@@ -24,14 +24,6 @@ class VLIWDemo < Template
   end
 
   def pre
-    preparator(:target => 'R') {
-      comment 'Initializer for R'  
-      vliw(
-        (lui target, value(16, 31)),
-        (addi target, target, value(0, 15))
-      )
-    }
-
     preparator(:target => 'F') {
       comment 'Initializer for F'
       # GPR[25] holds a temporary value
@@ -47,30 +39,29 @@ class VLIWDemo < Template
   end
 
   def run
-    print_fpr 3
-    print_fpr 5
-    print_fpr 4
-    print_fpr 6
+    print_all_fprs
 
     vliw(
       (add_s f(1), f(3), f(5) do situation('fp.add', :case => 'normal', :exp => 8, :frac => 23) end),
       (add_s f(2), f(4), f(6) do situation('fp.add', :case => 'overflow', :exp => 8, :frac => 23) end) 
     )
 
-    print_fpr 1
-    print_fpr 2
-  end
+    vliw(
+      (add_s f(7),  f(9), f(11) do situation('fp.add', :case => 'underflow', :exp => 8, :frac => 23) end),
+      (add_s f(8), f(10), f(12) do situation('fp.add', :case => 'inexact', :exp => 8, :frac => 23) end) 
+    )
 
-  def print_gpr(index)
-    trace "GPR[%d] = %s", index, gpr(index)
+    print_all_fprs
+  end
+  
+  def print_all_fprs
+    trace ''
+    (1..12).each { |index| print_fpr index }
+    trace ''
   end
 
   def print_fpr(index)
     trace "FPR[%d] = %s", index, fpr(index)
-  end
-
-  def gpr(index)
-    location('GPR', index)
   end
 
   def fpr(index)
