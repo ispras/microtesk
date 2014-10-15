25c1742d0d139562055e87d2154332a0a3bf3b7c
Andrei Tatarnikov <andrewt@ispras.ru>
2014-10-15 10:47:41 +0000

    New headers and Google code style for ru.ispras.microtesk.model.api.data.

--- src/main/java/ru/ispras/microtesk/model/api/data/Data.java
+++ src/main/java/ru/ispras/microtesk/model/api/data/Data.java
@@ -1,13 +1,15 @@
 /*
- * Copyright (c) 2012 ISPRAS
+ * Copyright 2012-2014 ISP RAS (http://www.ispras.ru)
  * 
- * Institute for System Programming of Russian Academy of Sciences
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * All rights reserved.
- * 
- * Data.java, Nov 28, 2012 4:37:01 PM Andrei Tatarnikov
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package ru.ispras.microtesk.model.api.data;
@@ -15,36 +17,30 @@ package ru.ispras.microtesk.model.api.data;
 import ru.ispras.fortress.data.types.bitvector.BitVector;
 import ru.ispras.microtesk.model.api.type.Type;
 
-public final class Data
-{
-    private BitVector rawData;
-    private Type       type;
+public final class Data {
+  private BitVector rawData;
+  private Type type;
 
-    public Data(BitVector rawData, Type type)
-    {
-        this.rawData = rawData;
-        this.type = type;
-    }
+  public Data(BitVector rawData, Type type) {
+    this.rawData = rawData;
+    this.type = type;
+  }
 
-    public Data(Data data)
-    {
-        this.rawData = data.getRawData().copy();
-        this.type    = data.getType();
-    }
+  public Data(Data data) {
+    this.rawData = data.getRawData().copy();
+    this.type = data.getType();
+  }
 
-    public Data(Type type)
-    {
-        this.rawData = BitVector.newEmpty(type.getBitSize());
-        this.type    = type;
-    }
+  public Data(Type type) {
+    this.rawData = BitVector.newEmpty(type.getBitSize());
+    this.type = type;
+  }
 
-    public BitVector getRawData()
-    {
-        return rawData;
-    }
+  public BitVector getRawData() {
+    return rawData;
+  }
 
-    public Type getType()
-    {
-        return type;
-    }
+  public Type getType() {
+    return type;
+  }
 }
--- src/main/java/ru/ispras/microtesk/model/api/data/IValueConverter.java
+++ src/main/java/ru/ispras/microtesk/model/api/data/IValueConverter.java
@@ -1,13 +1,15 @@
 /*
- * Copyright (c) 2012 ISPRAS
+ * Copyright 2012-2014 ISP RAS (http://www.ispras.ru)
  * 
- * Institute for System Programming of Russian Academy of Sciences
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- * 25 Alexander Solzhenitsyn st. Moscow 109004 Russia
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * All rights reserved.
- * 
- * IValueConverter.java, Nov 30, 2012 10:47:07 AM Andrei Tatarnikov
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package ru.ispras.microtesk.model.api.data;
@@ -15,13 +17,12 @@ package ru.ispras.microtesk.model.api.data;
 import ru.ispras.fortress.data.types.Radix;
 import ru.ispras.microtesk.model.api.type.Type;
 
-public interface IValueConverter
-{
-    public Data fromLong(Type type, long value);
-    public long toLong(Data data);
-
-    public Data fromInt(Type type, int value);
-    public int toInt(Data data);
-    
-    public Data fromString(Type type, String value, Radix radix);
+public interface IValueConverter {
+  public Data fromLong(Type type, long value);
+  public long toLong(Data data);
+  
+  public Data fromInt(Type type, int value);
+  public int toInt(Data data);
+  
+  public Data fromString(Type type, String value, Radix radix);
 }
