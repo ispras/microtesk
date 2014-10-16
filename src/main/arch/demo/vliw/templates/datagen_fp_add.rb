2d52cb1d1ddfe7fc373128fe288d01aac727e21d
Andrei Tatarnikov <andrewt@ispras.ru>
2014-10-16 18:14:55 +0000

    Test Situation: sub for fp

--- src/main/arch/demo/vliw/templates/datagen_fp_add.rb
+++ src/main/arch/demo/vliw/templates/datagen_fp_add.rb
@@ -27,13 +27,23 @@ class VliwDemo < VliwDemoTemplate
     trace_all_fprs
 
     vliw(
-      (add_s f(1), f(3), f(5) do situation('fp.add', :case => 'normal', :exp => 8, :frac => 23) end),
-      (add_s f(2), f(4), f(6) do situation('fp.add', :case => 'overflow', :exp => 8, :frac => 23) end) 
+      (add_s f(1), f(1), f(2) do situation('fp.add', :case => 'normal', :exp => 8, :frac => 23) end),
+      (add_s f(3), f(3), f(4) do situation('fp.add', :case => 'overflow', :exp => 8, :frac => 23) end) 
     )
 
     vliw(
-      (add_s f(7),  f(9), f(11) do situation('fp.add', :case => 'underflow', :exp => 8, :frac => 23) end),
-      (add_s f(8), f(10), f(12) do situation('fp.add', :case => 'inexact', :exp => 8, :frac => 23) end) 
+      (add_s f(5), f(5), f(6) do situation('fp.add', :case => 'underflow', :exp => 8, :frac => 23) end),
+      (add_s f(7), f(7), f(8) do situation('fp.add', :case => 'inexact', :exp => 8, :frac => 23) end) 
+    )
+
+    vliw(
+      (sub_s  f(9),  f(9), f(10) do situation('fp.sub', :case => 'normal', :exp => 8, :frac => 23) end),
+      (sub_s f(11), f(11), f(12) do situation('fp.sub', :case => 'overflow', :exp => 8, :frac => 23) end) 
+    )
+
+    vliw(
+      (sub_s f(13), f(13), f(14) do situation('fp.sub', :case => 'underflow', :exp => 8, :frac => 23) end),
+      (sub_s f(15), f(15), f(0) do situation('fp.sub', :case => 'inexact', :exp => 8, :frac => 23) end) 
     )
 
     trace_all_fprs
@@ -41,7 +51,7 @@ class VliwDemo < VliwDemoTemplate
 
   def trace_all_fprs
     trace ''
-    (1..12).each { |index| trace_fpr index }
+    (0..15).each { |index| trace_fpr index }
     trace ''
   end
 
