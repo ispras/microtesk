af74e63d140721615f4226b5759bea703a1b3771
Andrei Tatarnikov <andrewt@ispras.ru>
2014-10-16 18:09:56 +0000

    Test Situation: sub for int

--- src/main/arch/demo/vliw/templates/datagen_add.rb
+++ src/main/arch/demo/vliw/templates/datagen_add.rb
@@ -30,18 +30,26 @@ class VliwDemo < VliwDemoTemplate
   end
 
   def run
-    trace_gpr 3
-    trace_gpr 5
-    trace_gpr 4
-    trace_gpr 6
+    trace_all_gprs
 
     vliw(
       (add r(1), r(3), r(5) do situation('add', :case => 'normal', :size => 32) end),
       (add r(2), r(4), r(6) do situation('add', :case => 'overflow', :size => 32) end)
     )
 
-    trace_gpr 1
-    trace_gpr 2
+    vliw(
+      (sub r(7), r(8), r(9) do situation('sub', :case => 'normal', :size => 32) end),
+      (sub r(10), r(11), r(12) do situation('sub', :case => 'overflow', :size => 32) end)
+    )
+
+    trace_all_gprs
+  end
+
+  # Prints values of all involved GPRs
+  def trace_all_gprs
+    trace ''
+    (1..12).each { |index| trace_gpr index }
+    trace ''
   end
 
 end
