d6400401629fefe0d452c0d9d769023b3988537e
Alexander Kamkin <kamkin@ispras.ru>
2014-10-17 10:23:06 +0000

    combination.rb has been modified.

--- src/main/arch/demo/vliw/templates/combination.rb
+++ src/main/arch/demo/vliw/templates/combination.rb
@@ -28,31 +28,30 @@ class CombinationTemplate < VliwBaseTemplate
   def run
     # Randomized sequence
     comment "Randomized sequence"
-    block(:compositor => 'RANDOM', :combinator => 'RANDOM') {
-      vliw(
-        (addi r(rand(1, 31)), r(0), rand(0, 31)),
-        (addi r(rand(1, 31)), r(0), rand(0, 31))
-      )
+    block(:combinator => 'PRODUCT', :compositor => 'RANDOM') {
+      block {
+        vliw(
+          (add r(1), r(3), r(5) do situation('add', :case => 'normal',   :size => 32) end),
+          (add r(2), r(4), r(6) do situation('add', :case => 'overflow', :size => 32) end)
+        )
 
-      vliw(
-        (add r(1), r(3), r(5) do situation('add', :case => 'normal', :size => 32) end),
-        (add r(2), r(4), r(6) do situation('add', :case => 'overflow', :size => 32) end)
-      )
+        vliw(
+          (sub r(7),   r(8),  r(9) do situation('sub', :case => 'normal',   :size => 32) end),
+          (sub r(10), r(11), r(12) do situation('sub', :case => 'overflow', :size => 32) end)
+        )
+      }
 
-      vliw(
-        (sub r(7),   r(8),  r(9) do situation('sub', :case => 'normal', :size => 32) end),
-        (sub r(10), r(11), r(12) do situation('sub', :case => 'overflow', :size => 32) end)
-      )
+      block {
+        vliw(
+          (add_s f(5), f(5), f(6) do situation('fp.add', :case => 'underflow', :exp => 8, :frac => 23) end),
+          (add_s f(7), f(7), f(8) do situation('fp.add', :case => 'inexact',   :exp => 8, :frac => 23) end) 
+        )
 
-      vliw(
-        (add r(1), r(3), r(5)),
-        (add r(2), r(4), r(6))
-      ) do situation('zero', :size => 32) end
-
-      vliw(
-        (add r(_), r(_), r(_)),
-        (add r(_), r(_), r(_))
-      ) do situation('random', :size => 32, :min_imm => 1, :max_imm => 31) end
+        vliw(
+          (sub_s  f(9),  f(9), f(10) do situation('fp.sub', :case => 'normal',   :exp => 8, :frac => 23) end),
+          (sub_s f(11), f(11), f(12) do situation('fp.sub', :case => 'overflow', :exp => 8, :frac => 23) end) 
+        )
+      }
     }
     newline
     
