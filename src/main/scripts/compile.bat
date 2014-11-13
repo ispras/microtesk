62d4f5e9148a422437171764b2e7ae8d7d384bd5
Alexander Kamkin <kamkin@ispras.ru>
2014-11-13 08:42:03 +0000

    compile.bat has been fixed.

--- src/main/scripts/compile.bat
+++ src/main/scripts/compile.bat
@@ -1,4 +1,3 @@
-
-ant -f %MICROTESK_HOME%/bin/build.xml clean
-java -ea -jar "%MICROTESK_HOME%/lib/jars/microtesk.jar" -d "%MICROTESK_HOME%/gen/src" %*
-ant -f %MICROTESK_HOME%/bin/build.xml
+@call ant -f %MICROTESK_HOME%/bin/build.xml clean
+@call java -ea -jar "%MICROTESK_HOME%/lib/jars/microtesk.jar" -d "%MICROTESK_HOME%/gen/src" %*
+@call ant -f %MICROTESK_HOME%/bin/build.xml
