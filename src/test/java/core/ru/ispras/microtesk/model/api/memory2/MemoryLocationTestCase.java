f8f09537cf38c2f796bf0dee4e8c4a3d95198b79
Andrei Tatarnikov <andrewt@ispras.ru>
2014-11-26 14:59:09 +0000

    Updated test case for Memory/Location.

--- src/test/java/core/ru/ispras/microtesk/model/api/memory2/MemoryLocationTestCase.java
+++ src/test/java/core/ru/ispras/microtesk/model/api/memory2/MemoryLocationTestCase.java
@@ -16,25 +16,87 @@ package ru.ispras.microtesk.model.api.memory2;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import ru.ispras.fortress.data.types.bitvector.BitVector;
+import ru.ispras.fortress.randomizer.Randomizer;
 import ru.ispras.microtesk.model.api.data.Data;
 import ru.ispras.microtesk.model.api.type.Type;
 
-public class MemoryLocationTestCase {
+public final class MemoryLocationTestCase {
 
+  public static final Type  LONG = Type.INT(32);
+  public static final Type  WORD = Type.CARD(32);
+  public static final Type SHORT = Type.INT(16);
+  public static final Type HWORD = Type.CARD(16);
+  public static final Type SBYTE = Type.INT(8);
+  public static final Type  BYTE = Type.CARD(8);
+  public static final Type   BIT = Type.CARD(1);
+
+  public static final int COUNT = 32;
+  
   @Test
-  public void test() {
-    assertTrue(true);
-    
+  public void testRandom() {
     Memory.setHandlingEnabled(false);
-    final Memory mem = Memory.MEM("MEM", Type.CARD(32), 32);
-    //printAll(mem);
+
+    // Allocates memory
+    final Memory mem = Memory.MEM("MEM", WORD, COUNT);
+
+    // Creates random data
+    final List<Data> randomData = new ArrayList<Data>(COUNT);
+    for (int index = 0; index < COUNT; ++index) {
+      final BitVector rawData = BitVector.newEmpty(WORD.getBitSize());
+      Randomizer.get().fill(rawData);
+
+      final Data data = new Data(rawData, WORD);
+      randomData.add(data);
+    }
+
+    // Stores random data
+    for (int locationIndex = 0; locationIndex < COUNT; ++locationIndex) {
+      mem.access(locationIndex).store(randomData.get(locationIndex));
+    }
+
+    // Loads data and checks its validity
+    for (int locationIndex = 0; locationIndex < COUNT; ++locationIndex) {
+      assertEquals(randomData.get(locationIndex), mem.access(locationIndex).load());
+    }
+
+    // Reads data using bit fields and checks its validity
+    final int locationSize = mem.getType().getBitSize();
+    for (int locationIndex = 0; locationIndex < COUNT; ++locationIndex) {
+      int fieldSize = locationSize / 2;
+      while (fieldSize >= 1) {
+        final int fieldCount = locationSize / fieldSize;
+        for (int fieldIndex = 0; fieldIndex < fieldCount; ++fieldIndex) {
+          final int start = fieldSize * fieldIndex;
+          final int end = start + fieldSize - 1;
+
+          final BitVector expected = 
+              BitVector.newMapping(randomData.get(locationIndex).getRawData(), start, fieldSize);
+          final BitVector current = 
+              mem.access(locationIndex).bitField(start, end).load().getRawData();
+
+          assertEquals(expected, current);
+        }
+        fieldSize = fieldSize / 2;
+      }
+    }
+  }
+  
+  @Test
+  public void test2() {
+    /*
+    printAll(mem);
     
-    mem.access(10).store(new Data(BitVector.valueOf(0xFF0000FF, 32), Type.CARD(32)));
+    mem.access(10).store(new Data(BitVector.valueOf(0xFF0000FF, 32), WORD));
     
     System.out.println(mem.access(10).toBinString());
+    System.out.println(mem.access(10).bitField(0, 15).concat(mem.access(10).bitField(16, 31)).toBinString());
+    */
     
     /*
     System.out.println(mem.access(10).bitField(0, 15).toBinString());
@@ -46,6 +108,8 @@ public class MemoryLocationTestCase {
     System.out.println(mem.access(10).bitField(16, 23).toBinString());
     System.out.println(mem.access(10).bitField(24, 31).toBinString());
    */
+    
+    /*
 
     final Memory alias1 = Memory.MEM("MEM", Type.CARD(16), 2, mem.access(10));
     printAll(alias1);
@@ -60,14 +124,19 @@ public class MemoryLocationTestCase {
     printAll(alias4);
     
     final Memory alias5 = Memory.MEM("MEM", Type.CARD(1), 32, mem.access(10));
-    printAll(alias5);
+    printAll(alias5);*/
 
   }
   
+  /*
   private void printAll(Memory m) {
+    System.out.println("-----------------------------------");
+    System.out.println(m);
+
     for (int index = 0; index < m.getLength(); ++index) {
       System.out.println(m.access(index).toBinString());
     }
+    System.out.println();
   }
-
+  */
 }
