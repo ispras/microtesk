9b2407b6d5f627490fc7c77fbc3fd692649c692d
Platon Bibik <bibik.platon@yandex.ru>
2014-12-30 02:01:06 +0000

    Insertion sort is now with aliases. Optimization of registers usage is required

--- src/main/arch/minimips/templates/insertionsort.rb
+++ src/main/arch/minimips/templates/insertionsort.rb
@@ -24,7 +24,7 @@ class  InsertionSortTemplate < MinimipsBaseTemplate
 
       data {
         label :array
-        word 1, 6, 2, 7, 5, 6, 3, 8, 9, 5, 6, 7
+        word 1, 6, 2, 7, 5, 6, 3, 8, 9, 5, 6, 3
 
         label :length
         word 12
@@ -34,95 +34,95 @@ class  InsertionSortTemplate < MinimipsBaseTemplate
     def run
 
     #looks terrible, there sould be "lw" instruction for data
-    addi reg(1), reg(0), :length
-    lw reg(4), 0, reg(1)
+    addi at, zero, :length
+    lw a0, 0, at
 
-    la reg(5), :array
-    add reg(20), reg(0), reg(5)
-    trace "%x", gpr(5)
+    la a1, :array
+    add s4, zero, a1
     
-    addi reg(8), reg(0), 1 #counter of the top-level loop
+    addi t0, zero, 1 #counter of the top-level loop
 
     label :loop
-        beq reg(8), reg(4), :exit
-        sll reg(0), reg(0), 0 #DELAY SLOT
+      beq t0, a0, :exit
+      sll zero, zero, 0 #DELAY SLOT
     
-        addi reg(18), reg(0), 4
-        mult reg(8), reg(18) #computing the address of new element
-        mflo reg(6) #the fact that the result of multiplication is less than
-                    #32 bit long is not guaranteed!
-
-        add reg(6), reg(6), reg(5) #computing the address of new element
+      addi s2, zero, 4
+      mult t0, s2 #computing the address of new element
+      mflo a2 #the fact that the result of multiplication is less than
+                  #32 bit long is not guaranteed!
+      add a2, a2, a1 #computing the address of new element
+  
+      lw t1, 0, a2 #load new elenent
     
-        lw reg(9), 0, reg(6) #load new elenent
+      addi s1, zero, 4 #counter of an inner loop
     
-        addi reg(17), reg(0), 4 #counter of an inner loop
-    
-        label :loop1
-        sub reg(7), reg(6), reg(17) #computing address of new element in search
+      label :loop1
+        sub a3, a2, s1 #computing address of new element in search
        
-        lw reg(10), 0, reg(7)#loading the value of new elem in search
+        lw t2, 0, a3#loading the value of new elem in search
 
-        slt reg(1), reg(9), reg(10)
-        bne reg(1), reg(0), :cont 
-        sll reg(0), reg(0), 0 #DELAY SLOT
+        slt at, t1, t2
+        bne at, zero, :cont 
+        sll zero, zero, 0 #DELAY SLOT
 
-        addi reg(7), reg(7), 4 ##return to the greater number in search
+        addi a3, a3, 4 ##return to the greater number in search
            
        
-        add reg(12), reg(0), reg(6) #address of shifting (counter)       
-        add reg(13), reg(0), reg(9) #saving the value of element 
+        add t4, zero, a2 #address of shifting (counter)       
+        add t5, zero, t1 #saving the value of element 
                                     #that will be inserted
        
         label :loop2 #shift
-            beq reg(7), reg(12), :final #the shift is done, jump to 
+          beq a3, t4, :final #the shift is done, jump to 
                                         #inserting and next iteration
-            sll reg(0), reg(0), 0 #DELAY SLOT
-            addi reg(18), reg(0), 4
-            sub reg(15), reg(12), reg(18)
+          sll zero, zero, 0 #DELAY SLOT
+          addi s2, zero, 4
+          sub t7, t4, s2
          
-            #SHIFT
-            lw reg(14), 0, reg(15)
-            sw reg(14), 0, reg(12)
-            #SHIFT
+          #SHIFT
+          lw t6, 0, t7
+          sw t6, 0, t4
+          #SHIFT
          
-            addi reg(18), reg(0), 4
-            sub reg(12), reg(12), reg(18) #decrement of the counter
-            j :loop2
-            sll reg(0), reg(0), 0 #DELAY SLOT
+          addi s2, zero, 4
+          sub t4, t4, s2 #decrement of the counter
+          j :loop2
+          sll zero, zero, 0 #DELAY SLOT
        
         label :cont
-        addi reg(17), reg(17), 4 #increment
+        addi s1, s1, 4 #increment
         j :loop1
-        sll reg(0), reg(0), 0 #DELAY SLOT
+        sll zero, zero, 0 #DELAY SLOT
        
-    label :final
-    sw reg(13), 0, reg(7)#inserting
-    label :exit1
-    addi reg(8), reg(8), 1 #decrement for the counter
-    j :loop
-    sll reg(0), reg(0), 0 #DELAY SLOT
+      label :final
+      sw t5, 0, a3 #inserting
+      label :exit1
+
+      addi t0, t0, 1 #decrement for the counter
+      j :loop
+
+    sll zero, zero, 0 #DELAY SLOT
     
     label :exit
 
 #OUTPUT
     
-    #la reg(4), array
-    #addi reg(6), reg(0), 8
+    #la a0, array
+    #addi a2, zero, 8
     
 
-    #sub reg(4), reg(4), reg(6)
+    #sub a0, a0, a2
     #trace "%x", gpr(4)
     #trace "%x", array
     
-    add reg(4), reg(0), reg(0) #reset to zero of $4 (temporary hack)
-    la reg(4), :array
+    add a0, zero, zero #reset to zero of $4 (temporary hack)
+    la a0, :array
     $i = 0
     while $i < 12 do
-    	lw reg(3), 0, reg(4)    
+    	lw v1, 0, a0    
         #trace "%x", gpr(5)
         trace "%x", gpr(3)
-        addi reg(4), reg(4), 4
+        addi a0, a0, 4
         
         $i +=1
     end
