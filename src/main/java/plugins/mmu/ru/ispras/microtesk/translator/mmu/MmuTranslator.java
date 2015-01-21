d329340a68b72de972dcd534b200fb9b0a1ac6cf
Andrei Tatarnikov <andrewt@ispras.ru>
2015-01-21 11:50:26 +0000

    Applied code conventions to MMU translator.

--- src/main/java/plugins/mmu/ru/ispras/microtesk/translator/mmu/MmuTranslator.java
+++ src/main/java/plugins/mmu/ru/ispras/microtesk/translator/mmu/MmuTranslator.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2015 ISP RAS (http://www.ispras.ru)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package ru.ispras.microtesk.translator.mmu;
 
 import java.io.File;
@@ -16,102 +30,90 @@ import ru.ispras.microtesk.translator.mmu.grammar.MmuParser;
 import ru.ispras.microtesk.translator.mmu.grammar.MmuTreeWalker;
 import ru.ispras.microtesk.translator.simnml.ir.IR;
 
-public class MmuTranslator
-{
-    private static final ILogStore LOG = new ILogStore()
-    {
-        @Override
-        public void append(LogEntry entry)
-        {
-            System.err.println(entry);
-        }
-    };
-    
-    public static String getModelName(String fileName)
-    {
-        final String shortFileName = getShortFileName(fileName);
-        final int dotPos = shortFileName.lastIndexOf('.');
+public class MmuTranslator {
+  private static final ILogStore LOG = new ILogStore() {
+    @Override
+    public void append(LogEntry entry) {
+      System.err.println(entry);
+    }
+  };
 
-        if (-1 == dotPos)
-            return shortFileName.toLowerCase();
+  public static String getModelName(String fileName) {
+    final String shortFileName = getShortFileName(fileName);
+    final int dotPos = shortFileName.lastIndexOf('.');
 
-        return shortFileName.substring(0, dotPos).toLowerCase();
+    if (-1 == dotPos)
+      return shortFileName.toLowerCase();
+
+    return shortFileName.substring(0, dotPos).toLowerCase();
+  }
+
+  public static String getShortFileName(String fileName) {
+    return new File(fileName).getName();
+  }
+
+  public static void main(String[] args) {
+    if (args.length != 1) {
+      System.err.println("Number of cmd arguments is " + args.length);
+      return;
     }
 
-    public static String getShortFileName(String fileName)
-    {
-        return new File(fileName).getName();
+    final String fileName = args[0];
+    final String modelName = getModelName(fileName);
+
+    System.out.println("Translating: " + fileName);
+    System.out.println("Model name: " + modelName);
+    System.out.println();
+
+    final SymbolTable symbols = new SymbolTable();
+
+    final IR ir = new IR();
+
+    try {
+      final ANTLRReaderStream input = new ANTLRReaderStream(new FileReader(fileName));
+      input.name = fileName;
+
+      final MmuLexer lexer = new MmuLexer(input);
+      final CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+      final MmuParser parser = new MmuParser(tokens);
+
+      parser.assignLog(LOG);
+      parser.assignSymbols(symbols);
+      parser.commonParser.assignLog(LOG);
+      parser.commonParser.assignSymbols(symbols);
+
+      final MmuParser.startRule_return r = parser.startRule();
+      final CommonTree t = (CommonTree) r.getTree();
+
+      System.out.println("AST: " + t.toStringTree());
+
+      if (!parser.isCorrect()) {
+        System.err.println("TRANSLATION WAS INTERRUPTED DUE TO SYNTACTIC ERRORS.");
+        return;
+      }
+
+      final CommonTreeNodeStream nodes = new CommonTreeNodeStream(t);
+      nodes.setTokenStream(tokens);
+
+      final MmuTreeWalker walker = new MmuTreeWalker(nodes);
+      walker.assignLog(LOG);
+      walker.assignSymbols(symbols);
+      walker.assignIR(ir);
+
+      /*
+      walker.startRule();
+
+      if (!walker.isCorrect()) {
+        System.err.println("TRANSLATION WAS INTERRUPTED DUE TO SEMANTIC ERRORS.");
+        return;
+      }
+      */
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
-    public static void main(String[] args)
-    {
-        if (args.length != 1)
-        {
-            System.err.println("Number of cmd arguments is " + args.length);
-            return;
-        }
-
-        final String fileName = args[0];
-        final String modelName = getModelName(fileName);
-        
-        System.out.println("Translating: " + fileName);
-        System.out.println("Model name: " + modelName);
-        System.out.println();
-
-        final SymbolTable symbols = new SymbolTable();
-
-        final IR ir = new IR();
-
-        try
-        {
-            final ANTLRReaderStream input = new ANTLRReaderStream(new FileReader(fileName));
-            input.name = fileName;
-
-            final MmuLexer lexer = new MmuLexer(input);
-            final CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-            final MmuParser parser = new MmuParser(tokens);
-
-            parser.assignLog(LOG);
-            parser.assignSymbols(symbols);
-            parser.commonParser.assignLog(LOG);
-            parser.commonParser.assignSymbols(symbols);
-
-            final MmuParser.startRule_return r = parser.startRule();
-            final CommonTree t = (CommonTree) r.getTree();
-
-            System.out.println("AST: " + t.toStringTree());
-            
-            if (!parser.isCorrect())
-            {
-                System.err.println("TRANSLATION WAS INTERRUPTED DUE TO SYNTACTIC ERRORS.");
-                return;
-            }
-
-            final CommonTreeNodeStream nodes = new CommonTreeNodeStream(t);
-            nodes.setTokenStream(tokens);
-
-            final MmuTreeWalker walker = new MmuTreeWalker(nodes);
-            walker.assignLog(LOG);
-            walker.assignSymbols(symbols);
-            walker.assignIR(ir);
-
-            walker.startRule();
-            
-            if (!walker.isCorrect())
-            {
-                System.err.println("TRANSLATION WAS INTERRUPTED DUE TO SEMANTIC ERRORS.");
-                return;
-            }
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        
-//        final Generator generator = new Generator(modelName, getShortFileName(fileName), ir);
-//        generator.generate();
-        
-        
-    }
+    // final Generator generator = new Generator(modelName, getShortFileName(fileName), ir);
+    // generator.generate();
+  }
 }
