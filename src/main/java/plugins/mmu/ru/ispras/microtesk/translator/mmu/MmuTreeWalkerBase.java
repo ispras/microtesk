d3f0ebb767e480a59c9e254b21ca21cdca99ba88
Andrei Tatarnikov <andrewt@ispras.ru>
2015-02-03 12:02:10 +0000

    Added new factory methods to MuuTreeWalkerBase (to create addresses and segments).

--- src/main/java/plugins/mmu/ru/ispras/microtesk/translator/mmu/MmuTreeWalkerBase.java
+++ src/main/java/plugins/mmu/ru/ispras/microtesk/translator/mmu/MmuTreeWalkerBase.java
@@ -24,16 +24,21 @@ import org.antlr.runtime.tree.TreeNodeStream;
 import ru.ispras.fortress.data.Data;
 import ru.ispras.fortress.data.DataType;
 import ru.ispras.fortress.data.DataTypeId;
+import ru.ispras.fortress.data.types.bitvector.BitVector;
 import ru.ispras.fortress.expression.Node;
 import ru.ispras.fortress.expression.NodeOperation;
 import ru.ispras.fortress.expression.NodeValue;
 import ru.ispras.fortress.expression.StandardOperation;
 import ru.ispras.fortress.transformer.ReduceOptions;
 import ru.ispras.fortress.transformer.Transformer;
+import ru.ispras.fortress.util.InvariantChecks;
 
+import ru.ispras.microtesk.translator.antlrex.SemanticException;
 import ru.ispras.microtesk.translator.antlrex.TreeParserBase;
 import ru.ispras.microtesk.translator.antlrex.Where;
+import ru.ispras.microtesk.translator.mmu.ir.Address;
 import ru.ispras.microtesk.translator.mmu.ir.Ir;
+import ru.ispras.microtesk.translator.mmu.ir.Segment;
 
 public class MmuTreeWalkerBase extends TreeParserBase {
   private static final String ERR_NO_OPERATOR = 
@@ -58,22 +63,112 @@ public class MmuTreeWalkerBase extends TreeParserBase {
   }
 
   /**
-   * Creates a new operations expression.
+   * Creates an Address IR object and adds it to the MMU IR.
    * 
-   * <ol>
-   * <li>Find Fortress operator</li>
+   * @param addressId Address identifier.
+   * @param widthExpr Address width expression.
+   * @return New Address IR object.
+   * 
+   * @throws NullPointerException if any of the arguments is {@code null}.
+   * @throws SemanticException If the width expression is not a constant positive integer value. 
+   */
+
+  public Address newAddress(CommonTree addressId, Node widthExpr) throws SemanticException {
+    InvariantChecks.checkNotNull(addressId);
+
+    final Where w = where(addressId);
+    final BigInteger widthValue = extractInteger(widthExpr, w, "Address width");
+
+    final int value = widthValue.intValue();
+    if (value <= 0) {
+      raiseError(w, String.format("Illegal address width: %d", value));
+    }
+
+    final Address address = new Address(addressId.getText(), value);
+    ir.addAddress(address);
+    
+    return address;
+  }
+
+  /**
+   * Creates a segment IR object and adds it to the MMU IR.
+   * 
+   * @param segmentId Segment identifier.
+   * @param addressArgId Address argument identifier. 
+   * @param addressArgType Address argument type (identifier).
+   * @param rangeStartExpr Range start expression.
+   * @param rangeEndExpr Range and expression.
+   * @return New Segment IR object.
+   * 
+   * @throws NullPointerException if any of the arguments is {@code null}.
+   * @throws SemanticException if the specified address type is not defined;
+   * if the range expressions are not constant integer values; if the range start
+   * value is greater than the range end value.
+   */
+
+  public Segment newSegment(
+      CommonTree segmentId,
+      CommonTree addressArgId,
+      CommonTree addressArgType,
+      Node rangeStartExpr,
+      Node rangeEndExpr) throws SemanticException {
+
+    InvariantChecks.checkNotNull(segmentId);
+    InvariantChecks.checkNotNull(addressArgId);
+    InvariantChecks.checkNotNull(addressArgType);
+
+    final Where w = where(segmentId);
+    final String addressId = addressArgType.getText();
+
+    final Address address = ir.getAddresses().get(addressId);
+    if (null == address) {
+      raiseError(w, String.format("%s is not defined or is not an address.", addressId));
+    }
+
+    final BigInteger rangeStart = extractInteger(rangeStartExpr, w, "Range start");
+    final BigInteger rangeEnd = extractInteger(rangeEndExpr, w, "Range end");
+    
+    if (rangeStart.compareTo(rangeEnd) > 0) {
+      raiseError(w, String.format(
+          "Range start (%d) is greater than range end (%d).", rangeStart, rangeEnd));
+    }
+
+    final BitVector rangeStartBV = BitVector.valueOf(rangeStart, address.getWidth());
+    final BitVector rangeEndBV = BitVector.valueOf(rangeEnd, address.getWidth());
+
+    System.out.println(rangeStart.toString(16) + " - " + rangeEnd.toString(16));
+    System.out.println(rangeStartBV.toHexString() + " - " + rangeEndBV.toHexString());
+    System.out.println(rangeStartBV.bigIntegerValue().toString(16) + " - " + rangeEndBV.bigIntegerValue().toString(16));
+
+    final Segment segment = new Segment(
+        segmentId.getText(),
+        addressArgId.getText(),
+        address,
+        BitVector.valueOf(rangeStart, address.getWidth()),
+        BitVector.valueOf(rangeEnd, address.getWidth())
+        );
+
+    ir.addSegment(segment);
+    return segment;
+  }
+
+  /**
+   * Creates a new operator-based expression. Works in the following steps:
+   * 
+   * <ol><li>Find Fortress operator</li>
    * <li>Reduce all operands</li>
    * <li>Cast all value operands to common type (bit vector) if required</li>
-   * <li>Make NodeOperation and return</li>
-   * </ol>
+   * <li>Make NodeOperation and return</li></ol>
    * 
-   * @param operatorId
-   * @param operands
+   * @param operatorId Operator identifier.
+   * @param operands Array of operands. 
    * @return
    * @throws RecognitionException
    */
 
   public Node newExpression(CommonTree operatorId, Node ... operands) throws RecognitionException {
+    InvariantChecks.checkNotNull(operatorId);
+
     final Operator op = Operator.fromText(operatorId.getText());
     final Where w = where(operatorId);
     
@@ -113,5 +208,28 @@ public class MmuTreeWalkerBase extends TreeParserBase {
     }
 
     return new NodeOperation(fortressOp, reducedOperands);
+  }
+
+  public Node newVariable(String id) {
+    return null;
+  }
+
+  public Node newIndexedVariable(String id, Node index) {
+    return null;
+  }
+  
+  public Node newAttributeCall(String id, String attributeId) {
+    return null;
+  }
+  
+  private BigInteger extractInteger(Node node, Where where, String what) throws SemanticException {
+    InvariantChecks.checkNotNull(node);
+
+    if (node.getKind() != Node.Kind.VALUE || !node.isType(DataTypeId.LOGIC_INTEGER)) {
+      raiseError(where, String.format("%s is not a constant integer expression.", what)); 
+    }
+
+    final NodeValue nodeValue = (NodeValue) node;
+    return nodeValue.getInteger();
   }
 }
