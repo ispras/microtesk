dedf2947dfd65c5f385ab5478e38fc7a7f223749
Andrei Tatarnikov <andrewt@ispras.ru>
2015-02-19 11:02:57 +0000

    BlockBuilder was updated.

--- src/main/java/core/ru/ispras/microtesk/test/template/BlockBuilder.java
+++ src/main/java/core/ru/ispras/microtesk/test/template/BlockBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 ISP RAS (http://www.ispras.ru)
+ * Copyright 2014-2015 ISP RAS (http://www.ispras.ru)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -13,6 +13,8 @@
  */
 
 package ru.ispras.microtesk.test.template;
+
+import static ru.ispras.fortress.util.InvariantChecks.checkNotNull; 
 
 import java.util.ArrayList;
 import java.util.List;
@@ -80,16 +82,12 @@ public final class BlockBuilder {
   }
 
   public void addBlock(Block block) {
-    if (null == block) {
-      throw new NullPointerException();
-    }
+    checkNotNull(block);
     nestedBlocks.add(block);
   }
 
   public void addCall(Call call) {
-    if (null == call) {
-      throw new NullPointerException();
-    }
+    checkNotNull(call);
 
     if (call.isEmpty()) {
       return;
