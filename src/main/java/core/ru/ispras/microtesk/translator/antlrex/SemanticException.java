0ea1103ebe18f2013a1bf27373b1c5da1d2ad235
Andrei Tatarnikov <andrewt@ispras.ru>
2015-02-28 19:34:58 +0000

    JavaDoc for SemanticException.

--- src/main/java/core/ru/ispras/microtesk/translator/antlrex/SemanticException.java
+++ src/main/java/core/ru/ispras/microtesk/translator/antlrex/SemanticException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2014 ISP RAS (http://www.ispras.ru)
+ * Copyright 2013-2015 ISP RAS (http://www.ispras.ru)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -19,6 +19,19 @@ import org.antlr.runtime.RecognitionException;
 
 import ru.ispras.microtesk.translator.antlrex.ISemanticError;
 import ru.ispras.microtesk.translator.antlrex.Where;
+
+/**
+ * The {@code SemanticException} exception is thrown by the translator (lexer, parser, tree walker
+ * or other its parts) if it detects a semantic error in the translated specification.
+ * <p>All semantic errors found in the specification must be reported as
+ * {@code SemanticException}. In situations when an error is caused by issues in code (coding
+ * mistakes, invariant violations, limitations) an unchecked exception inherited from
+ * {@link RuntimeException} must be thrown. 
+ * <p>The {@code SemanticException} exception is inherited from the {@link RecognitionException}
+ * ANTLR exception to allow handling them in the same way.
+ * 
+ * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
+ */
 
 public final class SemanticException extends RecognitionException {
   private static final long serialVersionUID = 209516770104977723L;
