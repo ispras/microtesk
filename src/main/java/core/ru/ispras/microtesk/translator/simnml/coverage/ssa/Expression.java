4703752aed26b1019799256dc6cb4d8f9dff1c94
Artem Kotsynyak <kotsynyak@ispras.ru>
2015-03-23 15:15:10 +0000

    [coverage] inverse arguments to BVCONCAT

--- src/main/java/core/ru/ispras/microtesk/translator/simnml/coverage/ssa/Expression.java
+++ src/main/java/core/ru/ispras/microtesk/translator/simnml/coverage/ssa/Expression.java
@@ -43,6 +43,11 @@ final class Expression {
   }
 
   public static NodeOperation CONCAT(Node... args) {
+    for (int i = 0; i < args.length / 2; ++i) {
+      final Node tmp = args[i];
+      args[i] = args[args.length - i - 1];
+      args[args.length - i - 1] = tmp;
+    }
     return new NodeOperation(StandardOperation.BVCONCAT, args);
   }
 
