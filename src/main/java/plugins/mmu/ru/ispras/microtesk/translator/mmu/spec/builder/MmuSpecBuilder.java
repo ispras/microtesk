b70226efd670a910fcc72e08254ac93a677ce5eb
Andrei Tatarnikov <andrewt@ispras.ru>
2015-04-06 14:41:21 +0000

    Working on MMU

--- src/main/java/plugins/mmu/ru/ispras/microtesk/translator/mmu/spec/builder/MmuSpecBuilder.java
+++ src/main/java/plugins/mmu/ru/ispras/microtesk/translator/mmu/spec/builder/MmuSpecBuilder.java
@@ -15,11 +15,19 @@
 package ru.ispras.microtesk.translator.mmu.spec.builder;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
+import ru.ispras.fortress.expression.ExprUtils;
+import ru.ispras.fortress.expression.Node;
 import ru.ispras.microtesk.translator.TranslatorHandler;
 import ru.ispras.microtesk.translator.mmu.ir.Address;
+import ru.ispras.microtesk.translator.mmu.ir.Buffer;
+import ru.ispras.microtesk.translator.mmu.ir.Field;
 import ru.ispras.microtesk.translator.mmu.ir.Ir;
 import ru.ispras.microtesk.translator.mmu.spec.MmuAddress;
+import ru.ispras.microtesk.translator.mmu.spec.MmuDevice;
+import ru.ispras.microtesk.translator.mmu.spec.MmuExpression;
 import ru.ispras.microtesk.translator.mmu.spec.MmuSpecification;
 import ru.ispras.microtesk.translator.mmu.spec.basis.IntegerVariable;
 
@@ -31,24 +39,62 @@ import ru.ispras.microtesk.translator.mmu.spec.basis.IntegerVariable;
 
 public class MmuSpecBuilder implements TranslatorHandler<Ir> {
   private MmuSpecification spec; 
+  private Map<String, MmuAddress> addresses;
 
   @Override
   public void processIr(Ir ir) {
     System.out.println(ir);
-
+    
     this.spec = new MmuSpecification();
+    this.addresses = new HashMap<>();
 
     registerAddresses(ir.getAddresses().values());
+    registerDevices(ir.getBuffers().values());
 
     System.out.println(spec);
-    
   }
-  
-  private void registerAddresses(Collection<Address> addresses) {
-    for (Address address : addresses) {
-      spec.registerAddress(new MmuAddress(
-          new IntegerVariable(address.getId(), address.getBitSize())));
+
+  private void registerAddresses(Collection<Address> values) {
+    for (Address a : values) {
+      final IntegerVariable variable = new IntegerVariable(a.getId(), a.getBitSize());
+      final MmuAddress address = new MmuAddress(variable);
+
+      spec.registerAddress(address);
+      addresses.put(a.getId(), address);
     }
   }
-}
 
+  private void registerDevices(Collection<Buffer> buffers) {
+    for (Buffer buffer : buffers) {
+      final MmuAddress address = addresses.get(buffer.getAddress().getId());
+
+      final MmuDevice device = new MmuDevice(
+          buffer.getId(),
+          buffer.getWays(),
+          buffer.getSets(),
+          address,
+          getTagExpr(buffer.getMatch()),
+          getIndexExpr(buffer.getIndex()),
+          MmuExpression.ZERO(), // TODO offsetExpression,
+          true /*TODO: replaceable - ??? */);
+
+      for(Field field : buffer.getEntry().getFields()) {
+        device.addField(new IntegerVariable(field.getId(), field.getBitSize()));
+      }
+
+      spec.registerDevice(device);
+    }
+  }
+
+  private MmuExpression getIndexExpr(Node index) {
+    // System.out.println(index);
+    // TODO Auto-generated method stub
+    return MmuExpression.ZERO();
+  }
+
+  private MmuExpression getTagExpr(Node match) {
+    // System.out.println(match);
+    // TODO Auto-generated method stub
+    return MmuExpression.ZERO();
+  }
+}
