abd82662d9a4ab205e1134aea6e7385473f35f77
Andrei Tatarnikov <andrewt@ispras.ru>
2015-04-14 12:02:21 +0000

    Atom was added.

---
+++ src/main/java/plugins/mmu/ru/ispras/microtesk/translator/mmu/spec/builder/Atom.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2015 ISP RAS (http://www.ispras.ru)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.microtesk.translator.mmu.spec.builder;
+
+import static ru.ispras.fortress.util.InvariantChecks.checkNotNull;
+
+import java.math.BigInteger;
+
+import ru.ispras.microtesk.translator.mmu.spec.MmuExpression;
+import ru.ispras.microtesk.translator.mmu.spec.basis.IntegerField;
+import ru.ispras.microtesk.translator.mmu.spec.basis.IntegerVariable;
+
+public final class Atom {
+  public static enum AtomKind {
+    VALUE    (BigInteger.class),
+    VARIABLE (IntegerVariable.class),
+    FIELD    (IntegerField.class),
+    CONCAT   (MmuExpression.class);
+
+    private final Class<?> objectClass;
+    private AtomKind(Class<?> objectClass) {
+      this.objectClass = objectClass;
+    }
+
+    public Class<?> getObjectClass() {
+      return objectClass;
+    }
+  }
+
+  private final AtomKind kind;
+  private final Object object;
+
+  public Atom(AtomKind kind, Object object) {
+    checkNotNull(kind);
+    checkNotNull(object);
+
+    if (!object.getClass().equals(kind.getObjectClass())) {
+      throw new IllegalArgumentException(
+          kind.getObjectClass().getSimpleName() + " is expected.");
+    }
+
+    this.kind = kind;
+    this.object = object;
+  }
+
+  public AtomKind getKind() {
+    return kind;
+  }
+
+  public Object getObject() {
+    return object;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s: %s", kind, object);
+  }
+}
