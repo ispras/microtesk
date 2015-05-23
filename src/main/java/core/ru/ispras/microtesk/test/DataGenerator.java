b244a31a53194d34efa53fb92e017784d1de8899
Andrei Tatarnikov <andrewt@ispras.ru>
2015-05-23 09:56:51 +0000

    small fix

--- src/main/java/core/ru/ispras/microtesk/test/DataGenerator.java
+++ src/main/java/core/ru/ispras/microtesk/test/DataGenerator.java
@@ -558,18 +558,16 @@ final class TestBaseQueryCreator {
 
           case IMM_UNKNOWN:
             if (!((UnknownValue) arg.getValue()).isValueSet()) {
-              queryBuilder.setBinding(argName,
-                new NodeVariable(new Variable(argName, DataType.INTEGER)));
+              queryBuilder.setBinding(argName, new NodeVariable(argName, DataType.INTEGER));
               unknownValues.put(argName, (UnknownValue) arg.getValue());
             } else {
               queryBuilder.setBinding(argName,
-                NodeValue.newInteger(((UnknownValue) arg.getValue()).getValue()));
+                  NodeValue.newInteger(((UnknownValue) arg.getValue()).getValue()));
             }
             break;
 
           case MODE:
-            queryBuilder.setBinding(argName,
-              new NodeVariable(new Variable(argName, DataType.UNKNOWN)));
+            queryBuilder.setBinding(argName, new NodeVariable(argName, DataType.UNKNOWN));
             modes.put(argName, (Primitive) arg.getValue());
             visit(argName, (Primitive) arg.getValue());
             break;
@@ -580,7 +578,7 @@ final class TestBaseQueryCreator {
 
           default:
             throw new IllegalArgumentException(String.format(
-              "Illegal kind of argument %s: %s.", argName, arg.getKind()));
+                "Illegal kind of argument %s: %s.", argName, arg.getKind()));
         }
       }
     }
