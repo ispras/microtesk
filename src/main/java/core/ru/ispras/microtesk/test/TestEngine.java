fdd517cf9d040e86756faa659e9be92cd2808fd4
Andrei Tatarnikov <andrewt@ispras.ru>
2015-06-30 08:46:40 +0000

    Bug #6125 - Concrete calls should be supplied with correct addresses

--- src/main/java/core/ru/ispras/microtesk/test/TestEngine.java
+++ src/main/java/core/ru/ispras/microtesk/test/TestEngine.java
@@ -471,8 +471,6 @@ public final class TestEngine {
               if (logPrinter != null) {
                 logPrinter.createNewFile();
               }
-
-              engineContext.setCodeAddress(0);
             } catch (IOException e) {
               Logger.error(e.getMessage());
             }
@@ -584,6 +582,8 @@ public final class TestEngine {
             }
 
             printer.close();
+
+            engineContext.setCodeAddress(0);
           }
         } // Concrete sequence iterator
 
