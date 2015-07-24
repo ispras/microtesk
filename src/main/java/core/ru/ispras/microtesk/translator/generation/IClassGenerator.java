46e9f037379517879fe2e2eccc8eae2fbfdcf4fb
Andrei Tatarnikov <andrewt@ispras.ru>
2015-07-24 10:59:02 +0000

    Small update.

--- src/main/java/core/ru/ispras/microtesk/translator/generation/IClassGenerator.java
+++ src/main/java/core/ru/ispras/microtesk/translator/generation/IClassGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2014 ISP RAS (http://www.ispras.ru)
+ * Copyright 2012-2015 ISP RAS (http://www.ispras.ru)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -19,16 +19,16 @@ import java.io.IOException;
 /**
  * The IClassGenerator interface is a base interface to be implemented by all class file generators.
  * 
- * @author Andrei Tatarnikov
+ * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
  */
 
 public interface IClassGenerator {
   /**
    * Runs generation of a class file.
    * 
-   * @throws Exception Raised if the generator failed to generate the needed file due to an I/O
-   *         problem.
+   * @throws IOException if the generator failed to generate the needed file due to
+   *         an I/O problem.
    */
 
-  public void generate() throws IOException;
+  void generate() throws IOException;
 }
