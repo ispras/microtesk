85d48343450dcacd32cf8d265581fc86a1a8d559
Andrei Tatarnikov <andrewt@ispras.ru>
2015-07-28 08:31:28 +0000

    Redundant import.

--- src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/ir/Type.java
+++ src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/ir/Type.java
@@ -17,7 +17,6 @@ package ru.ispras.microtesk.mmu.translator.ir;
 import static ru.ispras.fortress.util.InvariantChecks.checkNotNull;
 import static ru.ispras.fortress.util.InvariantChecks.checkTrue;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 
