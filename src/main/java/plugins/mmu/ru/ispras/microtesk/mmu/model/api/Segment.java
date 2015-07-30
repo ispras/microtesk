29343ac2e4b258587a287c45643d4a2d81e72e5a
Andrei Tatarnikov <andrewt@ispras.ru>
2015-07-30 12:12:02 +0000

    Added a new class.

---
+++ src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/model/api/Segment.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015 ISP RAS (http://www.ispras.ru)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.microtesk.mmu.model.api;
+
+import ru.ispras.fortress.data.types.bitvector.BitVector;
+import ru.ispras.fortress.util.InvariantChecks;
+
+public abstract class Segment<D extends Data, A extends Address> implements Buffer<D, A> {
+  private final BitVector start;
+  private final BitVector end;
+
+  public Segment(final BitVector start, final BitVector end) {
+    InvariantChecks.checkNotNull(start);
+    InvariantChecks.checkNotNull(end);
+
+    this.start = start;
+    this.end = end;
+  }
+
+  @Override
+  public boolean isHit(final A address) {
+    final BitVector value = address.getValue();
+    return value.compareTo(start) >= 0 && value.compareTo(end) <= 0;
+  }
+
+  @Override
+  public D getData(final A va) {
+    // NOT SUPPORTED
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public D setData(final A address, final D data) {
+    // NOT SUPPORTED
+    throw new UnsupportedOperationException();
+  }
+}
