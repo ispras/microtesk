c65ca28423bdd244bcc27d08f88291bbcc3f3f18
Andrei Tatarnikov <andrewt@ispras.ru>
2015-08-11 13:20:06 +0000

    Added isField method

--- src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/ir/Variable.java
+++ src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/ir/Variable.java
@@ -27,36 +27,47 @@ import ru.ispras.fortress.expression.NodeVariable;
 public final class Variable extends Nested<Variable> {
   private final String name;
   private final Type type;
+  private final Variable parent;
   private final Object typeSource;
   private final NodeVariable node;
   private final Map<String, Variable> fields;
 
   public Variable(final String name, final Type type) {
-    this(name, type, null);
+    this(name, type, null, null);
   }
 
   public Variable(final String name, final Type type, final Object typeSource) {
+    this(name, type, null, typeSource);
+  }
+
+  private Variable(
+      final String name,
+      final Type type,
+      final Variable parent,
+      final Object typeSource) {
     checkNotNull(name);
     checkNotNull(type);
 
     this.name = name;
     this.type = type;
+    this.parent = parent;
     this.typeSource = typeSource;
 
     if (type.isStruct()) {
       final Map<String, Variable> fields = new HashMap<>(type.getFields().size());
       for (final Map.Entry<String, Type> fieldType : type.getFields().entrySet()) {
         final String varName = dotConc(name, fieldType.getKey());
-        fields.put(fieldType.getKey(),
-                   new Variable(varName, fieldType.getValue()));
+        fields.put(fieldType.getKey(), new Variable(varName, fieldType.getValue(), this, null));
       }
       this.fields = Collections.unmodifiableMap(fields);
     } else {
       this.fields = Collections.emptyMap();
     }
+
     this.node = new NodeVariable(name, type.getDataType());
     this.node.setUserData(this);
   }
+
 
   public String getName() {
     return name;
@@ -82,6 +93,10 @@ public final class Variable extends Nested<Variable> {
     return type.isStruct();
   }
 
+  public boolean isField() {
+    return null != parent;
+  }
+
   public NodeVariable getNode() {
     return node;
   }
@@ -104,94 +119,3 @@ public final class Variable extends Nested<Variable> {
     return String.format("%s: %s", name, type);
   }
 }
-
-/*
-public final class Variable {
-  private final String id;
-  private final Type type;
-
-  private final TypeProvider typeProvider; // Address, Buffer(entry) or null.
-  private final NodeVariable variable;
-
-  private final Map<String, NodeVariable> fieldVariables;
-
-  public Variable(String id, int bitSize) {
-    this(id, new Type(bitSize));
-  }
-
-  public Variable(String id, Type type) {
-    this(id, type, null);
-  }
-
-  public Variable(String id, TypeProvider typeProvider) {
-    this(id, null != typeProvider ? typeProvider.getType() : null, typeProvider);
-  }
-
-  private Variable(String id, Type type, TypeProvider typeProvider) {
-    checkNotNull(id);
-    checkNotNull(type);
-
-    if (type == Type.VOID) {
-      throw new IllegalArgumentException(Type.VOID + " is not allowed.");
-    }
-
-    this.id = id;
-
-    this.type = type;
-    this.typeProvider = typeProvider;
-
-    this.variable = new NodeVariable(id, type.getDataType());
-    this.variable.setUserData(this);
-
-    this.fieldVariables = (0 == type.getFieldCount()) ?
-        Collections.<String, NodeVariable>emptyMap() :
-        new HashMap<String, NodeVariable>();
-
-    for (Field field : type.getFields()) {
-      final String fieldVarId = String.format("%s.%s", id, field.getId());
-      final NodeVariable fieldVariable = new NodeVariable(fieldVarId, field.getDataType());
-
-      fieldVariable.setUserData(new FieldRef(this, field));
-      this.fieldVariables.put(field.getId(), fieldVariable);
-    }
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public Type getType() {
-    return type;
-  }
-
-  public String getTypeId() {
-    return (typeProvider != null) ? typeProvider.getTypeAlias() : null;
-  }
-
-  public DataType getDataType() {
-    return type.getDataType();
-  }
-
-  public int getBitSize() {
-    return type.getBitSize();
-  }
-
-  public NodeVariable getVariable() {
-    return variable;
-  }
-
-  public NodeVariable getVariableForField(String fieldId) {
-    return fieldVariables.get(fieldId);
-  }
-
-  public Variable rename(final String name) {
-    return new Variable(name, type, typeProvider);
-  }
-
-  @Override
-  public String toString() {
-    final String typeId = getTypeId();
-    return String.format("%s: %s%s", id, typeId != null ? typeId + "=" : "", type);
-  }
-}
-*/
