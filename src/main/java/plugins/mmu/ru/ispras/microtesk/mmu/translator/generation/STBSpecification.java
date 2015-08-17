00ccc6dd7832db280bddfc3502d7f2a7a11cbe35
Andrei Tatarnikov <andrewt@ispras.ru>
2015-08-17 12:25:36 +0000

    Working on MMU symbolic specification

--- src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/generation/STBSpecification.java
+++ src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/generation/STBSpecification.java
@@ -14,10 +14,13 @@
 
 package ru.ispras.microtesk.mmu.translator.generation;
 
+import java.util.List;
+
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroup;
 
 import ru.ispras.fortress.util.InvariantChecks;
+import ru.ispras.microtesk.mmu.translator.ir.Address;
 import ru.ispras.microtesk.mmu.translator.ir.Ir;
 import ru.ispras.microtesk.translator.generation.STBuilder;
 
@@ -26,6 +29,9 @@ public final class STBSpecification implements STBuilder {
 
   private static final Class<?> SPEC_CLASS =
       ru.ispras.microtesk.mmu.translator.ir.spec.MmuSubsystem.class;
+
+  private static final Class<?> INTEGER_CLASS = 
+      ru.ispras.microtesk.basis.solver.integer.IntegerVariable.class;
 
   private final String packageName;
   private final Ir ir;
@@ -49,14 +55,42 @@ public final class STBSpecification implements STBuilder {
   private void buildHeader(final ST st) {
     st.add("name", CLASS_NAME); 
     st.add("pack", packageName);
-
+    st.add("imps", String.format("%s.*", INTEGER_CLASS.getPackage().getName()));
     st.add("imps", String.format("%s.*", SPEC_CLASS.getPackage().getName()));
   }
 
   private void buildBody(final ST st, final STGroup group) {
     final ST stBody = group.getInstanceOf("body");
+
     stBody.add("name", CLASS_NAME);
     stBody.add("spec", SPEC_CLASS.getSimpleName());
     st.add("members", stBody);
+
+    buildAddresses(stBody, group);
+  }
+
+  private void buildAddresses(final ST st, final STGroup group) {
+    for(final Address address : ir.getAddresses().values()) {
+      final ST stDef = group.getInstanceOf("type_def");
+      stDef.add("name", address.getId());
+      stDef.add("var_name", listToString(address.getAccessChain()));
+      stDef.add("var_size", address.getAddressType().getBitSize());
+      st.add("members", stDef);
+
+      final ST stReg = group.getInstanceOf("type_reg");
+      stReg.add("name", address.getId());
+      st.add("stmts", stReg);
+    }
+  }
+
+  private static String listToString(final List<String> list) {
+    final StringBuilder sb = new StringBuilder();
+    for (final String string : list) {
+      if (sb.length() != 0) {
+        sb.append('.');
+      }
+      sb.append(string);
+    }
+    return sb.toString();
   }
 }
