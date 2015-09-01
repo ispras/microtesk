a70e8f3ecc0977c8395829de543cb2483d5f7585
Andrei Tatarnikov <andrewt@ispras.ru>
2015-09-01 12:43:38 +0000

    MMU Model API: added interface Data

---
+++ src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/model/api/Data.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2015 ISP RAS (http://www.ispras.ru)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.microtesk.mmu.model.api;
+
+import ru.ispras.fortress.data.types.bitvector.BitVector;
+
+/**
+ * The {@link Data} interface must be supported by classes describing
+ * complex data structures in order to be able to access data they store
+ * in a uniform way.
+ * 
+ * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
+ */
+
+public interface Data {
+  BitVector asBitVector();
+}
