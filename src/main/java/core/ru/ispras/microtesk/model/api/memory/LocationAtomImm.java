0c0177c22434abeace66391fede94bed0dcfd200
Andrei Tatarnikov <andrewt@ispras.ru>
2015-09-02 14:20:19 +0000

    Small renaming

--- src/main/java/core/ru/ispras/microtesk/model/api/memory/LocationAtomImm.java
+++ src/main/java/core/ru/ispras/microtesk/model/api/memory/LocationAtomImm.java
@@ -18,19 +18,19 @@ import ru.ispras.fortress.data.types.bitvector.BitVector;
 import ru.ispras.fortress.util.InvariantChecks;
 import ru.ispras.microtesk.model.api.memory.Location.Atom;
 
-final class LocationAtomConst implements Location.Atom {
+final class LocationAtomImm implements Location.Atom {
   private final BitVector value;
   private final int bitSize;
   private final int startBitPos;
 
-  protected LocationAtomConst(final BitVector value) {
+  protected LocationAtomImm(final BitVector value) {
     InvariantChecks.checkNotNull(value);
     this.value = value;
     this.bitSize = value.getBitSize();
     this.startBitPos = 0;
   }
 
-  private LocationAtomConst(
+  private LocationAtomImm(
       final BitVector value,
       final int bitSize,
       final int startBitPos) {
@@ -64,7 +64,7 @@ final class LocationAtomConst implements Location.Atom {
 
   @Override
   public Atom resize(final int newBitSize, final int newStartBitPos) {
-    return new LocationAtomConst(value, newBitSize, newStartBitPos);
+    return new LocationAtomImm(value, newBitSize, newStartBitPos);
   }
 
   @Override
