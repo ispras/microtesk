78a6fbfd63393cd5969a00eb9cadfe54bbcd01a2
Andrei Tatarnikov <andrewt@ispras.ru>
2015-09-04 16:33:46 +0000

    Basic miniMIPS MMU test template + deleted aligment checks from spec

---
+++ src/main/arch/minimips/templates/memory_subsystem.rb
@@ -0,0 +1,51 @@
+#
+# Copyright 2015 ISP RAS (http://www.ispras.ru)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_relative 'minimips_base'
+
+#
+# Description:
+#
+# This test template demonstrates how to generate test cases for the memory subsystem.
+#
+class MemorySubsystemTemplate < MiniMipsBaseTemplate
+  
+  def pre
+    super
+
+    buffer_preparator(:target => 'L1') {
+      la t0, address
+      lw t1, 0, t0
+    }
+
+    buffer_preparator(:target => 'L2') {
+      la t0, address
+      lw t1, 0, t0
+    }
+
+    buffer_preparator(:target => 'Memory') {
+      # Nothing
+    }
+  end
+
+  def run
+    atomic(:engine => 'memory', :classifier => 'event-based', :page_mask => 0x0fff) {
+      lw s0, 0, t0 do situation('address', :base => 'lw.address') end
+      lw s1, 0, t1 do situation('address', :base => 'lw.address') end
+    }
+  end
+
+end
