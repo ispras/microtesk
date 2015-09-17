f4f758182e8656ebf7d84987958fc1e921641e0e
Andrei Tatarnikov <andrewt@ispras.ru>
2015-09-17 11:54:41 +0000

    Fixed Bug #6278 - java.lang.ExceptionInInitializerError: null

--- src/main/java/core/ru/ispras/microtesk/model/api/memory/PhysicalMemory.java
+++ src/main/java/core/ru/ispras/microtesk/model/api/memory/PhysicalMemory.java
@@ -25,6 +25,8 @@ import ru.ispras.microtesk.test.TestSettings;
 final class PhysicalMemory extends Memory {
   private final MemoryDevice storage;
   private MemoryDevice handler;
+
+  private final boolean isLogical;
   private AddressTranslator addressTranslator;
   private final BigInteger addressableUnitsInData;
 
@@ -38,8 +40,22 @@ final class PhysicalMemory extends Memory {
     this.handler = null;
     this.addressTranslator = null;
 
-    InvariantChecks.checkTrue(storage.getDataBitSize() % 8 == 0);
-    this.addressableUnitsInData = BigInteger.valueOf(storage.getDataBitSize() / 8);
+    // A memory array that corresponds to a real physical memory must satisfy the following
+    // precondition: (1) element size is a multiple of 8 bits (byte), (2) element count is
+    // greater than one. This is required for MMU handlers and for simple translation logic
+    // to work correctly.
+    //
+    // There are situations when memory is used as a variable (logical memory) and MMU-related
+    // logic is not used. When the preconditions are violated, memory is considered logical.
+    //
+    if (storage.getDataBitSize() % 8 == 0 && length.compareTo(BigInteger.ONE) >= 0) {
+      this.addressableUnitsInData = BigInteger.valueOf(storage.getDataBitSize() / 8);
+      this.isLogical = false;
+    }
+    else {
+      this.addressableUnitsInData = null;
+      this.isLogical = true;
+    }
   }
 
   public MemoryDevice getStorage() {
@@ -88,11 +104,12 @@ final class PhysicalMemory extends Memory {
     storage.useTemporaryContext(value);
   }
 
-  private Location newLocationForRegion(final BitVector address) {
-    InvariantChecks.checkNotNull(address);
+  private Location newLocationForRegion(final BitVector index) {
+    InvariantChecks.checkNotNull(index);
 
-    final PhysicalMemoryAtom atom =
-        new PhysicalMemoryAtom(address, getType().getBitSize(), 0);
+    final Location.Atom atom = isLogical ?
+        new LogicalMemoryAtom(index, getType().getBitSize(), 0) :
+        new PhysicalMemoryAtom(index, getType().getBitSize(), 0);
 
     return new Location(getType(), atom);
   }
@@ -106,30 +123,6 @@ final class PhysicalMemory extends Memory {
     }
 
     return addressTranslator;
-  }
-
-  private BigInteger indexToAddress(final BigInteger index) {
-    if (addressableUnitsInData.equals(BigInteger.ONE)) {
-      return index;
-    }
-
-    return index.multiply(addressableUnitsInData);
-  }
-
-  private BigInteger addressToIndex(final BigInteger address) {
-    if (addressableUnitsInData.equals(BigInteger.ONE)) {
-      return address;
-    }
-
-    return address.divide(addressableUnitsInData);
-  }
-
-  private BitVector virtualIndexToPhysicalIndex(final BitVector index) {
-    final BigInteger virtualAddress = indexToAddress(index.bigIntegerValue(false));
-    final BigInteger physicalAddress = getAddressTranslator().virtualToPhysical(virtualAddress);
-
-    final BigInteger physicalIndex = addressToIndex(physicalAddress);
-    return BitVector.valueOf(physicalIndex, storage.getAddressBitSize());
   }
 
   private final class PhysicalMemoryAtom implements Location.Atom {
@@ -220,6 +213,99 @@ final class PhysicalMemory extends Memory {
       }
 
       targetDevice.store(targetAddress, region);
+    }
+
+    @Override
+    public String toString() {
+      return String.format("%s[%d]<%d..%d>",
+          getName(),
+          index.bigIntegerValue(false),
+          startBitPos,
+          startBitPos + bitSize - 1
+          );
+    }
+
+    private BigInteger indexToAddress(final BigInteger index) {
+      if (addressableUnitsInData.equals(BigInteger.ONE)) {
+        return index;
+      }
+
+      return index.multiply(addressableUnitsInData);
+    }
+
+    private BigInteger addressToIndex(final BigInteger address) {
+      if (addressableUnitsInData.equals(BigInteger.ONE)) {
+        return address;
+      }
+
+      return address.divide(addressableUnitsInData);
+    }
+
+    private BitVector virtualIndexToPhysicalIndex(final BitVector index) {
+      final BigInteger virtualAddress = indexToAddress(index.bigIntegerValue(false));
+      final BigInteger physicalAddress = getAddressTranslator().virtualToPhysical(virtualAddress);
+
+      final BigInteger physicalIndex = addressToIndex(physicalAddress);
+      return BitVector.valueOf(physicalIndex, storage.getAddressBitSize());
+    }
+  }
+
+  private final class LogicalMemoryAtom implements Location.Atom {
+    private final BitVector index;
+    private final int bitSize;
+    private final int startBitPos;
+
+    private LogicalMemoryAtom(
+        final BitVector index,
+        final int bitSize,
+        final int startBitPos) {
+      this.index = index;
+      this.bitSize = bitSize;
+      this.startBitPos = startBitPos;
+    }
+
+    @Override
+    public boolean isInitialized() {
+      return storage.isInitialized(index);
+    }
+
+    @Override
+    public LogicalMemoryAtom resize(
+        final int newBitSize,
+        final int newStartBitPos) {
+      return new LogicalMemoryAtom(index, newBitSize, newStartBitPos);
+    }
+
+    @Override
+    public int getBitSize() {
+      return bitSize;
+    }
+
+    @Override
+    public int getStartBitPos() {
+      return startBitPos;
+    }
+
+    @Override
+    public BitVector load(final boolean callHandler) {
+      final BitVector region = storage.load(index);
+      return BitVector.newMapping(region, startBitPos, bitSize);
+    }
+
+    @Override
+    public void store(final BitVector data, final boolean callHandler) {
+      InvariantChecks.checkNotNull(data);
+
+      final BitVector region;
+      if (bitSize == storage.getDataBitSize()) {
+        region = data;
+      } else {
+        region = storage.load(index).copy();
+        final BitVector mapping = BitVector.newMapping(region, startBitPos, bitSize);
+        mapping.assign(data);
+      }
+
+      storage.store(index, region);
     }
 
     @Override
