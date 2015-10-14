ee3aa2d36ac7c4002ff51b8be294a7464172b2f5
Andrei Tatarnikov <andrewt@ispras.ru>
2015-10-14 09:34:36 +0000

    Killed redundant import

--- src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/MmuTreeWalkerBase.java
+++ src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/MmuTreeWalkerBase.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Collection;
-import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
