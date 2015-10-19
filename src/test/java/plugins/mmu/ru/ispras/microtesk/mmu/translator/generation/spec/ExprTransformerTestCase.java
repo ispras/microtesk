673c2aaae64da0ed6163e3ddfed4f7e484a29c0f
Andrei Tatarnikov <andrewt@ispras.ru>
2015-10-19 14:22:52 +0000

    New unit tests.

---
+++ src/test/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/generation/spec/ExprTransformerTestCase.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2015 ISP RAS (http://www.ispras.ru)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.microtesk.mmu.translator.generation.spec;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ru.ispras.fortress.data.DataType;
+import ru.ispras.fortress.data.types.bitvector.BitVector;
+import ru.ispras.fortress.expression.Node;
+import ru.ispras.fortress.expression.NodeOperation;
+import ru.ispras.fortress.expression.NodeValue;
+import ru.ispras.fortress.expression.NodeVariable;
+import ru.ispras.fortress.expression.StandardOperation;
+
+public class ExprTransformerTestCase {
+
+  @Test
+  public void testLeftShift() {
+    testLeftShift(StandardOperation.BVASHL, 32, 0);
+    testLeftShift(StandardOperation.BVLSHL, 32, 0);
+
+    testLeftShift(StandardOperation.BVASHL, 32, 1);
+    testLeftShift(StandardOperation.BVLSHL, 32, 1);
+
+    testLeftShift(StandardOperation.BVASHL, 32, 16);
+    testLeftShift(StandardOperation.BVLSHL, 32, 16);
+
+    testLeftShift(StandardOperation.BVASHL, 64, 16);
+    testLeftShift(StandardOperation.BVLSHL, 64, 16);
+
+    testLeftShift(StandardOperation.BVASHL, 32, 32);
+    testLeftShift(StandardOperation.BVLSHL, 32, 32);
+  }
+
+  private static void testLeftShift(
+      final StandardOperation operator,
+      final int bitSize,
+      final int shiftAmount) {
+
+    final Node x =
+        new NodeVariable("x", DataType.BIT_VECTOR(bitSize));
+
+    final Node initial = new NodeOperation(
+        operator,
+        x,
+        NodeValue.newInteger(shiftAmount)
+        );
+
+    final Node expected = shiftAmount % bitSize == 0 ?
+        x :
+        new NodeOperation(
+        StandardOperation.BVCONCAT,
+        newField(x, 0, bitSize - shiftAmount - 1),
+        NodeValue.newBitVector(BitVector.newEmpty(shiftAmount))
+        );
+
+    final Node result = transform(initial);
+
+    /*
+    System.out.println(initial);
+    System.out.println(result);
+    System.out.println(expected);
+    */
+
+    assertEquals(expected, result);
+  }
+
+  private static Node transform(final Node expr) {
+    final ExprTransformer transformer = new ExprTransformer();
+    return transformer.transform(expr);
+  }
+
+  private static Node newField(final Node expr, final int from, final int to) {
+    return new NodeOperation(
+        StandardOperation.BVEXTRACT,
+        NodeValue.newInteger(to),
+        NodeValue.newInteger(from),
+        expr
+        );
+  }
+}
