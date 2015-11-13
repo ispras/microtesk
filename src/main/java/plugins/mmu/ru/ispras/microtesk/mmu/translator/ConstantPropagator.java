0c692bba1330f17aff02ae8a788b0c6f9c734efa
Andrei Tatarnikov <andrewt@ispras.ru>
2015-11-13 12:53:32 +0000

    Killed unused import

--- src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/ConstantPropagator.java
+++ src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/ConstantPropagator.java
@@ -26,7 +26,6 @@ import java.util.Collections;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
