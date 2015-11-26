947b07428d24758358f0b78b999c55e012a766c8
Andrei Tatarnikov <andrewt@ispras.ru>
2015-11-26 20:40:27 +0000

    Test Template: added a new class Where.

---
+++ src/main/java/core/ru/ispras/microtesk/test/template/Where.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015 ISP RAS (http://www.ispras.ru)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.microtesk.test.template;
+
+import ru.ispras.fortress.util.InvariantChecks;
+
+/**
+ * The {@link Where} class holds information about a position in a test template.
+ * 
+ * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
+ */
+
+public final class Where {
+  private final String file;
+  private final int line;
+
+  public Where(final String file, final int line) {
+    InvariantChecks.checkNotNull(file);
+    InvariantChecks.checkGreaterThanZero(line);
+
+    this.file = file;
+    this.line = line;
+  }
+
+  public String getFile() {
+    return file;
+  }
+
+  public int getLine() {
+    return line;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s:%d", file, line);
+  }
+}
