6308b08cf484521a0764a90f7a74ad05b3d2d734
Andrei Tatarnikov <andrewt@ispras.ru>
2015-12-15 12:46:31 +0000

    Added a new miniMIPS template

---
+++ src/main/arch/minimips/templates/memory_situation.rb
@@ -0,0 +1,51 @@
+#
+# Copyright 2015 ISP RAS (http://www.ispras.ru)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_relative 'minimips_base'
+
+#
+# Description:
+#
+# This test template demonstrates how to use memory-related situation 'address'.
+#
+class MemorySituationTemplate < MiniMipsBaseTemplate
+
+  def pre
+    super
+
+    buffer_preparator(:target => 'L1') {
+      la t0, address
+      lw t1, 0, t0
+    }
+
+    buffer_preparator(:target => 'L2') {
+      la t0, address
+      lw t1, 0, t0
+    }
+
+    buffer_preparator(:target => 'M') {
+      # Nothing
+    }
+  end
+
+  def run
+    atomic(:engine => 'memory', :classifier => 'event-based', :page_mask => 0x0fff, :align => 4) {
+      lw s0, 0, t0 do situation(
+          'address', :base => 'lw.address', :path => constraints(miss('L1'), hit('L2'))) end
+    }.run
+  end
+
+end
