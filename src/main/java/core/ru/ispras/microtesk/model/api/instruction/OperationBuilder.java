4d2a7a2a79d717c2d7d4e7e9348e41f4baa294f3
Andrei Tatarnikov <andrewt@ispras.ru>
2015-12-29 07:14:20 +0000

    OperationBuilder - code improvements.

--- src/main/java/core/ru/ispras/microtesk/model/api/instruction/OperationBuilder.java
+++ src/main/java/core/ru/ispras/microtesk/model/api/instruction/OperationBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 ISP RAS (http://www.ispras.ru)
+ * Copyright 2014-2015 ISP RAS (http://www.ispras.ru)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -18,6 +18,7 @@ import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
+import ru.ispras.microtesk.Logger;
 import ru.ispras.microtesk.model.api.ArgumentKind;
 import ru.ispras.microtesk.model.api.data.Data;
 import ru.ispras.microtesk.model.api.exception.ConfigurationException;
@@ -33,29 +34,35 @@ public final class OperationBuilder implements IOperationBuilder {
   private final Map<String, Operation.Param> decls;
   private final Map<String, Object> args;
 
-  public OperationBuilder(String opName, IOperation.IFactory factory, Operation.ParamDecls decls) {
+  public OperationBuilder(
+      final String opName,
+      final IOperation.IFactory factory,
+      final Operation.ParamDecls decls) {
     this.opName = opName;
     this.factory = factory;
     this.decls = decls.getDecls();
-    this.args = new HashMap<String, Object>();
+    this.args = new HashMap<>();
   }
 
   @Override
-  public LocationAccessor setArgument(String name, BigInteger value) throws ConfigurationException {
+  public LocationAccessor setArgument(
+      final String name,
+      final BigInteger value) throws ConfigurationException {
     checkUndeclaredArgument(name);
     checkReassignment(name);
 
     final Operation.Param decl = decls.get(name);
     if (decl.getKind() != ArgumentKind.IMM) {
       throw new UndeclaredException(String.format(
-        "The %s argument of the %s operation must be an immediate value.", name, opName));
+          "The %s argument of the %s operation must be an immediate value.", name, opName));
     }
 
     final Data data = Data.valueOf(decl.getType(), value);
     if (Data.isLossOfSignificantBits(decl.getType(), value)) {
-      System.out.printf("Warning: The value of the %s argument (= %d) of the %s operation " +
-         "will be truncated to suit %s. This will cause loss of significant bits. Result: %d%n",
-         name, value, opName, decl.getType(), data.getRawData().intValue());
+      Logger.warning(
+          "The value of the %s argument (= %d) of the %s operation " +
+          "will be truncated to suit %s. This will cause loss of significant bits. Result: %d",
+          name, value, opName, decl.getType(), data.getRawData().intValue());
     }
 
     final Location arg = Location.newLocationForConst(data);
@@ -65,8 +72,9 @@ public final class OperationBuilder implements IOperationBuilder {
   }
 
   @Override
-  public IOperationBuilder setArgument(String name, IAddressingMode value)
-      throws ConfigurationException {
+  public IOperationBuilder setArgument(
+      final String name,
+      final IAddressingMode value) throws ConfigurationException {
     checkUndeclaredArgument(name);
     checkReassignment(name);
 
@@ -74,12 +82,12 @@ public final class OperationBuilder implements IOperationBuilder {
 
     if (decl.getKind() != ArgumentKind.MODE) {
       throw new UndeclaredException(String.format(
-        "The %s argument of the %s operation must be an addressing mode.", name, opName));
+          "The %s argument of the %s operation must be an addressing mode.", name, opName));
     }
 
     if (!decl.isSupported(value)) {
       throw new UndeclaredException(String.format(
-        "The %s argument of the %s operation has an incompatible type.", name, opName));
+          "The %s argument of the %s operation has an incompatible type.", name, opName));
     }
 
     args.put(name, value);
@@ -87,7 +95,9 @@ public final class OperationBuilder implements IOperationBuilder {
   }
 
   @Override
-  public IOperationBuilder setArgument(String name, IOperation value) throws ConfigurationException {
+  public IOperationBuilder setArgument(
+      final String name,
+      final IOperation value) throws ConfigurationException {
     checkUndeclaredArgument(name);
     checkReassignment(name);
 
@@ -95,12 +105,12 @@ public final class OperationBuilder implements IOperationBuilder {
 
     if (decl.getKind() != ArgumentKind.OP) {
       throw new UndeclaredException(String.format(
-        "The %s argument of the %s operation must be an operation.", name, opName));
+          "The %s argument of the %s operation must be an operation.", name, opName));
     }
 
     if (!decl.isSupported(value)) {
       throw new UndeclaredException(String.format(
-        "The %s argument of the %s operation has an incompatible type.", name, opName));
+          "The %s argument of the %s operation has an incompatible type.", name, opName));
     }
 
     args.put(name, value);
@@ -113,30 +123,26 @@ public final class OperationBuilder implements IOperationBuilder {
     return factory.create(args);
   }
 
-  private void checkUndeclaredArgument(String name) throws UndeclaredException {
-    final String ERROR_FORMAT = "The %s argument is not declared for the %s operation.";
-
+  private void checkUndeclaredArgument(final String name) throws UndeclaredException {
     if (!decls.containsKey(name)) {
-      throw new UndeclaredException(String.format(ERROR_FORMAT, name, opName));
+      throw new UndeclaredException(String.format(
+          "The %s argument is not declared for the %s operation.", name, opName));
     }
   }
 
-  private void checkReassignment(String name) throws ReassignmentException {
-    final String ERROR_FORMAT =
-      "The value of the %s argument has already been assigned for " +
-      "the current instance of the %s operation.";
-
+  private void checkReassignment(final String name) throws ReassignmentException {
     if (args.containsKey(name)) {
-      throw new ReassignmentException(String.format(ERROR_FORMAT, name, opName));
+      throw new ReassignmentException(String.format(
+          "The value of the %s argument has already been assigned for " +
+          "the current instance of the %s operation.", name, opName));
     }
   }
 
   private void checkInitialized() throws UninitializedException {
-    final String ERROR_FORMAT = "The % argument of the %s operation is not initialized.";
-
-    for (String name : decls.keySet()) {
+    for (final String name : decls.keySet()) {
       if (!args.containsKey(name)) {
-        throw new UninitializedException(String.format(ERROR_FORMAT, name, opName));
+        throw new UninitializedException(
+            String.format("The % argument of the %s operation is not initialized.", name, opName));
       }
     }
   }
