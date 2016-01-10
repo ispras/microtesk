9ad190b4e6f02c1b954c95ae378eacc7fcec1e2c
Andrei Tatarnikov <andrewt@ispras.ru>
2016-01-10 07:19:09 +0000

    Added interface MetaDataProvider

---
+++ src/main/java/core/ru/ispras/microtesk/model/api/metadata/MetaDataProvider.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2016 ISP RAS (http://www.ispras.ru)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.microtesk.model.api.metadata;
+
+/**
+ * The {@link MetaDataProvider} interface is to be implemented by objects storing
+ * meta data. It is needed to deal with them in a uniform way.
+ * 
+ * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
+ *
+ * @param <T> Type of a {@link MetaData} object.
+ */
+
+public interface MetaDataProvider<T extends MetaData> {
+  /**
+   * Returns a {@code MetaData} object.
+   * @return {@code MetaData} object.
+   */
+
+  T getMetaData();
+}
