50623b12627426e5a9bbceeca2fe3035c3118362
Andrei Tatarnikov <andrewt@ispras.ru>
2016-01-20 15:16:23 +0000

    Small update

--- src/main/java/core/ru/ispras/microtesk/translator/nml/ir/shared/Type.java
+++ src/main/java/core/ru/ispras/microtesk/translator/nml/ir/shared/Type.java
@@ -21,6 +21,7 @@ import ru.ispras.microtesk.model.api.data.TypeId;
 
 public final class Type {
   public final static Type BOOLEAN = new Type(TypeId.BOOL, 1);
+  public final static Type LOGIC_INT = new Type(TypeId.INT, 0);
 
   public static Type INT(final int bitSize) {
     InvariantChecks.checkGreaterThanZero(bitSize);
