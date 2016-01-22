4d356542119e3ca18b31770c5be7008317dc8af4
Andrei Tatarnikov <andrewt@ispras.ru>
2016-01-22 12:03:11 +0000

    miniMIPS: added a new example.

---
+++ src/main/arch/minimips/templates/permutator.rb
@@ -0,0 +1,48 @@
+#
+# Copyright 2016 ISP RAS (http://www.ispras.ru)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_relative 'minimips_base'
+
+#
+# Description:
+#
+# This test template demonstrates how to generate permuations of instructions.
+#
+class PermutatorTemplate < MiniMipsBaseTemplate
+
+  def run
+    # Ordered as is:
+    atomic(:permutator => 'trivial') {
+      Add reg(_), reg(_), reg(_)
+      Sub reg(_), reg(_), reg(_)
+      And reg(_), reg(_), reg(_)
+      Or  reg(_), reg(_), reg(_)
+      Nor reg(_), reg(_), reg(_)
+      Xor reg(_), reg(_), reg(_)
+    }.run
+
+    # Random ordering:
+    atomic(:permutator => 'random') {
+      Add reg(_), reg(_), reg(_)
+      Sub reg(_), reg(_), reg(_)
+      And reg(_), reg(_), reg(_)
+      Or  reg(_), reg(_), reg(_)
+      Nor reg(_), reg(_), reg(_)
+      Xor reg(_), reg(_), reg(_)
+    }.run
+  end
+
+end
