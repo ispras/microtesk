4ba3acfafd4f43108facdda981f3e0958af29f6a
Andrei Tatarnikov <andrewt@ispras.ru>
2016-02-19 11:33:12 +0000

    Test Templates: added LabelUniqualizer.

---
+++ src/main/java/core/ru/ispras/microtesk/test/template/LabelUniqualizer.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2016 ISP RAS (http://www.ispras.ru)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.microtesk.test.template;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import ru.ispras.fortress.util.InvariantChecks;
+import ru.ispras.fortress.util.Pair;
+import ru.ispras.microtesk.test.GenerationAbortedException;
+
+public final class LabelUniqualizer {
+  public static final class SeriesId {
+    private final int index;
+
+    private SeriesId(int index) {
+      this.index = index;
+    }
+  }
+
+  private static LabelUniqualizer instance = null;
+
+  private final List<Integer> numbers;
+  private final Deque<Pair<Integer, Set<String>>> labelScopes;
+
+  private LabelUniqualizer() {
+    this.numbers = new ArrayList<>();
+    this.labelScopes = new ArrayDeque<>();
+  }
+
+  public static LabelUniqualizer get() {
+    if (null == instance) {
+      instance = new LabelUniqualizer();
+    }
+    return instance;
+  }
+
+  public SeriesId newSeries() {
+    numbers.add(0);
+    return new SeriesId(numbers.size() + 1);
+  }
+
+  public void resetNumbers() {
+    InvariantChecks.checkTrue(labelScopes.isEmpty());
+
+    for (int index = 0; index < numbers.size(); index++) {
+      numbers.set(index, 0);
+    }
+  }
+
+  public void pushLabelScope(final SeriesId seriesId) {
+    InvariantChecks.checkNotNull(seriesId);
+
+    final int referenceNumber = numbers.get(seriesId.index);
+    numbers.set(seriesId.index, referenceNumber + 1);
+
+    labelScopes.push(
+        new Pair<>(referenceNumber, (Set<String>) new HashSet<String>()));
+  }
+
+  public void popLabelScope() {
+    labelScopes.pop();
+  }
+
+  public void makeLabelsUnique(final Call call) {
+    InvariantChecks.checkNotNull(call);
+
+    final int referenceNumber = labelScopes.peek().first;
+    final Set<String> labelNames = labelScopes.peek().second;
+
+    for (final Label label : call.getLabels()) {
+      label.setReferenceNumber(referenceNumber);
+      if (!labelNames.add(label.getName())) {
+        throw new GenerationAbortedException(String.format(
+            "The %s label is redefined.", label.getName()));
+      }
+    }
+
+    for (final LabelReference labelRef : call.getLabelReferences()) {
+      final Label label = labelRef.getReference();
+      if (labelNames.contains(label.getName())) {
+        label.setReferenceNumber(referenceNumber);
+      }
+    }
+  }
+}
