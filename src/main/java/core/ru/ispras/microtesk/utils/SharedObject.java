9aeab5b26ee2d45d77853c637f6cfef0286e1df2
Andrei Tatarnikov <andrewt@ispras.ru>
2016-02-20 08:25:53 +0000

    SharedObject was updated.

--- src/main/java/core/ru/ispras/microtesk/utils/SharedObject.java
+++ src/main/java/core/ru/ispras/microtesk/utils/SharedObject.java
@@ -24,16 +24,12 @@ import ru.ispras.fortress.util.InvariantChecks;
  * When these objects are copied, the shared object must be copied only
  * once and all objects must use a reference to the same new copy.
  *
- * <p>The protocol is implemented in the following way: an object can
- * be copied only once. When it is copied, it saves a reference to the
- * created copy. When objects that refer to the shared object are copied,
- * they use the reference to the new object.
- * 
- * <p>A copy is created by the owner using the copy constructor. Other clients
- * must use the {@code sharedCopy} methods to receive the reference to a new
- * copy. This requires the owned to be copied first. Alternatively, clients
- * can use the {@code isCopied} method to check whether the shared object has
- * been copied to choose which way to get a copy to use.
+ * <p>The protocol is implemented in the following way: The owned of the
+ * shared object creates a new copy using a copy constructor. A reference
+ * to the new copy is saved in the copied object. Other clients must use
+ * the {@code sharedCopy} method to get the reference to the new copy.
+ * It is important what all clients get the shared copy before a newer
+ * copy is created otherwise they will refer to different instances.
  * 
  * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
  *
@@ -65,20 +61,18 @@ public abstract class SharedObject<T extends SharedObject<T>> {
    * Constructs a copy of a shared object.
    * 
    * <p> This constructor must be called by the owner to make a new
-   * copy of the object. An object can be copied only once. The created
-   * copy will be shared among the objects that refer to the current
-   * object to update the reference when they are copied.
+   * copy of the object. The created copy will be shared among the objects
+   * that refer to the current object to update the reference when they are
+   * copied.
    * 
    * @param other Object to be copied.
    * 
-   * @throws IllegalArgumentException if the argument is {@code null} and
-   *         if this object has already been copied.
+   * @throws IllegalArgumentException if the argument is {@code null}.
    */
 
   @SuppressWarnings("unchecked")
   protected SharedObject(final SharedObject<T> other) {
     InvariantChecks.checkNotNull(other);
-    InvariantChecks.checkTrue(null == other.copy, "An object can be copied only once.");
 
     this.copy = null;
     other.copy = (T) this;
@@ -99,15 +93,5 @@ public abstract class SharedObject<T extends SharedObject<T>> {
   public final T sharedCopy() {
     InvariantChecks.checkNotNull(copy, "Shared copy is unavailable.");
     return copy;
-  }
-
-  /**
-   * Checks whether the object has been copied.
-   * 
-   * @return {@code true} if the object has been copied or {@code false} otherwise.
-   */
-
-  public final boolean isCopied() {
-    return null != copy;
   }
 }
