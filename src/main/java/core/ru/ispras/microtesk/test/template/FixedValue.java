ea14a1f4fcdf2a17bd3d4c8a6982d7eab61375fd
Andrei Tatarnikov <andrewt@ispras.ru>
2016-02-24 12:04:47 +0000

    FixedValue.copy()

--- src/main/java/core/ru/ispras/microtesk/test/template/FixedValue.java
+++ src/main/java/core/ru/ispras/microtesk/test/template/FixedValue.java
@@ -27,6 +27,11 @@ public final class FixedValue implements Value {
   }
 
   @Override
+  public Value copy() {
+    return this;
+  }
+
+  @Override
   public BigInteger getValue() {
     return value;
   }
