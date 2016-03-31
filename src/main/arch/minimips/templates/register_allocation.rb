07908866043447a64e514d3cfb5d0b26f55a3f1d
Andrei Tatarnikov <andrewt@ispras.ru>
2016-03-31 09:46:59 +0000

    Added a new example.

---
+++ src/main/arch/minimips/templates/register_allocation.rb
@@ -0,0 +1,54 @@
+#
+# Copyright 2016 ISP RAS (http://www.ispras.ru)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_relative 'minimips_base'
+
+#
+# Description:
+#
+# This test template demonstrates how to randomly allocate registers so
+# that do not conflict with other registers used in a test case. 
+#
+class RegisterAllocationTemplate < MiniMipsBaseTemplate
+
+  def pre
+    super
+
+    #
+    # Test case level epilogue to return from an exception.
+    #
+    epilogue {
+      nop
+    }
+
+    #
+    # Start address
+    #
+    org 0x00020000
+  end
+
+  def run
+    # Destination of all instructions is a random register that
+    # is not used in this sequence.
+
+    sequence {
+      add get_register, t1, t2
+      sub get_register, t3, t4
+      slt get_register, t5, t6
+      xor get_register, t7, t8
+    }.run 3
+  end
+end
