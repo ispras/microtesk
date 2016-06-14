88912a7b2b762cdf471f547b8715d1a178ad3a71
Andrei Tatarnikov <andrewt@ispras.ru>
2016-06-14 16:20:24 +0000

    IrWalkerShortcuts was updated.

--- src/main/java/core/ru/ispras/microtesk/translator/nml/ir/IrWalkerShortcuts.java
+++ src/main/java/core/ru/ispras/microtesk/translator/nml/ir/IrWalkerShortcuts.java
@@ -14,14 +14,16 @@
 
 package ru.ispras.microtesk.translator.nml.ir;
 
+import java.util.Collection;
 import java.util.Map;
 
 import ru.ispras.fortress.util.InvariantChecks;
+import ru.ispras.fortress.util.TreeVisitor.Status;
 import ru.ispras.microtesk.translator.nml.ir.primitive.Primitive;
 import ru.ispras.microtesk.translator.nml.ir.primitive.PrimitiveAND;
 import ru.ispras.microtesk.translator.nml.ir.primitive.Shortcut;
 
-public class IrWalkerShortcuts {
+public final class IrWalkerShortcuts {
   private final Ir ir;
   private final IrVisitor visitor;
 
@@ -33,12 +35,38 @@ public class IrWalkerShortcuts {
     this.visitor = visitor;
   }
 
+  private boolean isStatus(final Status status) {
+    return visitor.getStatus() == status;
+  }
+
   public void visit() {
+    visitor.onBegin();
+    if (isStatus(Status.ABORT)) {
+      return;
+    }
+
+    if (isStatus(Status.OK)) {
+      visitPrimitives(ir.getOps().values());
+      if (isStatus(Status.ABORT)) {
+        return;
+      }
+    }
+
+    visitor.onEnd();
+  }
+
+  private void visitPrimitives(final Collection<Primitive> primitives) {
     visitor.onPrimitivesBegin();
+    if (isStatus(Status.ABORT)) {
+      return;
+    }
 
     for (final Primitive item : ir.getOps().values()) {
-      if (!item.isOrRule()) {
+      if (isStatus(Status.OK) && !item.isOrRule()) {
         visitPrimitive((PrimitiveAND) item);
+        if (isStatus(Status.ABORT)) {
+          return;
+        }
       }
     }
 
@@ -47,9 +75,17 @@ public class IrWalkerShortcuts {
 
   private void visitPrimitive(final PrimitiveAND primitive) {
     visitor.onPrimitiveBegin(primitive);
+    if (isStatus(Status.ABORT)) {
+      return;
+    }
 
     for (final Shortcut shortcut : primitive.getShortcuts()) {
-      visitShortcut(primitive, shortcut);
+      if (isStatus(Status.OK)) {
+        visitShortcut(primitive, shortcut);
+        if (isStatus(Status.ABORT)) {
+          return;
+        }
+      }
     }
 
     visitor.onPrimitiveEnd(primitive);
@@ -57,28 +93,53 @@ public class IrWalkerShortcuts {
 
   private void visitShortcut(final PrimitiveAND primitive, final Shortcut shortcut) {
     visitor.onShortcutBegin(primitive, shortcut);
+    if (isStatus(Status.ABORT)) {
+      return;
+    }
 
     final PrimitiveAND entry = shortcut.getEntry();
     final PrimitiveAND target = shortcut.getTarget();
 
-    visitPrimitive(entry, target);
+    if (isStatus(Status.OK)) {
+      visitPrimitive(entry, target);
+      if (isStatus(Status.ABORT)) {
+        return;
+      }
+    }
+
     visitor.onShortcutEnd(primitive, shortcut);
   }
 
   private void visitPrimitive(final PrimitiveAND entry, final PrimitiveAND target) {
     visitor.onPrimitiveBegin(entry);
+    if (isStatus(Status.ABORT)) {
+      return;
+    }
 
     final boolean reachedTarget = entry == target;
     for (final Map.Entry<String, Primitive> item : entry.getArguments().entrySet()) {
       final String argName = item.getKey();
       final Primitive arg = item.getValue();
 
-      if ((arg.getKind() == Primitive.Kind.OP) && !reachedTarget) {
+      if (isStatus(Status.OK) && (arg.getKind() == Primitive.Kind.OP) && !reachedTarget) {
         InvariantChecks.checkFalse(arg.isOrRule());
 
         visitor.onArgumentBegin(entry, argName, arg);
-        visitPrimitive((PrimitiveAND) arg, target);
+        if (isStatus(Status.ABORT)) {
+          return;
+        }
+
+        if (isStatus(Status.OK)) {
+          visitPrimitive((PrimitiveAND) arg, target);
+          if (isStatus(Status.ABORT)) {
+            return;
+          }
+        }
+
         visitor.onArgumentEnd(entry, argName, arg);
+        if (isStatus(Status.ABORT)) {
+          return;
+        }
       }
     }
 
