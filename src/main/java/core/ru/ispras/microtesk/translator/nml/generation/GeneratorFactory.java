40b588c9542c1358417b0a6387216b1e4e61f11b
Andrei Tatarnikov <andrewt@ispras.ru>
2016-09-01 13:29:00 +0000

    Small code update

--- src/main/java/core/ru/ispras/microtesk/translator/nml/generation/GeneratorFactory.java
+++ src/main/java/core/ru/ispras/microtesk/translator/nml/generation/GeneratorFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2014 ISP RAS (http://www.ispras.ru)
+ * Copyright 2012-2016 ISP RAS (http://www.ispras.ru)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -14,12 +14,7 @@
 
 package ru.ispras.microtesk.translator.nml.generation;
 
-import static ru.ispras.microtesk.translator.generation.PackageInfo.COMMON_TEMPLATE_DIR;
-import static ru.ispras.microtesk.translator.generation.PackageInfo.NML_TEMPLATE_DIR;
-import static ru.ispras.microtesk.translator.generation.PackageInfo.getModeFileFormat;
-import static ru.ispras.microtesk.translator.generation.PackageInfo.getModelFileFormat;
-import static ru.ispras.microtesk.translator.generation.PackageInfo.getOpFileFormat;
-import static ru.ispras.microtesk.translator.generation.PackageInfo.getSharedFileFormat;
+import ru.ispras.microtesk.translator.generation.PackageInfo;
 import ru.ispras.microtesk.translator.generation.STFileGenerator;
 import ru.ispras.microtesk.translator.generation.FileGenerator;
 import ru.ispras.microtesk.translator.generation.STBuilder;
@@ -37,11 +32,12 @@ final class GeneratorFactory {
   }
 
   public FileGenerator createModelGenerator(Ir ir) {
-    final String outputFileName = String.format(getModelFileFormat(outDir), modelName);
+    final String outputFileName = String.format(
+        PackageInfo.getModelFileFormat(outDir), modelName);
 
     final String[] templateGroups = new String[] { 
-      COMMON_TEMPLATE_DIR + "JavaCommon.stg",
-      NML_TEMPLATE_DIR + "Model.stg"
+        PackageInfo.COMMON_TEMPLATE_DIR + "JavaCommon.stg",
+        PackageInfo.NML_TEMPLATE_DIR + "Model.stg"
     };
 
     final STBuilder modelBuilder = new STBModel(modelName, ir);
@@ -49,11 +45,12 @@ final class GeneratorFactory {
   }
 
   public FileGenerator createSharedGenerator(Ir ir) {
-    final String outputFileName = String.format(getSharedFileFormat(outDir), modelName);
+    final String outputFileName =
+        String.format(PackageInfo.getSharedFileFormat(outDir), modelName);
 
     final String[] templateGroups = new String[] {
-      COMMON_TEMPLATE_DIR + "JavaCommon.stg",
-      NML_TEMPLATE_DIR + "Shared.stg"
+        PackageInfo.COMMON_TEMPLATE_DIR + "JavaCommon.stg",
+        PackageInfo.NML_TEMPLATE_DIR + "Shared.stg"
     };
 
     final STBuilder builder = new STBShared(ir, modelName);
@@ -62,11 +59,11 @@ final class GeneratorFactory {
 
   public FileGenerator createAddressingModeOr(PrimitiveOR mode) {
     final String outputFileName =
-      String.format(getModeFileFormat(outDir), modelName, mode.getName());
+      String.format(PackageInfo.getModeFileFormat(outDir), modelName, mode.getName());
 
     final String[] templateGroups = new String[] {
-      COMMON_TEMPLATE_DIR + "JavaCommon.stg",
-      NML_TEMPLATE_DIR + "AddressingModeOr.stg"
+        PackageInfo.COMMON_TEMPLATE_DIR + "JavaCommon.stg",
+        PackageInfo.NML_TEMPLATE_DIR + "AddressingModeOr.stg"
     };
 
     final STBuilder builder = new STBAddressingModeOr(modelName, mode);
@@ -75,11 +72,11 @@ final class GeneratorFactory {
 
   public FileGenerator createAddressingMode(PrimitiveAND mode) {
     final String outputFileName =
-      String.format(getModeFileFormat(outDir), modelName, mode.getName());
+      String.format(PackageInfo.getModeFileFormat(outDir), modelName, mode.getName());
 
     final String[] templateGroups = new String[] {
-      COMMON_TEMPLATE_DIR + "JavaCommon.stg",
-      NML_TEMPLATE_DIR + "AddressingMode.stg"
+        PackageInfo.COMMON_TEMPLATE_DIR + "JavaCommon.stg",
+        PackageInfo.NML_TEMPLATE_DIR + "AddressingMode.stg"
     };
 
     final STBuilder builder = new STBAddressingMode(modelName, mode);
@@ -88,11 +85,11 @@ final class GeneratorFactory {
 
   public FileGenerator createOperationOr(PrimitiveOR op) {
     final String outputFileName = 
-      String.format(getOpFileFormat(outDir), modelName, op.getName());
+      String.format(PackageInfo.getOpFileFormat(outDir), modelName, op.getName());
 
     final String[] templateGroups = new String[] {
-      COMMON_TEMPLATE_DIR + "JavaCommon.stg",
-      NML_TEMPLATE_DIR + "OperationOr.stg"
+        PackageInfo.COMMON_TEMPLATE_DIR + "JavaCommon.stg",
+        PackageInfo.NML_TEMPLATE_DIR + "OperationOr.stg"
     };
 
     final STBuilder builder = new STBOperationOr(modelName, op);
@@ -100,11 +97,12 @@ final class GeneratorFactory {
   }
 
   public FileGenerator createOperation(PrimitiveAND op) {
-    final String outputFileName = String.format(getOpFileFormat(outDir), modelName, op.getName());
+    final String outputFileName = String.format(
+        PackageInfo.getOpFileFormat(outDir), modelName, op.getName());
 
     final String[] templateGroups = new String[] {
-      COMMON_TEMPLATE_DIR + "JavaCommon.stg",
-      NML_TEMPLATE_DIR + "Operation.stg"
+        PackageInfo.COMMON_TEMPLATE_DIR + "JavaCommon.stg",
+        PackageInfo.NML_TEMPLATE_DIR + "Operation.stg"
     };
 
     final STBuilder builder = new STBOperation(modelName, op);
