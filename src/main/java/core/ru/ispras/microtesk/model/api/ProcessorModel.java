8d863413c885267f9e1b1da46c995084a456dcc3
Andrei Tatarnikov <andrewt@ispras.ru>
2016-09-29 09:45:27 +0000

    ProcessorModel - deleted metadata-related code

--- src/main/java/core/ru/ispras/microtesk/model/api/ProcessorModel.java
+++ src/main/java/core/ru/ispras/microtesk/model/api/ProcessorModel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2014 ISP RAS (http://www.ispras.ru)
+ * Copyright 2012-2016 ISP RAS (http://www.ispras.ru)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -14,10 +14,7 @@
 
 package ru.ispras.microtesk.model.api;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import ru.ispras.fortress.util.InvariantChecks;
@@ -30,24 +27,19 @@ import ru.ispras.microtesk.model.api.instruction.InstructionCall;
 import ru.ispras.microtesk.model.api.instruction.Operation;
 import ru.ispras.microtesk.model.api.memory.Label;
 import ru.ispras.microtesk.model.api.memory.Memory;
-import ru.ispras.microtesk.model.api.metadata.MetaAddressingMode;
-import ru.ispras.microtesk.model.api.metadata.MetaGroup;
-import ru.ispras.microtesk.model.api.metadata.MetaLocationStore;
 import ru.ispras.microtesk.model.api.metadata.MetaModel;
 import ru.ispras.microtesk.model.api.metadata.MetaModelBuilder;
-import ru.ispras.microtesk.model.api.metadata.MetaOperation;
 import ru.ispras.microtesk.model.api.state.ModelStateObserver;
 import ru.ispras.microtesk.model.api.state.Resetter;
 
 /**
- * The ProcessorModel class is base class for all families of microprocessor model classes. It
- * implements all methods of the interfaces that provide services to external users. The
+ * The {@link ProcessorModel} class is base class for all families of microprocessor model classes.
+ * It implements all methods of the interfaces that provide services to external users. The
  * responsibility to initialize member data (to create corresponding objects) is delegated to
  * descendant classes.
  * 
- * @author Andrei Tatarnikov
+ * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
  */
-
 public abstract class ProcessorModel implements IModel, CallFactory {
   private final String name;
 
@@ -57,37 +49,6 @@ public abstract class ProcessorModel implements IModel, CallFactory {
   private final ModelStateObserver observer;
   private final Resetter resetter;
   private final MetaModel metaModel;
-
-  public ProcessorModel(
-      final String name,
-      final AddressingMode.IInfo[] modes,
-      final AddressingMode.IInfo[] modeGroups,
-      final Operation.IInfo[] ops,
-      final Operation.IInfo[] opGroups,
-      final Memory[] registers,
-      final Memory[] memory,
-      final Memory[] variables,
-      final Label[] labels) {
-    this.name = name;
-
-    this.modes = new AddressingModeStore(modes);
-    this.ops = new OperationStore(ops);
-
-    this.observer = new ModelStateObserver(registers, memory, labels);
-    this.resetter = new Resetter(variables);
-
-    final List<MetaGroup> modeGroupList = modeGroupsToList(modeGroups);
-    final List<MetaGroup> opGroupList = opGroupsToList(opGroups);
-
-    this.metaModel = new MetaModel(
-      this.modes.getMetaData(),
-      modeGroupList,
-      this.ops.getMetaData(),
-      opGroupList,
-      new MemoryStore(registers).getMetaData(),
-      new MemoryStore(memory).getMetaData()
-    );
-  }
 
   public ProcessorModel(
       final String name,
@@ -109,27 +70,6 @@ public abstract class ProcessorModel implements IModel, CallFactory {
     this.resetter = new Resetter(variables);
     this.metaModel = metaModelBuilder.build();
   }
-
-  private static List<MetaGroup> modeGroupsToList(final AddressingMode.IInfo[] modeGroups) {
-    final List<MetaGroup> result = new ArrayList<>();
-
-    for (final AddressingMode.IInfo i : modeGroups) {
-      result.add(((AddressingMode.InfoOrRule) i).getMetaDataGroup());
-    }
-
-    return result;
-  }
-  
-  private static List<MetaGroup> opGroupsToList(final Operation.IInfo[] opGroups) {
-    final List<MetaGroup> result = new ArrayList<>();
-
-    for (final Operation.IInfo i : opGroups) {
-      result.add(((Operation.InfoOrRule) i).getMetaDataGroup());
-    }
-
-    return result;
-  }
-
 
   public final String getName() {
     return name;
@@ -210,64 +150,33 @@ public abstract class ProcessorModel implements IModel, CallFactory {
     return new InstructionCall(resetter, op);
   }
 
-  private static final class MemoryStore {
-    private final Collection<MetaLocationStore> metaData;
-
-    public MemoryStore(final Memory[] memory) {
-      this.metaData = new ArrayList<>(memory.length);
-      for (final Memory m : memory) {
-        this.metaData.add(new MetaLocationStore(m.getName(), m.getType(), m.getLength()));
-      }
-    }
-
-    public Collection<MetaLocationStore> getMetaData() {
-      return metaData;
-    }
-  }
-
   private static final class AddressingModeStore {
     private final Map<String, AddressingMode.IInfo> items;
-    private final Collection<MetaAddressingMode> metaData;
 
     public AddressingModeStore(final AddressingMode.IInfo[] modes) {
       this.items = new HashMap<>(modes.length);
-      this.metaData = new ArrayList<>(modes.length);
-
       for (AddressingMode.IInfo i : modes) {
         items.put(i.getName(), i);
-        this.metaData.addAll(i.getMetaData());
       }
     }
 
     public AddressingMode.IInfo getModeInfo(final String name) {
       return items.get(name);
     }
-
-    public Collection<MetaAddressingMode> getMetaData() {
-      return metaData;
-    }
   }
 
   private static final class OperationStore {
     private final Map<String, Operation.IInfo> items;
-    private final Collection<MetaOperation> metaData;
 
     public OperationStore(final Operation.IInfo[] ops) {
       this.items = new HashMap<>(ops.length);
-      this.metaData = new ArrayList<>(ops.length);
-
       for (Operation.IInfo i : ops) {
         items.put(i.getName(), i);
-        this.metaData.addAll(i.getMetaData());
       }
     }
 
     public Operation.IInfo getOpInfo(final String name) {
       return items.get(name);
-    }
-
-    public Collection<MetaOperation> getMetaData() {
-      return metaData;
     }
   }
 }
