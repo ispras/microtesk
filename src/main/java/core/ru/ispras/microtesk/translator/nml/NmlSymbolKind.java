6bc4c8374fb9688dc72405ea2b0bb807971d3b9c
Andrei Tatarnikov <andrewt@ispras.ru>
2016-11-07 15:49:07 +0000

    Killed space

--- src/main/java/core/ru/ispras/microtesk/translator/nml/NmlSymbolKind.java
+++ src/main/java/core/ru/ispras/microtesk/translator/nml/NmlSymbolKind.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 ISP RAS (http://www.ispras.ru)
+ * Copyright 2012-2016 ISP RAS (http://www.ispras.ru)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -19,7 +19,6 @@ package ru.ispras.microtesk.translator.nml;
  * 
  * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
  */
-
 public enum NmlSymbolKind {
   /** Reserved keywords */
   KEYWORD,
