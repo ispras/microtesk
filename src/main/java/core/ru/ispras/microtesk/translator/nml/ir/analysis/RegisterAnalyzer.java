d4b42508cb6ff066b6ea8a13be6fd8e1f921a7fd
Alexander Kamkin <kamkin@ispras.ru>
2016-11-25 12:51:58 +0000

    Register Analyzer

---
+++ src/main/java/core/ru/ispras/microtesk/translator/nml/ir/analysis/RegisterAnalyzer.java
@@ -0,0 +1,152 @@
+package ru.ispras.microtesk.translator.nml.ir.analysis;
+
+import ru.ispras.fortress.expression.ExprTreeVisitorDefault;
+import ru.ispras.fortress.expression.ExprTreeWalker;
+import ru.ispras.fortress.expression.NodeOperation;
+import ru.ispras.fortress.expression.NodeVariable;
+import ru.ispras.fortress.expression.StandardOperation;
+import ru.ispras.fortress.util.InvariantChecks;
+import ru.ispras.fortress.util.TreeVisitor.Status;
+import ru.ispras.microtesk.model.api.memory.Memory;
+import ru.ispras.microtesk.translator.TranslatorHandler;
+import ru.ispras.microtesk.translator.nml.NmlSymbolKind;
+import ru.ispras.microtesk.translator.nml.ir.Ir;
+import ru.ispras.microtesk.translator.nml.ir.IrVisitorDefault;
+import ru.ispras.microtesk.translator.nml.ir.IrWalker;
+import ru.ispras.microtesk.translator.nml.ir.expr.Expr;
+import ru.ispras.microtesk.translator.nml.ir.expr.Location;
+import ru.ispras.microtesk.translator.nml.ir.expr.LocationSource;
+import ru.ispras.microtesk.translator.nml.ir.expr.LocationSourceMemory;
+import ru.ispras.microtesk.translator.nml.ir.expr.NodeInfo;
+import ru.ispras.microtesk.translator.nml.ir.primitive.Attribute;
+import ru.ispras.microtesk.translator.nml.ir.primitive.Primitive;
+import ru.ispras.microtesk.translator.nml.ir.primitive.PrimitiveAND;
+import ru.ispras.microtesk.translator.nml.ir.primitive.Statement;
+import ru.ispras.microtesk.translator.nml.ir.primitive.StatementAttributeCall;
+import ru.ispras.microtesk.translator.nml.ir.primitive.StatementFormat;
+
+public class RegisterAnalyzer implements TranslatorHandler<Ir> {
+
+  @Override
+  public void processIr(Ir ir) {
+    final IrWalker walker = new IrWalker(ir);
+    walker.visit(new Visitor(), IrWalker.Direction.LINEAR);
+  }
+
+  private static final class Visitor extends IrVisitorDefault {
+
+    @Override
+    public void onPrimitiveBegin(Primitive item) {
+      if (item.isOrRule() || item.getKind() != Primitive.Kind.MODE) {
+        setStatus(Status.SKIP);
+        return;
+      }
+
+      final PrimitiveAND primitive = (PrimitiveAND) item;
+      final Expr expr = primitive.getReturnExpr();
+
+      if (null == expr || !isRegisterAccess(expr)) {
+        setStatus(Status.SKIP);
+        return;
+      }
+
+      System.out.println(item.getName());
+    }
+
+    @Override
+    public void onPrimitiveEnd(Primitive item) {
+      if (getStatus() == Status.SKIP) {
+        setStatus(Status.OK);
+      }
+    }
+
+    @Override
+    public void onAttributeBegin(final PrimitiveAND andRule, final Attribute attr) {
+      if (!attr.getName().equals(Attribute.SYNTAX_NAME)) {
+        setStatus(Status.SKIP);
+        return;
+      }
+
+      InvariantChecks.checkTrue(
+          2 >= attr.getStatements().size(), "image cannot have more than 2 statements");
+    }
+
+    @Override
+    public void onAttributeEnd(final PrimitiveAND andRule, final Attribute attr) {
+      setStatus(Status.OK);
+    }
+
+    @Override
+    public void onStatement(
+        final PrimitiveAND andRule,
+        final Attribute attr,
+        final Statement stmt) {
+      InvariantChecks.checkTrue(stmt.getKind() == Statement.Kind.FORMAT ||
+                                stmt.getKind() == Statement.Kind.CALL);
+      if (stmt instanceof StatementFormat) {
+        onStatementFormat(andRule, (StatementFormat) stmt);
+      } else if (stmt instanceof StatementAttributeCall) {
+        //onStatementAttributeCall(andRule, (StatementAttributeCall) stmt);
+      } else {
+        InvariantChecks.checkTrue(
+            false, "Unsupported statement: " + stmt.getKind());
+      }
+    }
+
+    private void onStatementFormat(PrimitiveAND andRule, StatementFormat stmt) {
+      System.out.println(andRule.getName() + " : " + stmt.getFormat());
+      // TODO Auto-generated method stub
+    }
+  }
+
+  private static boolean isRegisterAccess(final Expr expr) {
+    final ExprVisitor visitor = new ExprVisitor();
+    final ExprTreeWalker walker = new ExprTreeWalker(visitor);
+
+    walker.visit(expr.getNode());
+    return visitor.isRegister();
+  }
+
+  private static final class ExprVisitor extends ExprTreeVisitorDefault {
+    private boolean register = false;
+
+    public boolean isRegister() {
+      return register;
+    }
+
+    @Override
+    public void onVariable(final NodeVariable variable) {
+      if (isRegister(variable)) {
+        register = true;
+        setStatus(Status.ABORT);
+      }
+    }
+
+    @Override
+    public void onOperationBegin(NodeOperation node) {
+      if (node.getOperationId() != StandardOperation.ITE) {
+        setStatus(Status.SKIP);
+      }
+    }
+
+    @Override
+    public void onOperationEnd(NodeOperation node) {
+      if (getStatus() == Status.SKIP) {
+        setStatus(Status.OK);
+      }
+    }
+
+    private static boolean isRegister(final NodeVariable variable) {
+      final NodeInfo nodeInfo = (NodeInfo) variable.getUserData();
+      final Location location = (Location) nodeInfo.getSource();
+      final LocationSource locationSource = location.getSource();
+
+      if (locationSource.getSymbolKind() != NmlSymbolKind.MEMORY) {
+        return false;
+      }
+
+      final LocationSourceMemory memory = (LocationSourceMemory) locationSource;
+      return memory.getMemory().getKind() == Memory.Kind.REG;
+    }
+  }
+}
