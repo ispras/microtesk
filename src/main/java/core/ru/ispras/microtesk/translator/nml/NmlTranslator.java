34184d060aa063b1aa1cf11e29f914ed557aef52
Andrei Tatarnikov <andrewt@ispras.ru>
2016-12-08 17:32:35 +0000

    New reserved keywords

--- src/main/java/core/ru/ispras/microtesk/translator/nml/NmlTranslator.java
+++ src/main/java/core/ru/ispras/microtesk/translator/nml/NmlTranslator.java
@@ -57,6 +57,7 @@ public final class NmlTranslator extends Translator<Ir> {
 
     getSymbols().defineReserved(NmlSymbolKind.KEYWORD, ReservedKeywords.JAVA);
     getSymbols().defineReserved(NmlSymbolKind.KEYWORD, ReservedKeywords.RUBY);
+    getSymbols().defineReserved(NmlSymbolKind.KEYWORD, ReservedKeywords.NML);
 
     // Detects parent-child connections between primitives
     addHandler(new ReferenceDetector());
