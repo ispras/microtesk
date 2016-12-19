8b2ee549ada8b577bea8c3ad2da4a5e5548475d8
Andrei Tatarnikov <andrewt@ispras.ru>
2016-12-19 13:40:20 +0000

    Disassembler - refactor to make it reusable

--- src/main/java/core/ru/ispras/microtesk/decoder/Disassembler.java
+++ src/main/java/core/ru/ispras/microtesk/decoder/Disassembler.java
@@ -33,15 +33,20 @@ import ru.ispras.microtesk.utils.FileUtils;
 public final class Disassembler {
   public static interface Output {
     void add(final IsaPrimitive primitive);
+    void close();
+  }
+
+  public static interface OutputFactory {
+    Output createOutput(final Model model);
   }
 
   public static boolean disassemble(
-      final Options options,
       final String modelName,
-      final String fileName) {
-    InvariantChecks.checkNotNull(options);
+      final String fileName,
+      final OutputFactory outputFactory) {
     InvariantChecks.checkNotNull(modelName);
     InvariantChecks.checkNotNull(fileName);
+    InvariantChecks.checkNotNull(outputFactory);
 
     final Model model = loadModel(modelName);
     if (null == model) {
@@ -53,26 +58,70 @@ public final class Disassembler {
       return false;
     }
 
+    Output output = null; 
+    try {
+      output = outputFactory.createOutput(model);
+      if (null == output) {
+        return false;
+      }
+
+      return decode(model.getDecoder(), reader, output);
+    } finally {
+      reader.close();
+      if (null != output) {
+        output.close();
+      }
+    }
+  }
+
+  public static boolean disassemble(
+      final Options options,
+      final String modelName,
+      final String fileName) {
+    InvariantChecks.checkNotNull(options);
+    InvariantChecks.checkNotNull(modelName);
+    InvariantChecks.checkNotNull(fileName);
+
     final String fileExt = options.getValueAsString(Option.CODE_EXT);
     final String fileShortName = FileUtils.getShortFileNameNoExt(fileName);
     final PrintWriter writer = newWriter(getOutDir(options), fileShortName, fileExt);
     if (null == writer) {
-      reader.close();
       return false;
     }
 
-    try {
-      final TemporaryVariables tempVars = model.getTempVars();
-      return decode(model.getDecoder(), reader, new Output() {
-        @Override
-        public void add(final IsaPrimitive primitive) {
-          final String text = primitive.syntax(tempVars);
-          Logger.debug(text);
-          writer.println(text);
-        }
-      });
-    } finally {
-      reader.close();
+    final OutputFactory outputFactory = new OutputFactory() {
+      @Override
+      public Output createOutput(final Model model) {
+        InvariantChecks.checkNotNull(model);
+        final TemporaryVariables tempVars = model.getTempVars();
+        return new OutputImpl(writer, tempVars);
+      }
+    }; 
+
+    return disassemble(modelName, fileName, outputFactory);
+  }
+
+  private static final class OutputImpl implements Output {
+    private final PrintWriter writer;
+    private final TemporaryVariables tempVars;
+
+    private OutputImpl(final PrintWriter writer, final TemporaryVariables tempVars) {
+      InvariantChecks.checkNotNull(writer);
+      InvariantChecks.checkNotNull(tempVars);
+
+      this.writer = writer;
+      this.tempVars = tempVars;
+    }
+
+    @Override
+    public void add(final IsaPrimitive primitive) {
+      final String text = primitive.syntax(tempVars);
+      Logger.debug(text);
+      writer.println(text);
+    }
+
+    @Override
+    public void close() {
       writer.close();
     }
   }
@@ -128,7 +177,7 @@ public final class Disassembler {
     }
   }
 
-  public static boolean decode(
+  private static boolean decode(
       final Decoder decoder,
       final BinaryReader reader,
       final Output output) {
