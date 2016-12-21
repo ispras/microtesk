c5ee1fda8e012696ae0cefc35400696d83116729
Andrei Tatarnikov <andrewt@ispras.ru>
2016-12-21 08:06:53 +0000

    DecoderGroup - deleted premature optimization

--- src/main/java/core/ru/ispras/microtesk/decoder/DecoderGroup.java
+++ src/main/java/core/ru/ispras/microtesk/decoder/DecoderGroup.java
@@ -15,27 +15,20 @@
 package ru.ispras.microtesk.decoder;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import ru.ispras.fortress.data.types.bitvector.BitVector;
 import ru.ispras.fortress.util.InvariantChecks;
 
 public abstract class DecoderGroup extends Decoder {
-  private final boolean hasOpcMask;
   private final List<Decoder> decoderList;
-  private final Map<BitVector, Decoder> decoderMap;
 
   protected DecoderGroup(
       final int maxImageSize,
       final boolean imageSizeFixed,
       final String opcMask) {
     super(maxImageSize, imageSizeFixed, null, opcMask);
-
-    this.hasOpcMask = null != opcMask;
-    this.decoderList = hasOpcMask ? null : new ArrayList<Decoder>();
-    this.decoderMap = hasOpcMask ? new HashMap<BitVector, Decoder>() : null;
+    this.decoderList = new ArrayList<>();
   }
 
   protected final void add(final Decoder decoder) {
@@ -43,28 +36,12 @@ public abstract class DecoderGroup extends Decoder {
     InvariantChecks.checkTrue(getMaxImageSize() >= decoder.getMaxImageSize());
     InvariantChecks.checkTrue(isImageSizeFixed() ? decoder.isImageSizeFixed() : true);
 
-    if (hasOpcMask) {
-      InvariantChecks.checkNotNull(decoder.getOpc());
-      decoderMap.put(decoder.getOpc(), decoder);
-    } else {
-      decoderList.add(decoder);
-    }
+    decoderList.add(decoder);
   }
 
   @Override
   public final DecoderResult decode(final BitVector image) {
-    return hasOpcMask ? decodeUsingDecoderMap(image) : decodeUsingDecoderList(image);
-  }
-
-  private DecoderResult decodeUsingDecoderMap(final BitVector image) {
-    final BitVector imageOpc = applyOpcMask(image);
-
-    final Decoder decoder = decoderMap.get(imageOpc);
-    if (null == decoder) {
-      return null;
-    }
-
-    return decoder.decode(image);
+    return decodeUsingDecoderList(image);
   }
 
   private DecoderResult decodeUsingDecoderList(final BitVector image) {
