cad51dfbcf50842b09ff557f24552a577cc896e7
Andrei Tatarnikov <andrewt@ispras.ru>
2017-01-26 10:48:34 +0000

    Small update

--- src/main/java/core/ru/ispras/microtesk/test/Statistics.java
+++ src/main/java/core/ru/ispras/microtesk/test/Statistics.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 ISP RAS (http://www.ispras.ru)
+ * Copyright 2016-2017 ISP RAS (http://www.ispras.ru)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -140,10 +140,6 @@ public final class Statistics {
   public void incSequences() {
     sequences++;
     sequenceTraceLength = 0;
-  }
-
-  public void incInstructions() {
-    incInstructions(1);
   }
 
   public void incInstructions(final int count) {
