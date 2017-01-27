a2788a300dc099d4e920191bca019d1ba1f7ff7d
Andrei Tatarnikov <andrewt@ispras.ru>
2017-01-27 12:06:11 +0000

    TestProgram stores data

--- src/main/java/core/ru/ispras/microtesk/test/TestProgram.java
+++ src/main/java/core/ru/ispras/microtesk/test/TestProgram.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import ru.ispras.fortress.util.InvariantChecks;
 import ru.ispras.fortress.util.Pair;
 import ru.ispras.microtesk.test.template.Block;
+import ru.ispras.microtesk.test.template.DataSection;
 
 /**
  * This structure describes test program being generated.
@@ -34,11 +35,16 @@ final class TestProgram {
   private final List<TestProgramEntry> entries;
   private final List<Pair<List<TestSequence>, Map<String, TestSequence>>> exceptionHandlers;
 
+  private final List<DataSection> globalData;
+  private final List<DataSection> localData;
+
   public TestProgram() {
     this.prologue = null;
     this.epilogue = null;
     this.entries = new ArrayList<>();
     this.exceptionHandlers = new ArrayList<>();
+    this.globalData = new ArrayList<>();
+    this.localData = new ArrayList<>();
   }
 
   public TestSequence getPrologue() {
@@ -82,7 +88,25 @@ final class TestProgram {
     return entries.get(index);
   }
 
+  public void addData(final DataSection data) {
+    InvariantChecks.checkNotNull(data);
+    if (data.isGlobal()) {
+      globalData.add(data);
+    } else {
+      localData.add(data);
+    }
+  }
+
+  public List<DataSection> getGlobalData() {
+    return globalData;
+  }
+
+  public List<DataSection> getLocalData() {
+    return localData;
+  }
+
   public void reset() {
     entries.clear();
+    localData.clear();
   }
 }
