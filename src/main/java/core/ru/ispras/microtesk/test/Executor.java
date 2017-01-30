6f8e1cb9b356c4bab5ddda43f6df67189cb40259
Andrei Tatarnikov <andrewt@ispras.ru>
2017-01-30 09:04:36 +0000

    Executor: added a check for PC increment

--- src/main/java/core/ru/ispras/microtesk/test/Executor.java
+++ src/main/java/core/ru/ispras/microtesk/test/Executor.java
@@ -320,6 +320,12 @@ final class Executor {
       }
 
       final long address = getPC();
+
+      if (address == call.getAddress()) {
+        throw new GenerationAbortedException(
+            String.format("PC was not updated. It still points to 0x%016x", address));
+      }
+
       final boolean isJump = address != call.getAddress() + call.getByteSize();
 
       // NORMAL
