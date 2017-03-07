8fda6b9526b0275e6c948410bb8ab58b6f92c7e2
Andrei Tatarnikov <andrewt@ispras.ru>
2017-03-07 08:34:22 +0000

    MemorySubsystemTestCase updated

--- src/test/java/core/ru/ispras/microtesk/model/minimips/MemorySubsystemTestCase.java
+++ src/test/java/core/ru/ispras/microtesk/model/minimips/MemorySubsystemTestCase.java
@@ -27,6 +27,6 @@ public class MemorySubsystemTestCase extends MiniMipsTest {
 
     Assert.assertEquals(1, statistics.getPrograms());
     Assert.assertEquals(1, statistics.getSequences());
-    Assert.assertEquals(32, statistics.getInstructions());
+    Assert.assertEquals(31, statistics.getInstructions());
   }
 }
