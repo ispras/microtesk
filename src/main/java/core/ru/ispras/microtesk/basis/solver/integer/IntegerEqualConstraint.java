49750bfefdcb0a8b972e9584ee110f15ec4845bb
Alexander Kamkin <kamkin@ispras.ru>
2017-05-03 08:28:42 +0000

    IntegerEqualConstraint added.

---
+++ src/main/java/core/ru/ispras/microtesk/basis/solver/integer/IntegerEqualConstraint.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2017 ISP RAS (http://www.ispras.ru)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.microtesk.basis.solver.integer;
+
+import java.math.BigInteger;
+
+import ru.ispras.fortress.util.InvariantChecks;
+import ru.ispras.microtesk.test.template.Value;
+
+/**
+ * {@link IntegerEqualConstraint} class represents constraints of the kind {@code x == value}.
+ * 
+ * @author <a href="mailto:kamkin@ispras.ru">Alexander Kamkin</a>
+ */
+public final class IntegerEqualConstraint<V> implements IntegerConstraint<V> {
+  private final V variable;
+  private final Value variate;
+
+  public IntegerEqualConstraint(
+      final V variable,
+      Value variate) {
+    InvariantChecks.checkNotNull(variable);
+    InvariantChecks.checkNotNull(variate);
+
+    this.variable = variable;
+    this.variate = variate;
+  }
+
+  public V getVariable() {
+    return variable;
+  }
+
+  public Value getVariate() {
+    return variate;
+  }
+
+  @Override
+  public IntegerFormula<V> getFormula() {
+    // Value is randomized.
+    final BigInteger value = variate.getValue();
+
+    final IntegerFormula.Builder<V> builder = new IntegerFormula.Builder<>();
+    builder.addEquation(variable, value, true);
+
+    return builder.build();
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s == %s", variable, variate);
+  }
+}
