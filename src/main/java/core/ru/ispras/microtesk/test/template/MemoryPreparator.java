621701008c7fc05afd50a7098ce7140611d543d6
Andrei Tatarnikov <andrewt@ispras.ru>
2017-05-03 13:25:04 +0000

    MemoryPreparator - first step

---
+++ src/main/java/core/ru/ispras/microtesk/test/template/MemoryPreparator.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2017 ISP RAS (http://www.ispras.ru)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.microtesk.test.template;
+
+import java.util.Collections;
+import java.util.List;
+
+import ru.ispras.fortress.data.types.bitvector.BitVector;
+import ru.ispras.fortress.util.InvariantChecks;
+import ru.ispras.microtesk.Logger;
+
+/**
+ * The {@link MemoryPreparator} describes instruction sequences to set up memory state.
+ * 
+ * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
+ */
+public final class MemoryPreparator {
+  private final int dataSize;
+  private final LazyData addressHolder;
+  private final LazyData dataHolder;
+  private final List<Call> calls;
+  private final LabelUniqualizer.SeriesId labelSeriesId;
+
+  protected MemoryPreparator(
+      final int dataSize,
+      final LazyData addressHolder,
+      final LazyData dataHolder,
+      final List<Call> calls) {
+    InvariantChecks.checkGreaterOrEqZero(dataSize);
+    InvariantChecks.checkNotNull(addressHolder);
+    InvariantChecks.checkNotNull(dataHolder);
+    InvariantChecks.checkNotNull(calls);
+
+    this.dataSize = dataSize;
+    this.addressHolder = addressHolder;
+    this.dataHolder = dataHolder;
+    this.calls = Collections.unmodifiableList(calls);
+    this.labelSeriesId = LabelUniqualizer.get().newSeries();
+  }
+
+  public int getDataSize() {
+    return dataSize;
+  }
+
+  public List<Call> makeInitializer(
+      final PreparatorStore preparators,
+      final BitVector addressValue,
+      final BitVector dataValue) {
+    InvariantChecks.checkNotNull(preparators);
+    InvariantChecks.checkNotNull(addressValue);
+    InvariantChecks.checkNotNull(dataValue);
+
+    Logger.debug("Making a memory preparator");
+    Logger.debug("Address: 0x%s", addressValue.toHexString());
+    Logger.debug("Data: 0x%s", dataValue.toHexString());
+
+    addressHolder.setValue(addressValue);
+    dataHolder.setValue(dataValue);
+
+    return Preparator.expandPreparators(labelSeriesId, preparators, calls);
+  }
+}
---
+++ src/main/java/core/ru/ispras/microtesk/test/template/MemoryPreparatorBuilder.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2017 ISP RAS (http://www.ispras.ru)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package ru.ispras.microtesk.test.template;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.ispras.fortress.util.InvariantChecks;
+
+/**
+ * The {@link MemoryPreparatorBuilder} class is responsible for construction
+ * of memory preparators.
+ * 
+ * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
+ */
+public final class MemoryPreparatorBuilder {
+  private int dataSize;
+  private final LazyData address;
+  private final LazyData data;
+  private final List<Call> calls;
+
+  protected MemoryPreparatorBuilder() {
+    this.dataSize = 0;
+    this.address = new LazyData();
+    this.data = new LazyData();
+    this.calls = new ArrayList<>();
+  }
+
+  public void setDataSize(final int value) {
+    InvariantChecks.checkGreaterOrEqZero(value);
+    dataSize = value;
+  }
+
+  public LazyValue newDataReference() {
+    return new LazyValue(data);
+  }
+
+  public LazyValue newDataReference(final int start, final int end) {
+    return new LazyValue(data, start, end);
+  }
+
+  public LazyValue newAddressReference() {
+    return new LazyValue(address);
+  }
+
+  public LazyValue newAddressReference(final int start, final int end) {
+    return new LazyValue(address, start, end);
+  }
+
+  public void addCall(final Call call) {
+    InvariantChecks.checkNotNull(call);
+    calls.add(call);
+  }
+
+  public MemoryPreparator build() {
+    return new MemoryPreparator(dataSize, address, data, calls);
+  }
+}
