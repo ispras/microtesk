535c6fbdce1efc568f78a52dbfe4d17818a9b1ba
Andrei Tatarnikov <andrewt@ispras.ru>
2017-05-17 12:23:56 +0000

    GroupsTestCase updated

--- src/test/java/core/ru/ispras/microtesk/model/minimips/GroupsTestCase.java
+++ src/test/java/core/ru/ispras/microtesk/model/minimips/GroupsTestCase.java
@@ -27,6 +27,6 @@ public class GroupsTestCase extends MiniMipsTest {
 
     Assert.assertEquals(1,   statistics.getPrograms());
     Assert.assertEquals(20,  statistics.getSequences());
-    Assert.assertEquals(322, statistics.getInstructions());
+    Assert.assertEquals(326, statistics.getInstructions());
   }
 }
