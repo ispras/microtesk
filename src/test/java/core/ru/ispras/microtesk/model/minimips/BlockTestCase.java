557bbfef0732cc770a59375c8ed7c4fbef63ce89
Andrei Tatarnikov <andrewt@ispras.ru>
2017-05-17 12:19:15 +0000

    BlockTestCase updated

--- src/test/java/core/ru/ispras/microtesk/model/minimips/BlockTestCase.java
+++ src/test/java/core/ru/ispras/microtesk/model/minimips/BlockTestCase.java
@@ -27,6 +27,6 @@ public class BlockTestCase extends MiniMipsTest {
 
     Assert.assertEquals(1, statistics.getPrograms());
     Assert.assertEquals(10, statistics.getSequences());
-    Assert.assertEquals(124, statistics.getInstructions());
+    Assert.assertEquals(123, statistics.getInstructions());
   }
 }
