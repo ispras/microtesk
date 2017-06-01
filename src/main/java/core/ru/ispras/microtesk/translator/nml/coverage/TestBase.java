85c5d6a47a237834728f662f984dd944125191d0
Andrei Tatarnikov <andrewt@ispras.ru>
2017-06-01 13:44:03 +0300

    TestBase updated
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/translator/nml/coverage/TestBase.java
+++ src/main/java/core/ru/ispras/microtesk/translator/nml/coverage/TestBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 ISP RAS (http://www.ispras.ru)
+ * Copyright 2015-2017 ISP RAS (http://www.ispras.ru)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -89,9 +89,11 @@ public final class TestBase {
 
   public TestBaseQueryResult executeQuery(TestBaseQuery query) {
     final TestBaseQueryResult rc = testBase.executeQuery(query);
-    if (rc.getStatus() == TestBaseQueryResult.Status.OK) {
+    if (rc.getStatus() == TestBaseQueryResult.Status.OK ||
+        rc.getStatus() == TestBaseQueryResult.Status.ERROR) {
       return rc;
     }
+
     SolverResult result;
     try {
       final PathConstraintBuilder builder = constraintBuilder(query);
