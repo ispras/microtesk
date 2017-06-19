a7d96cfa80c1608009d0c2bf9a462d84609a116f
Andrei Tatarnikov <andrewt@ispras.ru>
2017-06-19 16:53:09 +0300

    Additional options to control allocation.
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/options/Option.java
+++ src/main/java/core/ru/ispras/microtesk/options/Option.java
@@ -95,11 +95,17 @@ public enum Option {
   SEPARATOR_TOKEN("separator-token", "Text used to create separators", "=", GENERATE),
   ORIGIN_FORMAT("origin-format", "Origin directive format", ".org 0x%x", GENERATE),
   ALIGN_FORMAT("align-format", "Alignment directive format", ".align %d", GENERATE),
-  CODE_SECTION_KEYWORD("code-section-keyword", "Code section directive", ".text", GENERATE),
-  DATA_SECTION_KEYWORD("data-section-keyword", "Data section directive", ".data", GENERATE),
 
   BASE_VA("base-virtual-address", "Base VA for memory allocation", BigInteger.ZERO, GENERATE),
   BASE_PA("base-physical-address", "Base PA for memory allocation", BigInteger.ZERO, GENERATE),
+
+  CODE_SECTION_KEYWORD("code-section-keyword", "Code section directive", ".text", GENERATE),
+  CODE_BASE_VA("base-code-virtual-address", "Base VA for code section", BigInteger.ZERO, GENERATE),
+  CODE_BASE_PA("base-code-physical-address", "Base PA for code section", BigInteger.ZERO, GENERATE),
+
+  DATA_SECTION_KEYWORD("data-section-keyword", "Data section directive", ".data", GENERATE),
+  DATA_BASE_VA("base-data-virtual-address", "Base VA for data section", BigInteger.ZERO, GENERATE),
+  DATA_BASE_PA("base-data-physical-address", "Base PA for data section", BigInteger.ZERO, GENERATE),
 
   INSTANCE_NUMBER("instance-number", "Number of processing element instances", 1, GENERATE),
 
