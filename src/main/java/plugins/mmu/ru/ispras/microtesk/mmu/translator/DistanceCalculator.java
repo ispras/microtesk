f006d17131d7f3d9f86325f1df3e03e04126ecc3
Andrei Tatarnikov <andrewt@ispras.ru>
2017-10-10 13:01:16 +0300

    Small refactoring
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/DistanceCalculator.java
+++ src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/DistanceCalculator.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2015 ISP RAS (http://www.ispras.ru)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -25,6 +25,7 @@ import ru.ispras.fortress.data.types.bitvector.BitVector;
 import ru.ispras.fortress.expression.Node;
 import ru.ispras.fortress.expression.NodeOperation;
 import ru.ispras.fortress.expression.NodeValue;
+import ru.ispras.fortress.expression.Nodes;
 import ru.ispras.fortress.expression.StandardOperation;
 import ru.ispras.fortress.transformer.NodeTransformer;
 import ru.ispras.fortress.transformer.ReduceOptions;
@@ -130,10 +131,7 @@ final class DistanceCalculator {
         }
       }
 
-      return new NodeOperation(
-          StandardOperation.ADD,
-          operands
-          );
+      return Nodes.ADD(operands);
     }
   }
 
@@ -158,17 +156,14 @@ final class DistanceCalculator {
           InvariantChecks.checkTrue(((NodeOperation) current).getOperandCount() == 1);
           operands.add(((NodeOperation) current).getOperand(0));
         } else {
-          operands.add(new NodeOperation(StandardOperation.MINUS, current));
+          operands.add(Nodes.MINUS(current));
         }
       }
 
-      return new NodeOperation(
-          StandardOperation.ADD,
-          operands
-          );
+      return Nodes.ADD(operands);
     }
   }
-  
+
   private static final class MinimizeAddRule implements TransformerRule {
     @Override
     public boolean isApplicable(final Node expr) {
@@ -231,10 +226,7 @@ final class DistanceCalculator {
         return newOperands.get(0);
       }
 
-      return new NodeOperation(
-          StandardOperation.ADD,
-          newOperands
-          );
+      return Nodes.ADD(newOperands);
     }
 
     private boolean isNedation(final Node node1, final Node node2) {
