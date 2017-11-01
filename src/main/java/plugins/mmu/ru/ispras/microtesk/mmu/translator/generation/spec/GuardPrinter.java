b5021162dd82d1e4192e6c19a265a62ace133985
Andrei Tatarnikov <andrewt@ispras.ru>
2017-11-01 18:47:20 +0300

    Small update
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/generation/spec/GuardPrinter.java
+++ src/main/java/plugins/mmu/ru/ispras/microtesk/mmu/translator/generation/spec/GuardPrinter.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2015-2017 ISP RAS (http://www.ispras.ru)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -13,8 +13,6 @@
  */
 
 package ru.ispras.microtesk.mmu.translator.generation.spec;
-
-import static ru.ispras.fortress.util.InvariantChecks.checkNotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,7 +33,6 @@ import ru.ispras.microtesk.mmu.translator.ir.AttributeRef;
 import ru.ispras.microtesk.mmu.translator.ir.Buffer;
 import ru.ispras.microtesk.mmu.translator.ir.Ir;
 import ru.ispras.microtesk.mmu.translator.ir.Segment;
-import ru.ispras.microtesk.utils.StringUtils;
 
 final class GuardPrinter {
   private final Ir ir;
@@ -45,9 +42,9 @@ final class GuardPrinter {
   private final Condition condFalse;
 
   public GuardPrinter(final Ir ir, final String context, final Node expr) {
-    checkNotNull(ir);
-    checkNotNull(context);
-    checkNotNull(expr);
+    InvariantChecks.checkNotNull(ir);
+    InvariantChecks.checkNotNull(context);
+    InvariantChecks.checkNotNull(expr);
 
     this.ir = ir;
     this.context = context;
