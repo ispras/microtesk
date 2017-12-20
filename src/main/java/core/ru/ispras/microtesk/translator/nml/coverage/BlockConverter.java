940b06573ac16024ac22eb2873f80f9a0d884e0b
Andrei Tatarnikov <andrewt@ispras.ru>
2017-12-20 15:26:35 +0300

    Refactoring in SSA...
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/translator/nml/coverage/BlockConverter.java
+++ src/main/java/core/ru/ispras/microtesk/translator/nml/coverage/BlockConverter.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2014-2015 ISP RAS (http://www.ispras.ru)
- * 
+ * Copyright 2014-2017 ISP RAS (http://www.ispras.ru)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -17,10 +17,8 @@ package ru.ispras.microtesk.translator.nml.coverage;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import ru.ispras.fortress.data.DataType;
@@ -136,106 +134,5 @@ public final class BlockConverter {
       name = "";
     }
     return new NodeVariable(name, DataType.BOOLEAN);
-  }
-}
-
-final class SsaConverter {
-  final Map<String, SsaForm> ssa;
-  final Map<String, Constraint> constraints;
-  final Map<String, Block> blocks;
-  final List<Block> terminals;
-  final NodeTransformer xform;
-
-  SsaConverter(Collection<Constraint> constraints) {
-    this.ssa = new HashMap<>();
-    this.constraints = new HashMap<>();
-    this.blocks = new HashMap<>();
-    this.terminals = new ArrayList<>();
-    this.xform = new NodeTransformer();
-
-    for (Constraint c : constraints) {
-      this.constraints.put(c.getName(), c);
-    }
-
-    final TransformerRule rule = new TransformerRule() {
-      @Override
-      public boolean isApplicable(Node node) {
-        return Version.hasBakedVersion(node);
-      }
-
-      @Override
-      public Node apply(Node node) {
-        return Version.undoVersion((NodeVariable) node);
-      }
-    };
-    this.xform.addRule(Node.Kind.VARIABLE, rule);
-  }
-
-  public SsaForm convert(String name) {
-    if (ssa.containsKey(name)) {
-      return ssa.get(name);
-    }
-    final Block entry = restoreBlock(name);
-    final SsaForm ssaForm = new SsaForm(entry, terminals.get(0), blocks.values());
-
-    blocks.clear();
-    terminals.clear();
-    ssa.put(name, ssaForm);
-
-    return ssaForm;
-  }
-
-  private Block restoreBlock(String name) {
-    if (blocks.containsKey(name)) {
-      return blocks.get(name);
-    }
-    Block block = null;
-    final Block.Builder builder = new Block.Builder();
-    final Formulas formulas = (Formulas) constraints.get(name).getInnerRep();
-
-    for (Node node : formulas.exprs()) {
-      final NodeOperation op = (NodeOperation) node;
-      if (ExprUtils.isOperation(op, SsaOperation.THIS_CALL)) {
-        block = Block.newSingleton(op);
-      } else if (ExprUtils.isOperation(op, SsaOperation.CALL)) {
-        final NodeOperation call = BlockConverter.convertCall(op, this.xform);
-        block = Block.newSingleton(call);
-      } else if (ExprUtils.isOperation(op, SsaOperation.PHI)) {
-        block = Block.newPhi();
-      } else if (!ExprUtils.isOperation(op, SsaOperation.BLOCK)) {
-        builder.add((NodeOperation) Utility.transform(op, this.xform));
-      }
-    }
-    if (block == null) {
-      block = builder.build();
-    }
-    blocks.put(name, block);
-
-    final List<GuardedBlock> children = restoreChildren(formulas);
-    if (!children.isEmpty()) {
-      block.setChildren(children);
-    } else {
-      terminals.add(block);
-    }
-    return block;
-  }
-
-  private List<GuardedBlock> restoreChildren(Formulas formulas) {
-    final List<GuardedBlock> children = new ArrayList<>();
-    for (Node node : formulas.exprs()) {
-      if (ExprUtils.isOperation(node, SsaOperation.BLOCK)) {
-        final NodeOperation op = (NodeOperation) node;
-        final String name = operandName(op, 0);
-        final Node guard = Utility.transform(op.getOperand(1), this.xform);
-        final GuardedBlock guarded =
-            new GuardedBlock(name, guard, restoreBlock(name));
-        children.add(guarded);
-      }
-    }
-    return children;
-  }
-
-  private static String operandName(NodeOperation node, int i) {
-    return ((NodeVariable) node.getOperand(i)).getName();
   }
 }
