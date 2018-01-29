143596da4a4d82512e768efda320ef7d55b71382
Andrei Tatarnikov <andrewt@ispras.ru>
2018-01-29 15:48:04 +0300

    Fixes in tests
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/test/java/core/ru/ispras/microtesk/model/minimips/ExternalLabels2TestCase.java
+++ src/test/java/core/ru/ispras/microtesk/model/minimips/ExternalLabels2TestCase.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2017 ISP RAS (http://www.ispras.ru)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -27,6 +27,6 @@ public class ExternalLabels2TestCase extends MiniMipsTest {
 
     Assert.assertEquals(1,  statistics.getPrograms());
     Assert.assertEquals(1,  statistics.getSequences());
-    Assert.assertEquals(26, statistics.getInstructions());
+    Assert.assertEquals(24, statistics.getInstructions());
   }
 }
