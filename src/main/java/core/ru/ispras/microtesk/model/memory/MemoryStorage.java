7cf11303072d3311bdbd191f02b32e3c74b9e032
Andrei Tatarnikov <andrewt@ispras.ru>
2018-02-05 16:28:22 +0300

    Removed dependency on BitVectorAlgorithm
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/model/memory/MemoryStorage.java
+++ src/main/java/core/ru/ispras/microtesk/model/memory/MemoryStorage.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2014-2017 ISP RAS (http://www.ispras.ru)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -20,7 +20,6 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import ru.ispras.fortress.data.types.bitvector.BitVector;
-import ru.ispras.fortress.data.types.bitvector.BitVectorAlgorithm;
 import ru.ispras.fortress.util.InvariantChecks;
 import ru.ispras.microtesk.settings.GeneratorSettings;
 import ru.ispras.microtesk.settings.MemorySettings;
@@ -81,7 +80,8 @@ final class MemoryStorage implements MemoryDevice {
       }
 
       final BitVector adjusted = BitVector.newEmpty(bitSize);
-      BitVectorAlgorithm.copy(bv, adjusted);
+      adjusted.assign(bv);
+
       return adjusted;
     }
 
