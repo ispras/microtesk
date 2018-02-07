07f609a1bd68ee062d32acaced174fd843da5afe
Andrei Tatarnikov <andrewt@ispras.ru>
2018-02-07 18:14:53 +0300

    New miniMIPS example: testdata.rb + corresponding JUnit test.
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

---
+++ src/main/arch/demo/minimips/templates/testdata.rb
@@ -0,0 +1,68 @@
+#
+# Copyright 2018 ISP RAS (http://www.ispras.ru)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_relative 'minimips_base'
+
+#
+# Description:
+#
+# This test template demonstrates how to use test data generators that are
+# independent of the context and produces multiple sets of test data.
+#
+class TestDataTemplate < MiniMipsBaseTemplate
+
+  def pre
+    super
+
+    #
+    # Test case level prologue
+    #
+    prologue {
+      align 4
+    }
+
+    #
+    # Test case level epilogue
+    #
+    epilogue {
+      nop
+      nop
+    }
+
+    #
+    # Start address
+    #
+    org 0x00020000
+  end
+
+  def run
+    # Test data for individual registers.
+    sequence {
+      add reg(8),  (reg(9)  do testdata('boundary') end),
+                   (reg(10) do testdata('boundary') end)
+
+      sub reg(11), (reg(12) do testdata('boundary') end),
+                   (reg(13) do testdata('boundary') end)
+    }.run
+
+    # Test data for entire instructions.
+    sequence {
+      add reg(8),  reg(9),  reg(10) do testdata('boundary') end
+      sub reg(11), reg(12), reg(13) do testdata('boundary') end
+    }.run
+  end
+
+end
