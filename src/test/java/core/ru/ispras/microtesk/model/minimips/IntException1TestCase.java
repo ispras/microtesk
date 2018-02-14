7776d2f35538b398c0a64de8f6dfb3046d0bd546
Andrei Tatarnikov <andrewt@ispras.ru>
2018-02-14 14:59:05 +0300

    IntException1TestCase - fixes statistics.
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/test/java/core/ru/ispras/microtesk/model/minimips/IntException1TestCase.java
+++ src/test/java/core/ru/ispras/microtesk/model/minimips/IntException1TestCase.java
@@ -27,6 +27,6 @@ public class IntException1TestCase extends MiniMipsTest {
 
     Assert.assertEquals(1, statistics.getPrograms());
     Assert.assertEquals(4, statistics.getSequences());
-    Assert.assertEquals(45, statistics.getInstructions());
+    Assert.assertEquals(56, statistics.getInstructions());
   }
 }
