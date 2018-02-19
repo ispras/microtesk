3458ed8a250ba08ab3726b4735ba615fe92784fc
Andrei Tatarnikov <andrewt@ispras.ru>
2018-02-19 11:24:18 +0300

    WhyML code generator stub
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/translator/nml/generation/whyml/WhymlGenerator.java
+++ src/main/java/core/ru/ispras/microtesk/translator/nml/generation/whyml/WhymlGenerator.java
@@ -1,11 +1,34 @@
+/*
+ * Copyright 2018 ISP RAS (http://www.ispras.ru)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package ru.ispras.microtesk.translator.nml.generation.whyml;
 
+import ru.ispras.fortress.util.InvariantChecks;
+import ru.ispras.microtesk.translator.Translator;
 import ru.ispras.microtesk.translator.TranslatorHandler;
 import ru.ispras.microtesk.translator.nml.ir.Ir;
 
-public class WhymlGenerator implements TranslatorHandler<Ir> {
+public final class WhymlGenerator implements TranslatorHandler<Ir> {
+  private final Translator<Ir> translator;
+
+  public WhymlGenerator(final Translator<Ir> translator) {
+    InvariantChecks.checkNotNull(translator);
+    this.translator = translator;
+  }
+
   @Override
-  public void processIr(Ir ir) {
+  public void processIr(final Ir ir) {
 
   }
 }
