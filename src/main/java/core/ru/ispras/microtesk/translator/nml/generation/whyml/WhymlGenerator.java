a5fac5e41cb544743368d8e56d2d7b0cf6aecc1f
Andrei Tatarnikov <andrewt@ispras.ru>
2018-02-19 11:23:04 +0300

    fortressVersion=0.4.30-beta-180219 and testbaseVersion=0.0.19-alpha-180219
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

---
+++ src/main/java/core/ru/ispras/microtesk/translator/nml/generation/whyml/WhymlGenerator.java
@@ -0,0 +1,11 @@
+package ru.ispras.microtesk.translator.nml.generation.whyml;
+
+import ru.ispras.microtesk.translator.TranslatorHandler;
+import ru.ispras.microtesk.translator.nml.ir.Ir;
+
+public class WhymlGenerator implements TranslatorHandler<Ir> {
+  @Override
+  public void processIr(Ir ir) {
+
+  }
+}
