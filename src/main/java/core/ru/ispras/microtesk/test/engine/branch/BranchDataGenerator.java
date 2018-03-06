62fc13fcf9548ad81023f14651213a2cb91f8264
Andrei Tatarnikov <andrewt@ispras.ru>
2018-03-06 12:03:14 +0300

    BranchDataGenerator - common code moved.
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/test/engine/branch/BranchDataGenerator.java
+++ src/main/java/core/ru/ispras/microtesk/test/engine/branch/BranchDataGenerator.java
@@ -14,21 +14,26 @@
 
 package ru.ispras.microtesk.test.engine.branch;
 
+import ru.ispras.fortress.data.DataType;
 import ru.ispras.fortress.data.DataTypeId;
+import ru.ispras.fortress.data.types.bitvector.BitVector;
 import ru.ispras.fortress.expression.Node;
+import ru.ispras.fortress.expression.NodeValue;
 import ru.ispras.fortress.util.InvariantChecks;
+
 import ru.ispras.testbase.TestBaseQuery;
 import ru.ispras.testbase.TestData;
 import ru.ispras.testbase.generator.DataGenerator;
 import ru.ispras.testbase.generator.Utils;
 import ru.ispras.testbase.knowledge.iterator.Iterator;
+import ru.ispras.testbase.knowledge.iterator.SingleValueIterator;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * {@link BranchDataGenerator} is a base class for test data generators for conditional branch
- * instructions.
+ * {@link BranchDataGenerator} is a base class for test data generators
+ * for conditional branch instructions.
  *
  * @author <a href="mailto:kamkin@ispras.ru">Alexander Kamkin</a>
  */
@@ -37,24 +42,6 @@ public abstract class BranchDataGenerator implements DataGenerator {
   public static final String PARAM_CONDITION_THEN = "true";
   public static final String PARAM_CONDITION_ELSE = "false";
   public static final String PARAM_STREAM = "stream";
-
-  public static Map<String, Node> extractUnknown(final TestBaseQuery query) {
-    InvariantChecks.checkNotNull(query);
-
-    final Map<String, Node> result =  new LinkedHashMap<>();
-    for (final Map.Entry<String, Node> e : query.getBindings().entrySet()) {
-      final Node value = e.getValue();
-
-      if (/* Known registers are considered to be unknown (stream based initialization) */
-          value.getDataType().getTypeId() != DataTypeId.LOGIC_INTEGER
-          /* Known immediate values are considered to be known */
-          || Utils.isUnknownVariable(value)) {
-        result.put(e.getKey(), value);
-      }
-    }
-
-    return result;
-  }
 
   @Override
   public final boolean isSuitable(final TestBaseQuery query) {
@@ -98,5 +85,47 @@ public abstract class BranchDataGenerator implements DataGenerator {
     }
 
     return null;
+  }
+
+  protected static Iterator<TestData> generate(
+      final TestBaseQuery query,
+      final Map<String, Long> values) {
+    InvariantChecks.checkNotNull(query, "Query is null");
+    InvariantChecks.checkNotNull(values, "Values are null");
+
+    final Map<String, Node> unknowns = extractUnknown(query);
+    final Map<String, Object> bindings = new LinkedHashMap<>();
+
+    for (final Map.Entry<String, Node> entry : unknowns.entrySet()) {
+      final String name = entry.getKey();
+
+      if (values.containsKey(name)) {
+        final long value = values.get(name);
+        final DataType type = entry.getValue().getDataType();
+        final BitVector data = BitVector.valueOf(value, type.getSize());
+
+        bindings.put(name, NodeValue.newBitVector(data));
+      }
+    }
+
+    return new SingleValueIterator<>(new TestData(BranchEngine.ID, bindings));
+  }
+
+  protected static Map<String, Node> extractUnknown(final TestBaseQuery query) {
+    InvariantChecks.checkNotNull(query);
+
+    final Map<String, Node> result =  new LinkedHashMap<>();
+    for (final Map.Entry<String, Node> e : query.getBindings().entrySet()) {
+      final Node value = e.getValue();
+
+      if (/* Known registers are considered to be unknown (stream based initialization) */
+          !value.isType(DataTypeId.LOGIC_INTEGER)
+          /* Known immediate values are considered to be known */
+              || Utils.isUnknownVariable(value)) {
+        result.put(e.getKey(), value);
+      }
+    }
+
+    return result;
   }
 }
