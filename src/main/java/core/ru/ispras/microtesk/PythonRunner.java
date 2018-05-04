f750b96ba0910f34459d318b64e43862656b4e35
Andrei Tatarnikov <andrewt0301@gmail.com>
2018-05-04 23:40:25 +0300

    Included Jython in the build
    Signed-off-by: Andrei Tatarnikov <andrewt0301@gmail.com>

--- src/main/java/core/ru/ispras/microtesk/PythonRunner.java
+++ src/main/java/core/ru/ispras/microtesk/PythonRunner.java
@@ -14,9 +14,9 @@
 
 package ru.ispras.microtesk;
 
-/* // EXCLUDED FROM RELEASE (NOT READY)
+///* // EXCLUDED FROM RELEASE (NOT READY)
 import org.python.util.PythonInterpreter;
-*/
+//*/
 
 import ru.ispras.fortress.util.InvariantChecks;
 import ru.ispras.microtesk.options.Options;
@@ -43,7 +43,7 @@ final class PythonRunner {
    *         of the engines invoked by the script decided to abort generation.
    */
   public static void run(final Options options, final String templateFile) throws Throwable {
-    /* // EXCLUDED FROM RELEASE (NOT READY)
+    ///* // EXCLUDED FROM RELEASE (NOT READY)
     InvariantChecks.checkNotNull(options);
     InvariantChecks.checkNotNull(templateFile);
     
@@ -71,6 +71,6 @@ final class PythonRunner {
         interpreter.close();
       }
     }
-    */
+    //*/
   }
 }
