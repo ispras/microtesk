5737c852d97a48c76dfbbe8d084b3dc4627af811
Andrei Tatarnikov <andrewt@ispras.ru>
2018-06-20 12:02:27 +0300

    Permutator 'exhaustive' is aliased to 'full'.
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/test/sequence/GeneratorConfig.java
+++ src/main/java/core/ru/ispras/microtesk/test/sequence/GeneratorConfig.java
@@ -69,6 +69,7 @@ public final class GeneratorConfig<T> {
     compositors.put("random", CompositorRandom.class);
 
     permutators.put("exhaustive", PermutatorExhaustive.class);
+    permutators.put("full", PermutatorExhaustive.class); // Alias for exhaustive
     permutators.put("random", PermutatorRandom.class);
     permutators.put("trivial", PermutatorTrivial.class);
 
