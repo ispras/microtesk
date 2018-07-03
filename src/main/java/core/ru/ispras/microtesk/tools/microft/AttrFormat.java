0cf0362380fcb986426f821a9f86dd32632df764
Artem Kotsynyak <kotsynyak@ispras.ru>
2018-07-03 03:18:59 +0300

    [microft] AttrFormat: Entity-based processing
    Signed-off-by: Artem Kotsynyak <kotsynyak@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/tools/microft/AttrFormat.java
+++ src/main/java/core/ru/ispras/microtesk/tools/microft/AttrFormat.java
@@ -43,7 +43,7 @@ class AttrFormat extends IrInspector.Attribute {
 
   @Override
   public JsonValue get(final List<PrimitiveAND> primitives, final Map<String, JsonValue> env) {
-    final String s = evaluateAttribute(getName(), Decl.create(primitives));
+    final String s = evaluateAttribute(getName(), Entity.create(primitives));
     if (s != null) {
       return JsonUtil.createString(s);
     }
@@ -123,6 +123,130 @@ class AttrFormat extends IrInspector.Attribute {
     return node;
   }
 
+  private static Node evaluateCall(final StatementAttributeCall call, final Decl decl) {
+    if (call.isArgumentCall()) {
+      PrimitiveAND arg = decl.getArgument(call.getCalleeName());
+      if (arg == null) {
+        final Primitive p = decl.getParameter(call.getCalleeName());
+        if (p.isOrRule() || p.getKind() != Primitive.Kind.OP) {
+          return NodeValue.newString(String.format("${%s:%s}", call.getCalleeName(), p.getName()));
+        }
+        arg = (PrimitiveAND) p;
+      }
+      return NodeValue.newString(
+        evaluateAttribute(call.getAttributeName(), decl.create(arg)));
+    } else if (call.isInstanceCall()) {
+      // TODO
+    }
+    return null;
+  }
+
+  private static String evaluateAttribute(final String attr, final Entity e) {
+    final StatementFormat fmt =
+      findStatement(e.getType(), attr, StatementFormat.class);
+
+    final StatementAttributeCall call =
+      findStatement(e.getType(), attr, StatementAttributeCall.class);
+
+    if (fmt != null) {
+      return evaluateFormat(fmt, e);
+    } else if (call != null) {
+      return evaluateCall(call, e).toString();
+    }
+    return null;
+  }
+
+  private static String evaluateFormat(final StatementFormat fmt, final Entity e) {
+    final String origin = fmt.getFormat();
+    final StringBuilder builder = new StringBuilder();
+
+    final List<Node> values = evaluateAll(fmt.getArguments(), e);
+    final IterablePair<FormatMarker, Node> args =
+      IterablePair.create(fmt.getMarkers(), values);
+
+    int offset = 0;
+    for (final Pair<FormatMarker, Node> token : args) {
+      builder.append(origin.substring(offset, token.first.getStart()));
+      builder.append(format(token.first, token.second));
+
+      offset = token.first.getEnd();
+    }
+    builder.append(origin.substring(offset, origin.length()));
+
+    return builder.toString();
+  }
+
+  private static List<Node> evaluateAll(final List<Node> nodes, final Entity e) {
+    final List<Node> evaluated = new ArrayList<>(nodes.size());
+    for (final Node node : nodes) {
+      evaluated.add(evaluate(node, e));
+    }
+    return evaluated;
+  }
+
+  private static Node evaluate(final Node node, final Entity e) {
+    if (node.getUserData() instanceof StatementAttributeCall) {
+      final StatementAttributeCall call =
+        (StatementAttributeCall) node.getUserData();
+      final Node eval = evaluateCall(call, e);
+      return eval;
+    }
+    /*
+    if (ExprUtils.isOperation(node, StandardOperation.ITE)) {
+      final Iterator<Node> it = valuesOf((NodeOperation) node).iterator();
+
+      final StringBuilder builder = new StringBuilder("${");
+      builder.append(it.next().toString());
+      while (it.hasNext()) {
+        builder.append("|").append(it.next().toString());
+      }
+      builder.append("}");
+
+      return NodeValue.newString(builder.toString());
+    }
+    //*/
+    if (ExprUtils.isVariable(node)) {
+      final NodeVariable var = (NodeVariable) node;
+      final Map<String, Primitive> params = e.getType().getArguments();
+      if (params.containsKey(var.getName())) {
+        final Primitive p = params.get(var.getName());
+        return NodeValue.newString(
+          String.format("${%s:%s}", var.getName(), p.getName()));
+      }
+    }
+    return node;
+  }
+
+  private static Node evaluateCall(final StatementAttributeCall call, final Entity e) {
+    final String hostName = call.getCalleeName();
+
+    final Entity host;
+    if (call.isArgumentCall()) {
+      host = e.getTypeArguments().get(hostName);
+      if (host == null || isUnboundMode(host)) {
+        final Primitive p = e.getType().getArguments().get(hostName);
+        return NodeValue.newString(
+          String.format("${%s:%s}", hostName, p.getName()));
+      }
+    } else if (call.isInstanceCall()) {
+      host = Entity.create(call.getCalleeInstance(), e);
+    } else {
+      throw new IllegalStateException();
+    }
+    return NodeValue.newString(
+      evaluateAttribute(call.getAttributeName(), host));
+  }
+
+  private static boolean isUnboundMode(final Entity e) {
+    return e.isTerminal() &&
+      e.hasUnbound() &&
+      e.getType().getKind() == Primitive.Kind.MODE ;
+  }
+
+  private static String format(final FormatMarker fmt, final Node node) {
+    return node.toString(); 
+  }
+
   private static List<Node> valuesOf(final NodeOperation ite) {
     final List<Node> values = new ArrayList<>();
     collectValues(ite, values);
@@ -142,28 +266,6 @@ class AttrFormat extends IrInspector.Attribute {
     } else {
       values.add(node);
     }
-  }
-
-  private static Node evaluateCall(final StatementAttributeCall call, final Decl decl) {
-    if (call.isArgumentCall()) {
-      PrimitiveAND arg = decl.getArgument(call.getCalleeName());
-      if (arg == null) {
-        final Primitive p = decl.getParameter(call.getCalleeName());
-        if (p.isOrRule() || p.getKind() != Primitive.Kind.OP) {
-          return NodeValue.newString(String.format("${%s:%s}", call.getCalleeName(), p.getName()));
-        }
-        arg = (PrimitiveAND) p;
-      }
-      return NodeValue.newString(
-        evaluateAttribute(call.getAttributeName(), decl.create(arg)));
-    } else if (call.isInstanceCall()) {
-      // TODO
-    }
-    return null;
-  }
-
-  private static String format(final FormatMarker fmt, final Node node) {
-    return node.toString(); 
   }
 
   private static <T extends Statement> T findStatement(final PrimitiveAND p, final String name, final Class<T> c) {
