3f4cd75517146c3ffe9f85ba373799df3ca98353
Artem Kotsynyak <kotsynyak@ispras.ru>
2018-11-24 20:50:55 +0300

    [mir,nml] implement translation entry point
    Signed-off-by: Artem Kotsynyak <kotsynyak@ispras.ru>

---
+++ src/main/java/core/ru/ispras/microtesk/translator/mir/MirTransHandler.java
@@ -0,0 +1,45 @@
+package ru.ispras.microtesk.translator.mir;
+
+import java.util.Map;
+
+import ru.ispras.microtesk.translator.Translator;
+import ru.ispras.microtesk.translator.TranslatorHandler;
+import ru.ispras.microtesk.translator.nml.ir.Ir;
+import ru.ispras.microtesk.translator.nml.ir.primitive.Attribute;
+import ru.ispras.microtesk.translator.nml.ir.primitive.Primitive;
+import ru.ispras.microtesk.translator.nml.ir.primitive.PrimitiveAND;
+import ru.ispras.microtesk.utils.NamePath;
+
+public class MirTransHandler implements TranslatorHandler<Ir> {
+  private final Translator<Ir> translator;
+  private final Map<Ir, Map<NamePath, MirContext>> cache =
+      new java.util.IdentityHashMap<>();
+
+  public MirTransHandler(final Translator<Ir> t) {
+    this.translator = t;
+  }
+
+  @Override
+  public void processIr(final Ir ir) {
+    final Map<NamePath, MirContext> mirs = loadMir(ir);
+    for (final Primitive p : ir.getOps().values()) {
+      if (!p.isOrRule()) {
+        final PrimitiveAND item = (PrimitiveAND) p;
+        for (final Attribute attr : item.getAttributes().values()) {
+          if (attr.getKind().equals(Attribute.Kind.ACTION)) {
+            mirs.put(
+              NamePath.get(item.getName(), attr.getName()),
+              NmlIrTrans.translate(attr.getStatements()));
+          }
+        }
+      }
+    }
+  }
+
+  private Map<NamePath, MirContext> loadMir(final Ir ir) {
+    if (!cache.containsKey(ir)) {
+      cache.put(ir, new java.util.HashMap<NamePath, MirContext>());
+    }
+    return cache.get(ir);
+  }
+}
