401c13cd3bab0a3a2d3018057944548426948a66
Artem Kotsynyak <kotsynyak@ispras.ru>
2019-03-17 01:40:33 +0300

    [mir,nml] MirTransHandler: serialize MIR
    Signed-off-by: Artem Kotsynyak <kotsynyak@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/translator/mir/MirTransHandler.java
+++ src/main/java/core/ru/ispras/microtesk/translator/mir/MirTransHandler.java
@@ -1,7 +1,12 @@
 package ru.ispras.microtesk.translator.mir;
 
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
+import ru.ispras.microtesk.Logger;
 import ru.ispras.microtesk.translator.Translator;
 import ru.ispras.microtesk.translator.TranslatorHandler;
 import ru.ispras.microtesk.translator.nml.ir.Ir;
@@ -22,17 +27,27 @@ public class MirTransHandler implements TranslatorHandler<Ir> {
   @Override
   public void processIr(final Ir ir) {
     final Map<NamePath, MirContext> mirs = loadMir(ir);
-    for (final Primitive p : ir.getOps().values()) {
-      if (!p.isOrRule()) {
-        final PrimitiveAND item = (PrimitiveAND) p;
-        for (final Attribute attr : item.getAttributes().values()) {
-          if (attr.getKind().equals(Attribute.Kind.ACTION)) {
-            mirs.put(
-              NamePath.get(item.getName(), attr.getName()),
-              NmlIrTrans.translate(item, attr.getStatements()));
+    final Path path = Paths.get(translator.getOutDir(), ir.getModelName() + ".zip");
+    try (final ArchiveWriter archive = new ArchiveWriter(path)) {
+      for (final Primitive p : ir.getOps().values()) {
+        if (!p.isOrRule()) {
+          final PrimitiveAND item = (PrimitiveAND) p;
+          for (final Attribute attr : item.getAttributes().values()) {
+            if (attr.getKind().equals(Attribute.Kind.ACTION)) {
+              final MirContext mir = NmlIrTrans.translate(item, attr.getStatements());
+              final NamePath name = NamePath.get(item.getName(), attr.getName());
+              mirs.put(name, mir);
+
+              try (final Writer writer = archive.newText(name.resolve("mir").toString())) {
+                final MirText text = new MirText(mir);
+                writer.write(text.toString());
+              }
+            }
           }
         }
       }
+    } catch (final IOException e) {
+      Logger.error("Failed to store MIR '%s': %s", path.toString(), e.toString());
     }
   }
 
