b6cfd2c3498172132ed2e2e1368116c2a5a5837d
Artem Kotsynyak <kotsynyak@ispras.ru>
2019-03-23 01:37:11 +0300

    [mir,io] MirParser: recursive descent complete
    Signed-off-by: Artem Kotsynyak <kotsynyak@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/translator/mir/MirParser.java
+++ src/main/java/core/ru/ispras/microtesk/translator/mir/MirParser.java
@@ -40,20 +40,16 @@ public class MirParser {
       }
     }
     MirBlock block = null;
-    final Map<String, Parser> parsers = standardParsers(blocks);
 
     for (final String line : lines) {
       if (line.matches("\\w+:")) {
         final String name = line.substring(0, line.indexOf(":"));
         block = blocks.get(name);
       } else {
-        final String[] tokens = line.replaceAll("[,<>()]", "").trim().split("\\s+");
-        final Parser parser = parsers.get(tokens[0]);
-        if (parser != null) {
-          block.append(parser.parse(tokens));
-        } else {
-          // TODO
-        }
+        final String input =
+            line.replaceAll("[,()<>]", "").replaceAll("([\\[\\]\\{\\}])", " $1 ").trim();
+        final Parser2 parser = new Parser2(new Scanner(input), blocks);
+        block.append(parser.nextInsn());
       }
     }
     return ctx;
@@ -187,9 +183,31 @@ public class MirParser {
 
   private static final class Parser2 {
     private final Scanner scanner;
+    private final Map<String, MirBlock> blocks;
 
-    Parser2(final Scanner s) {
+    Parser2(final Scanner s, final Map<String, MirBlock> blocks) {
       this.scanner = s;
+      this.blocks = blocks;
+    }
+
+    public Instruction nextInsn() {
+      final Scanner s = this.scanner;
+      for (final Instruction insn = nextAssign(s); insn != null; ) {
+        return insn;
+      }
+      for (final Branch insn = nextBranch(blocks, s); insn != null; ) {
+        return insn;
+      }
+      if (s.hasNext("call")) {
+        return nextCall("", s);
+      }
+      for (final Return insn = nextReturn(s); insn != null; ) {
+        return insn;
+      }
+      for (final Store insn = nextStore(s); insn != null; ) {
+        return insn;
+      }
+      throw new IllegalArgumentException();
     }
 
     public static Token nextToken(final TokenKind kind, final Scanner s) {
@@ -226,29 +244,24 @@ public class MirParser {
 
     private static MirStruct nextStruct(final Scanner s) {
       if (TokenKind.LBRACE.nextIn(s)) {
-        nextToken(TokenKind.LBRACE, s);
+        TokenKind.LBRACE.next(s);
 
-        final Map<String, TyRef> fields =
-            nextFields(s, new java.util.LinkedHashMap<String, TyRef>());
+        final Map<String, TyRef> fields = new java.util.LinkedHashMap<>();
+        while (!TokenKind.RBRACE.nextIn(s)) {
+          final MirTy type = nextType(s);
+          final String name = TokenKind.IDENT.next(s);
+
+          fields.put(name, new TyRef(type));
+        }
+        TokenKind.RBRACE.next(s);
 
         return new MirStruct(fields);
       }
       return null;
     }
 
-    private static Map<String, TyRef> nextFields(final Scanner s, final Map<String, TyRef> fields) {
-      if (nextToken(TokenKind.RBRACE, s) != null) {
-        return fields;
-      }
-      final MirTy type = nextType(s);
-      final String name = TokenKind.IDENT.next(s);
-
-      fields.put(name, new TyRef(type));
-      return nextFields(s, fields);
-    }
-
     public static Call nextCall(final String target, final Scanner s) {
-      s.skip("call");
+      TokenKind.IDENT.next(s);
       final MirTy retty = nextType(s);
       final Local ret;
       if (target.isEmpty()) {
@@ -258,40 +271,198 @@ public class MirParser {
       }
 
       final String calleeName = TokenKind.LOCAL.next(s);
-      TokenKind.LPAREN.next(s);
 
-      final List<Operand> args = nextArguments(s, new java.util.ArrayList<Operand>());
-      final List<MirTy> params = new java.util.ArrayList<>(args.size());
-      for (final Operand arg : args) {
-        params.add(arg.getType());
+      final List<Operand> args = new java.util.ArrayList<>();
+      final List<MirTy> params = new java.util.ArrayList<>();
+      while (s.hasNext()) {
+        final MirTy type = nextType(s);
+        final Operand arg = nextOperand(type, s);
+        params.add(type);
+        args.add(arg);
       }
+
       final Operand callee =
           new Local(Integer.valueOf(calleeName), new FuncTy(retty, params));
 
       return new Call(callee, args, ret);
     }
 
-    public static List<Operand> nextArguments(final Scanner s, final List<Operand> args) {
-      if (nextToken(TokenKind.RPAREN, s) != null) {
-        return args;
-      }
-      final MirTy type = nextType(s);
-      final Operand arg = nextOperand(type, s);
-
-      args.add(arg);
-      return nextArguments(s, args);
-    }
-
     public static Operand nextOperand(final MirTy type, final Scanner s) {
       if (TokenKind.LOCAL.nextIn(s)) {
         return new Local(Integer.valueOf(TokenKind.LOCAL.next(s)), type);
       }
-      return new Constant(type.getSize(), Integer.valueOf(s.next("\\d+")));
+      try {
+        return new Constant(type.getSize(), s.nextInt());
+      } catch (final java.util.InputMismatchException e) { // FIXME
+        return new Constant(type.getSize(), 0);
+      }
     }
 
     public static String nextLabel(final Scanner s) {
-      s.skip("label");
+      TokenKind.IDENT.next(s);
       return TokenKind.LOCAL.next(s);
+    }
+
+    public static Lvalue nextMemory(final MirTy type, final Scanner s) {
+      final String name = TokenKind.IDENT.next(s);
+      Lvalue mem = new Static(name, type);
+
+      while (s.hasNext()) {
+        if (TokenKind.DOT.nextIn(s)) {
+          TokenKind.DOT.next(s);
+          mem = new Field(mem, TokenKind.IDENT.next(s));
+        } else if (TokenKind.LBRACKET.nextIn(s)) {
+          TokenKind.LBRACKET.next(s);
+          final Operand index = nextOperand(new IntTy(64), s);
+          mem = new Index(mem, index);
+          TokenKind.RBRACKET.next(s);
+        }
+      }
+      return mem;
+    }
+
+    public static Store nextStore(final Scanner s) {
+      if (s.hasNext("store")) {
+        TokenKind.IDENT.next(s);
+
+        final MirTy srcty = nextType(s);
+        final Operand src = nextOperand(srcty, s);
+        final MirTy memty = nextType(s);
+        final Lvalue mem = nextMemory(memty, s);
+
+        return new Store(mem, src);
+      }
+      return null;
+    }
+
+    public static Instruction nextAssign(final Scanner s) {
+      if (TokenKind.LOCAL.nextIn(s)) {
+        final String name = TokenKind.LOCAL.next(s);
+        TokenKind.EQUALS.next(s);
+
+        if (s.hasNext("load")) {
+          return nextLoad(name, s);
+        }
+        if (s.hasNext("call")) {
+          return nextCall(name, s);
+        }
+        if (s.hasNext("Sext") || s.hasNext("Zext")) {
+          return nextCast(name, s);
+        }
+        if (s.hasNext("Extract")) {
+          return nextExtract(name, s);
+        }
+        if (s.hasNext("Concat")) {
+          return nextConcat(name, s);
+        }
+
+        final MirTy lhsty = nextType(s);
+        final Lvalue lhs = new Local(Integer.valueOf(name), lhsty);
+
+        final String insn = TokenKind.IDENT.next(s);
+        final MirTy rhsty = nextType(s);
+        final Operand op1 = nextOperand(rhsty, s);
+        final Operand op2 = nextOperand(rhsty, s);
+
+        for (final BvOpcode opc : BvOpcode.values()) {
+          if (opc.name().equals(insn)) {
+            return new Assignment(lhs, opc.make(op1, op2));
+          }
+        }
+        for (final CmpOpcode opc : CmpOpcode.values()) {
+          if (opc.name().equals(insn)) {
+            return new Assignment(lhs, opc.make(op1, op2));
+          }
+        }
+        throw new IllegalArgumentException();
+      }
+      return null;
+    }
+
+    private static Instruction nextConcat(final String id, final Scanner s) {
+      TokenKind.IDENT.next(s);
+      final MirTy lhsty = nextType(s);
+      final Lvalue lhs = new Local(Integer.valueOf(id), lhsty);
+
+      final List<Operand> args = new java.util.ArrayList<>();
+      while (s.hasNext()) {
+        final MirTy type = nextType(s);
+        final Operand arg = nextOperand(type, s);
+        args.add(arg);
+      }
+      return new Concat(lhs, args);
+    }
+
+    private static Instruction nextExtract(final String id, final Scanner s) {
+      TokenKind.IDENT.next(s);
+      final MirTy lhsty = nextType(s);
+      final Lvalue lhs = new Local(Integer.valueOf(id), lhsty);
+
+      TokenKind.IDENT.next(s); // of
+      final MirTy rhsty = nextType(s);
+      final Operand rhs = nextOperand(rhsty, s);
+
+      final Operand hi = nextOperand(rhsty, s);
+      final Operand lo = nextOperand(rhsty, s);
+
+      return new Extract(lhs, rhs, lo, hi);
+    }
+
+    private static Instruction nextCast(final String id, final Scanner s) {
+      final String cast = TokenKind.IDENT.next(s);
+      final MirTy rhsty = nextType(s);
+      final Operand rhs = nextOperand(rhsty, s);
+      TokenKind.IDENT.next(s);
+
+      final MirTy lhsty = nextType(s);
+      final Local lhs = new Local(Integer.valueOf(id), lhsty);
+
+      if (cast.equals("Sext")) {
+        return new Sext(lhs, rhs);
+      }
+      return new Zext(lhs, rhs);
+    }
+
+    private static Load nextLoad(final String id, final Scanner s) {
+      TokenKind.IDENT.next(s);
+
+      final MirTy dstty = nextType(s);
+      final Local dst = new Local(Integer.valueOf(id), dstty);
+      final MirTy memty = nextType(s);
+      final Lvalue mem = nextMemory(memty, s);
+      return new Load(mem, dst);
+    }
+
+    public static Branch nextBranch(final Map<String, MirBlock> bbs, final Scanner s) {
+      if (s.hasNext("br")) {
+        TokenKind.IDENT.next(s);
+        if (s.hasNext("label")) {
+          final String label = nextLabel(s);
+          return new Branch(bbs.get(label).bb);
+        } else {
+          final MirTy type = nextType(s);
+          final Operand guard = nextOperand(type, s);
+          final String takenLabel = nextLabel(s);
+          final String otherLabel = nextLabel(s);
+
+          return new Branch(guard, bbs.get(takenLabel).bb, bbs.get(otherLabel).bb);
+        }
+      }
+      return null;
+    }
+
+    public static Return nextReturn(final Scanner s) {
+      if (s.hasNext("ret")) {
+        TokenKind.IDENT.next(s);
+        if (s.hasNext("void")) {
+          return new Return(null);
+        }
+        final MirTy type = nextType(s);
+        final Operand value = nextOperand(type, s);
+
+        return new Return(value);
+      }
+      return null;
     }
   }
 }
