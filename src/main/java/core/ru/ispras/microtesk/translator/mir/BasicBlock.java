e452294a4e6f973bbf0f118aa43c220042c1bf16
Artem Kotsynyak <kotsynyak@ispras.ru>
2019-04-11 01:30:48 +0300

    [mir] set origin per basic block
    Signed-off-by: Artem Kotsynyak <kotsynyak@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/translator/mir/BasicBlock.java
+++ src/main/java/core/ru/ispras/microtesk/translator/mir/BasicBlock.java
@@ -3,5 +3,6 @@ package ru.ispras.microtesk.translator.mir;
 import java.util.List;
 
 public final class BasicBlock {
+  public int origin = 0;
   public final List<Instruction> insns = new java.util.ArrayList<>();
 }
