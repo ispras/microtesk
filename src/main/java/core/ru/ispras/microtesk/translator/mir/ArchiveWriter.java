b303d529e9e4f70e46e3ce86a9af3227e58e7fbf
Artem Kotsynyak <kotsynyak@ispras.ru>
2019-04-28 11:34:41 +0300

    [mir,io] ArchiveWriter: fix identation
    Signed-off-by: Artem Kotsynyak <kotsynyak@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/translator/mir/ArchiveWriter.java
+++ src/main/java/core/ru/ispras/microtesk/translator/mir/ArchiveWriter.java
@@ -5,61 +5,60 @@ import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 final class ArchiveWriter implements Closeable {
-    public ArchiveWriter(final Path path) throws IOException {
-        this.zip = new ZipOutputStream(Files.newOutputStream(path));
-        this.writer = new java.io.OutputStreamWriter(zip, "UTF-8");
+  public ArchiveWriter(final Path path) throws IOException {
+    this.zip = new ZipOutputStream(Files.newOutputStream(path));
+    this.writer = new java.io.OutputStreamWriter(zip, "UTF-8");
+  }
+
+  public Writer newText(final String name) throws IOException {
+    return new TextWriter(name);
+  }
+
+  @Override
+  public void close() throws IOException {
+    writer.close();
+  }
+
+  private final ZipOutputStream zip;
+  private final Writer writer;
+
+  private final class TextWriter extends Writer {
+    private ZipEntry entry;
+
+    TextWriter(final String name) throws IOException {
+      this.entry = new ZipEntry(name);
+      zip.putNextEntry(entry);
     }
 
-    public Writer newText(final String name) throws IOException {
-        return new TextWriter(name);
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+      if (entry != null) {
+        writer.write(cbuf, off, len);
+      } else {
+        throw new IOException();
+      }
+    }
+
+    @Override
+    public void flush() throws IOException {
+      if (entry != null) {
+        writer.flush();
+      } else {
+        throw new IOException();
+      }
     }
 
     @Override
     public void close() throws IOException {
-        writer.close();
+      if (entry != null) {
+        flush();
+        zip.closeEntry();
+        entry = null;
+      }
     }
-
-    private final ZipOutputStream zip;
-    private final Writer writer;
-
-    private final class TextWriter extends Writer {
-        private ZipEntry entry;
-
-        TextWriter(final String name) throws IOException {
-            this.entry = new ZipEntry(name);
-            zip.putNextEntry(entry);
-        }
-
-        @Override
-        public void write(char[] cbuf, int off, int len) throws IOException {
-            if (entry != null) {
-                writer.write(cbuf, off, len);
-            } else {
-                throw new IOException();
-            }
-        }
-
-        @Override
-        public void flush() throws IOException {
-            if (entry != null) {
-                writer.flush();
-            } else {
-                throw new IOException();
-            }
-        }
-
-        @Override
-        public void close() throws IOException {
-            if (entry != null) {
-                flush();
-                zip.closeEntry();
-                entry = null;
-            }
-        }
-    }
+  }
 }
