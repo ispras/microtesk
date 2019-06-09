cae7db44c66c7c2f74e31dbcbdd010ea25dcfd50
Artem Kotsynyak <kotsynyak@ispras.ru>
2019-06-09 23:40:04 +0300

    [mir] Frame: set(), getValues() utils
    Signed-off-by: Artem Kotsynyak <kotsynyak@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/translator/mir/Frame.java
+++ src/main/java/core/ru/ispras/microtesk/translator/mir/Frame.java
@@ -1,5 +1,6 @@
 package ru.ispras.microtesk.translator.mir;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -7,15 +8,42 @@ public final class Frame {
   public final Map<String, List<Operand>> globals;
   public final List<Operand> locals = new java.util.ArrayList<>();
 
+  public Frame() {
+    this.globals = new java.util.HashMap<>();
+  }
+
   public Frame(final Map<String, List<Operand>> globals) {
     this.globals = globals;
   }
 
   public Operand get(final String name, final int version) {
-    final List<Operand> variants = globals.get(name);
-    if (variants != null && variants.size() >= version) {
+    final List<Operand> variants = getValues(name);
+    if (variants.size() >= version) {
       return variants.get(version - 1);
     }
     return VoidTy.VALUE;
+  }
+
+  List<Operand> getValues(final String name) {
+    final List<Operand> values = globals.get(name);
+    if (values != null) {
+      return values;
+    }
+    return Collections.emptyList();
+  }
+
+  void set(final String name, final int version, final Operand value) {
+    final List<Operand> values;
+    if (globals.containsKey(name)) {
+      values = globals.get(name);
+    } else {
+      values = new java.util.ArrayList<>();
+      globals.put(name, values);
+    }
+    final int ndiff = version - values.size();
+    if (ndiff > 0) {
+      values.addAll(Collections.nCopies(ndiff, VoidTy.VALUE));
+    }
+    values.set(version - 1, value);
   }
 }
