05e19cce66e376d85d03850bc0dc0e816773480f
Artem Kotsynyak <kotsynyak@ispras.ru>
2019-06-09 23:42:46 +0300

    [mir,pass] StoreAnalysis: track memory assignments
    Signed-off-by: Artem Kotsynyak <kotsynyak@ispras.ru>

---
+++ src/main/java/core/ru/ispras/microtesk/translator/mir/StoreAnalysis.java
@@ -0,0 +1,85 @@
+package ru.ispras.microtesk.translator.mir;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static ru.ispras.microtesk.translator.mir.GlobalNumbering.*;
+
+public class StoreAnalysis extends Pass {
+  private Frame analysisFrame;
+
+  @Override
+  public MirContext apply(final MirContext ctx) {
+    final StoreVisitor visitor = new StoreVisitor();
+    for (final BasicBlock bb : ctx.blocks) {
+      for (final Instruction insn : bb.insns) {
+        insn.accept(visitor);
+      }
+    }
+    this.analysisFrame = visitor.frame;
+
+    return ctx;
+  }
+
+  public Collection<Operand> getOutputValues(final String name) {
+    final Operand value = lastValueOf(name);
+    if (value instanceof Ite) {
+      final Ite ite = (Ite) value;
+      return Arrays.asList(ite.taken, ite.other);
+    }
+    return Collections.singleton(value);
+  }
+
+  private Operand lastValueOf(final String name) {
+    final int version = analysisFrame.getValues(name).size();
+    return analysisFrame.get(name, version);
+  }
+
+  public Operand getCondition(final String name) {
+    final Operand value = lastValueOf(name);
+    if (value instanceof Ite) {
+      return ((Ite) value).guard;
+    }
+    return new Constant(1, 1);
+  }
+
+  private static final class StoreVisitor extends InsnVisitor {
+    private final Frame frame = new Frame();
+    
+    @Override
+    public void visit(final Store insn) {
+      final Static mem = (Static) insn.target;
+      if (!Types.isArray(mem) && mem.version > 0) {
+        store(mem, insn.source);
+      }
+    }
+
+    @Override
+    public void visit(final SsaStore insn) {
+      if (!Types.isArray(insn.target)) {
+        store(insn.target, insn.origin.source);
+      }
+    }
+
+    @Override
+    public void visit(final Phi insn) {
+      if (!Types.isArray(insn.target) && insn.values != null) {
+        store(insn.target, insn.value);
+      }
+    }
+
+    private void store(final Static mem, final Operand val) {
+      frame.set(mem.name, mem.version, getValueRec(val));
+    }
+
+    private Operand getValueRec(final Operand opnd) {
+      if (opnd instanceof Static) {
+        final Static mem = (Static) opnd;
+        final Operand value = frame.get(mem.name, mem.version);
+        return (value.equals(VoidTy.VALUE)) ? opnd : value;
+      }
+      return opnd;
+    }
+  }
+}
