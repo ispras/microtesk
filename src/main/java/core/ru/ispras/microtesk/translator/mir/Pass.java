60ac719c6971861fa11e3710469fe5a3105d6f74
Artem Kotsynyak <kotsynyak@ispras.ru>
2019-11-08 18:51:27 +0300

    [mir] Pass: inlineContext() util
    Signed-off-by: Artem Kotsynyak <kotsynyak@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/translator/mir/Pass.java
+++ src/main/java/core/ru/ispras/microtesk/translator/mir/Pass.java
@@ -29,30 +29,34 @@ public abstract class Pass {
   }
 
   static MirContext copyOf(final MirContext src, final String name) {
-    final FuncTy signature = src.getSignature();
+    return inlineContext(new MirContext(name, src.getSignature()), src);
+  }
 
-    final MirContext ctx = new MirContext(name, signature);
-    ctx.locals.addAll(tailList(src.locals, signature.params.size() + 1));
+  public static MirContext inlineContext(final MirContext dst, final MirContext src) {
+    dst.locals.addAll(tailList(src.locals, src.getSignature().params.size() + 1));
 
+    final List<BasicBlock> body = new java.util.ArrayList<>();
     for (final BasicBlock bb : src.blocks) {
-      ctx.blocks.add(BasicBlock.copyOf(bb));
+      body.add(BasicBlock.copyOf(bb));
     }
-    for (final BasicBlock bb : ctx.blocks) {
+    dst.blocks.addAll(body);
+
+    for (final BasicBlock bb : body) {
       final int index = bb.insns.size() - 1;
       final Instruction insn = bb.insns.get(index);
       if (insn instanceof Branch) {
         final Branch br = (Branch) insn;
         if (br.successors.size() == 1) {
-          bb.insns.set(index, new Branch(retargetBlock(br.successors.get(0), src, ctx)));
+          bb.insns.set(index, new Branch(retargetBlock(br.successors.get(0), src, dst)));
         } else {
-          final BasicBlock taken = retargetBlock(br.successors.get(0), src, ctx);
-          final BasicBlock other = retargetBlock(br.successors.get(1), src, ctx);
+          final BasicBlock taken = retargetBlock(br.successors.get(0), src, dst);
+          final BasicBlock other = retargetBlock(br.successors.get(1), src, dst);
 
           bb.insns.set(index, new Branch(br.guard, taken, other));
         }
       }
     }
-    return ctx;
+    return dst;
   }
 
   private static BasicBlock retargetBlock(
