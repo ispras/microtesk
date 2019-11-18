fe46cb341008dae03cfa98f27ab5b494c4dda9d4
Protsenko <protsenko@ispras.ru>
2019-11-18 17:30:42 +0300

    GeneratorSettings was updated
    Signed-off-by: Protsenko <protsenko@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/settings/GeneratorSettings.java
+++ src/main/java/core/ru/ispras/microtesk/settings/GeneratorSettings.java
@@ -34,7 +34,7 @@ public class GeneratorSettings extends AbstractSettings {
 
   public static void set(final GeneratorSettings settings) {
     InvariantChecks.checkNotNull(settings);
-    InvariantChecks.checkTrue(null == instance, "GeneratorSettings are already initialized.");
+    //InvariantChecks.checkTrue(null == instance, "GeneratorSettings are already initialized.");
     if (null != instance)
       Logger.warning("GeneratorSettings are already initialized.", "");
     instance = settings;
