8d6e3d277265aa265e759dd0b1df635e35e83dbd
Alexander Kamkin <kamkin@ispras.ru>
2019-12-03 17:22:11 +0300

    Replaced: new_data_manager -> DataManager.new(self).
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- src/main/ruby/template.rb
+++ src/main/ruby/template.rb
@@ -807,15 +807,11 @@ class Template
     # Default value is 8 bits if other value is not explicitly specified
     addressableSize = attrs.has_key?(:item_size) ? attrs[:item_size] : 8
 
-    @data_manager = new_data_manager
+    @data_manager = DataManager.new(self)
     @data_manager.beginConfig target, addressableSize
 
     @data_manager.instance_eval &contents
     @data_manager.endConfig
-  end
-
-  def new_data_manager
-    DataManager.new(self)
   end
 
   def data(attrs = {}, &contents)
