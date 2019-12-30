fa8e2821988c6f4769df3e35cd14644a86ffef9c
Artem Kotsynyak <kotsynyak@ispras.ru>
2019-12-30 23:29:21 +0300

    [util] more Lists utilities
    Signed-off-by: Artem Kotsynyak <kotsynyak@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/translator/mir/Lists.java
+++ src/main/java/core/ru/ispras/microtesk/translator/mir/Lists.java
@@ -1,5 +1,6 @@
 package ru.ispras.microtesk.translator.mir;
 
+import java.util.Arrays;
 import java.util.List;
 
 final class Lists {
@@ -12,11 +13,28 @@ final class Lists {
   }
 
   public static <T> List<T> removeLastN(final List<? extends T> list, final int n) {
-    final List<? extends T> tail = list.subList(list.size() - n, list.size());
+    final List<? extends T> tail = tailOf(list, list.size() - n);
     final List<T> items = new java.util.ArrayList<T>(tail);
     tail.clear();
 
     return items;
+  }
+
+  public static <T> List<? extends T> tailOf(
+      final List<? extends T> list, final int index) {
+    return list.subList(index, list.size());
+  }
+
+  @SafeVarargs
+  public static <T> List<T> newList(final T... items) {
+    return new java.util.ArrayList<T>(Arrays.asList(items));
+  }
+
+  public static <T> void moveAll(
+      final List<? super T> dst, final List<? extends T> src) {
+    dst.clear();
+    dst.addAll(src);
+    src.clear();
   }
 
   private Lists() { }
