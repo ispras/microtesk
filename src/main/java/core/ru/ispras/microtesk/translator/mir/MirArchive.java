7c8492d7c17ddc78f4fbfdcc1027a6223f94ae4f
Artem Kotsynyak <kotsynyak@ispras.ru>
2020-03-04 02:24:55 +0300

    [mir,io] MirArchive: load content on-demand
    Signed-off-by: Artem Kotsynyak <kotsynyak@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/translator/mir/MirArchive.java
+++ src/main/java/core/ru/ispras/microtesk/translator/mir/MirArchive.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
+import java.util.AbstractMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
@@ -17,23 +19,19 @@ import javax.json.JsonReader;
 public class MirArchive {
   static final String MANIFEST = "manifest.json";
 
-  private final Map<String, MirContext> mir = new java.util.HashMap<>();
+  private Map<String, MirContext> mir;
   private JsonObject manifest;
 
   public static MirArchive open(final Path path) {
     final MirArchive arch = new MirArchive();
-    try (final ZipFile zip = new ZipFile(path.toFile())) {
+    try {
+      // TODO enforce ZIP resource deallocation (close() it)
+      final ZipFile zip = new ZipFile(path.toFile());
       final ZipEntry manifestEntry = zip.getEntry(MANIFEST);
       try (final JsonReader reader = Json.createReader(zip.getInputStream(manifestEntry))) {
         arch.manifest = reader.readObject();
       }
-      for (final ZipEntry entry : Collections.list(zip.entries())) {
-        if (!entry.getName().equals(MANIFEST)) {
-          final MirParser parser = new MirParser(zip.getInputStream(entry));
-          final MirContext ctx = parser.parse();
-          arch.mir.put(ctx.name, ctx);
-        }
-      }
+      arch.mir = new MirLibrary(zip);
     } catch (final IOException e) {
       Logger.warning("MirArchive: failed to load '%s': %s", path.toString(), e.toString());
     }
@@ -49,6 +47,72 @@ public class MirArchive {
   }
 
   public Map<String, MirContext> loadAll() {
-    return Collections.unmodifiableMap(mir);
+    // return Collections.unmodifiableMap(mir);
+    return mir;
+  }
+}
+
+final class MirLibrary extends AbstractMap<String, MirContext> {
+  private static final String SUFFIX = ".mir";
+
+  private final ZipFile zip;
+  private final Set<Map.Entry<String, MirContext>> entrySet;
+
+  MirLibrary(final ZipFile zip) {
+    this.zip = zip;
+
+    final var entries = new java.util.HashSet<Map.Entry<String, MirContext>>();
+    for (final ZipEntry entry : Collections.list(zip.entries())) {
+      final String name = entry.getName();
+      if (name.endsWith(SUFFIX)) {
+        entries.add(new Entry(name.substring(0, name.length() - SUFFIX.length())));
+      }
+    }
+    this.entrySet = Collections.unmodifiableSet(entries);
+  }
+
+  @Override
+  public Set<Map.Entry<String, MirContext>> entrySet() {
+    return entrySet;
+  }
+
+  private final class Entry implements Map.Entry<String, MirContext> {
+    private final String key;
+
+    Entry(final String key) {
+      this.key = key;
+    }
+
+    @Override
+    public String getKey() {
+      return key;
+    }
+
+    @Override
+    public MirContext getValue() {
+      final var entry = zip.getEntry(key + SUFFIX);
+      try {
+        final var parser = new MirParser(zip.getInputStream(entry));
+        return parser.parse();
+      } catch (final IOException e) {
+        throw new IllegalStateException(
+          String.format("Unable to load MIR '%s'", entry.getName()), e);
+      }
+    }
+
+    @Override
+    public MirContext setValue(final MirContext value) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean equals(final Object that) {
+      return this == that;
+    }
+
+    @Override
+    public int hashCode() {
+      return key.hashCode();
+    }
   }
 }
