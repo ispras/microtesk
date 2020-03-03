a14080a727e526f43e9fa98fc3de4f91a913917e
Artem Kotsynyak <kotsynyak@ispras.ru>
2020-03-03 15:43:31 +0300

    [symexec,mir] restore MIR control flow for binary
    Signed-off-by: Artem Kotsynyak <kotsynyak@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/tools/symexec/SymbolicExecutor.java
+++ src/main/java/core/ru/ispras/microtesk/tools/symexec/SymbolicExecutor.java
@@ -134,42 +134,62 @@ public final class SymbolicExecutor {
   private static MirContext composeMir(final String name, final BodyInfo info) {
     final MirContext mir = new MirContext(name, MirBuilder.VOID_TO_VOID_TYPE);
 
+    final int nblocks = info.bbMir.size();
+    final List<MirBlock> intros = new java.util.ArrayList<>(nblocks);
+    final List<MirBlock> outros = new java.util.ArrayList<>(nblocks);
+
     int bbIndex = 0;
-    MirBlock link = mir.newBlock();
     for (final MirContext body : info.bbMir) {
-      final Map<String, Pair<Static, Static>> inoutMap = info.bbInOut.get(bbIndex);
-
-      final MirBlock inbb = link;
-      for (final Pair<Static, Static> inout : inoutMap.values()) {
-        inbb.assign(inout.first, inout.first.newVersion(0));
-      }
-
-      final int start = mir.blocks.size();
-      final int end = start + body.blocks.size();
-      Pass.inlineContext(mir, body);
-
-      final MirBlock outbb = mir.newBlock();
-      for (final Pair<Static, Static> inout : inoutMap.values()) {
-        outbb.assign(inout.second.newVersion(0), inout.second);
-      }
-
-      final BasicBlock entry = mir.blocks.get(start);
-      inbb.jump(entry);
-      for (final BasicBlock bb : mir.blocks.subList(start, end)) {
-        final int index = bb.insns.size() - 1;
-        final Instruction insn = bb.insns.get(index);
-
-        if (insn instanceof Return) {
-          bb.insns.set(index, new Branch(outbb.bb));
-        }
-      }
-
-      link = outbb;
-      ++bbIndex;
+      final var inoutMap = info.bbInOut.get(bbIndex);
+      final var linkPair = wrapInline(body, mir, inoutMap.values());
+      intros.add(linkPair.first);
+      outros.add(linkPair.second);
     }
-    link.append(new Return(null));
+    final var ranges = info.bbRange;
+    for (int i = 0; i < nblocks; ++i) {
+      final var range = ranges.get(i);
+      final int taken = getLinkIndex(range, range.nextTaken, ranges);
+      final int other = getLinkIndex(range, range.nextOther, ranges);
 
+      final var outro = outros.get(i);
+      if (taken >= 0 && taken == other) {
+        outro.jump(intros.get(taken).bb);
+      } else if (taken >= 0) {
+        final Operand guard = outro.getLocal(evalGuardId(i, info));
+        outro.append(new Branch(guard, intros.get(taken).bb, intros.get(other).bb));
+      } else {
+        outro.append(new Return(null));
+      }
+    }
     return mir;
+  }
+
+  private static Pair<MirBlock, MirBlock> wrapInline(
+      final MirContext callee,
+      final MirContext mir,
+      final Collection<Pair<Static, Static>> inout) {
+    final MirBlock inbb = mir.newBlock();
+    final int start = mir.blocks.size();
+    final int end = start + callee.blocks.size();
+    Pass.inlineContext(mir, callee);
+    final MirBlock outbb = mir.newBlock();
+
+    for (final var pair : inout) {
+      inbb.assign(pair.first, pair.first.newVersion(0));
+      outbb.assign(pair.second.newVersion(0), pair.second);
+    }
+
+    final BasicBlock entry = mir.blocks.get(start);
+    inbb.jump(entry);
+    for (final BasicBlock bb : mir.blocks.subList(start, end)) {
+      final int index = bb.insns.size() - 1;
+      final Instruction insn = bb.insns.get(index);
+
+      if (insn instanceof Return) {
+        bb.insns.set(index, new Branch(outbb.bb));
+      }
+    }
+    return new Pair<>(inbb, outbb);
   }
 
   private static BodyInfo writeMir(final Model model, final List<IsaPrimitive> insnList) {
@@ -342,15 +362,20 @@ public final class SymbolicExecutor {
     if (range.nextTaken == range.nextOther) {
       cond = "true";
     } else {
-      int version =
-        Integer.valueOf(info.bbCond.get(index).toString().substring(1));
-      for (int i = 0; i < index; ++i) {
-        version += info.bbMir.get(i).locals.size() - 1;
-      }
-      cond = String.format("%%%d", version);
+      cond = String.format("%%%d", evalGuardId(index, info));
     }
     parent.add("condition_smt", cond);
     parent.add("hwstate_mod_smt", newHwstateMapping(index, info, factory));
+  }
+
+  private static int evalGuardId(final int index, final BodyInfo info) {
+    final Operand guard = info.bbCond.get(index);
+    // TODO enfoce guards to be Local instance
+    int version = Integer.valueOf(guard.toString().substring(1));
+    for (int i = 0; i < index; ++i) {
+      version += info.bbMir.get(i).locals.size() - 1;
+    }
+    return version;
   }
 
   private static JsonArrayBuilder newHwstateMapping(
