658032844b1aa4d1436797f3c150baf045b50754
Alexander Kamkin <kamkin@ispras.ru>
2020-03-04 12:02:33 +0300

    Changed NASM to GNU in x86 demo's run.sh.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- src/main/arch/demo/x86/templates/run.sh
+++ src/main/arch/demo/x86/templates/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sh $MICROTESK_HOME/bin/generate.sh x86nasm $1.rb --code-file-prefix $1 --code-file-extension s -v -sd --ri I80386 1>$1.stdout 2>$1.stderr
+sh $MICROTESK_HOME/bin/generate.sh x86gnu $1.rb --code-file-prefix $1 --code-file-extension s -v -sd --ri I80386_GNU 1>$1.stdout 2>$1.stderr
