9115203b38a0ebec96b69c200cd531faa5d02879
Alexander Kamkin <kamkin@ispras.ru>
2020-03-24 11:29:52 +0300

    Improved CacheUnitTestCase.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- src/test/java/plugins/mmu/ru/ispras/microtesk/mmu/model/sim/CacheUnitTestCase.java
+++ src/test/java/plugins/mmu/ru/ispras/microtesk/mmu/model/sim/CacheUnitTestCase.java
@@ -16,9 +16,11 @@ package ru.ispras.microtesk.mmu.model.sim;
 
 import org.junit.Assert;
 import org.junit.Test;
+import ru.ispras.fortress.randomizer.Randomizer;
 import ru.ispras.microtesk.mmu.model.sim.model.Model;
 
 public final class CacheUnitTestCase {
+
   public static Model model = new Model(
       CachePolicy.create(
           EvictionPolicyId.FIFO,
@@ -28,17 +30,55 @@ public final class CacheUnitTestCase {
       )
   );
 
+  private int nextCore() {
+    return Randomizer.get().nextIntRange(0, Model.N1 - 1);
+  }
+
+  private int nextAddress(final int start, final int end) {
+    final int l1tag = Randomizer.get().nextIntRange(start >>> 12, end >>> 12);
+    final int l1idx = Randomizer.get().nextIntRange(0, 2);
+    final int l1pos = Randomizer.get().nextIntRange(0, 7);
+
+    return (l1tag << 12) | (l1idx << 5) | (l1pos << 2);
+  }
+
+  private int nextWord() {
+    return Randomizer.get().nextInt();
+  }
+
   private void test(final Model model, final int start, final int end) {
     // Initialize the main memory.
-    model.memset(start, end, 0);
+    model.memset(start, end, 0xdeadbeef);
 
-    final int word = 0x12345678;
-    model.sw(0, 0, word);
-    Assert.assertTrue(model.lw(0, 0) == word);
+    final int numberOfTests = 100;
+    final int numberOfLoadsPerStore = 10;
+
+    for (int i = 0; i < numberOfTests; i++) {
+      final int storeCore = nextCore();
+      final int storeAddress = nextAddress(start, end);
+      final int storeWord = nextWord();
+
+      model.sw(storeCore, storeAddress, storeWord);
+
+      for (int j = 0; j < numberOfLoadsPerStore; j++) {
+        final int loadCore = nextCore();
+        final int loadAddress = nextAddress(start, end);
+
+        final int loadReceived = model.lw(loadCore, loadAddress);
+        final int loadExpected = model.lookup(loadAddress);
+
+        Assert.assertTrue(
+            String.format("lw core=%d, address=%x: %x != %x",
+                loadCore, loadAddress, loadReceived, loadExpected
+            ),
+            loadReceived == loadExpected
+        );
+      }
+    }
   }
 
   @Test
   public void test() {
-    test(model, 0x0000, 0x8000);
+    test(model, 0x0000, 0xffff);
   }
 }
--- src/test/java/plugins/mmu/ru/ispras/microtesk/mmu/model/sim/model/Model.java
+++ src/test/java/plugins/mmu/ru/ispras/microtesk/mmu/model/sim/model/Model.java
@@ -30,13 +30,11 @@ public class Model {
 
     @Override
     public BitVector load(final BitVector address) {
-      System.out.format("load: %s %s\n", address.toHexString(), storage.get(address).toHexString());
       return storage.get(address);
     }
 
     @Override
     public void store(final BitVector address, final BitVector data) {
-      System.out.format("store: %s %s\n", address.toHexString(), data.toHexString());
       storage.put(address, data);
     }
 
@@ -51,6 +49,8 @@ public class Model {
       return storage.containsKey(address);
     }
   };
+
+  private final Map<Integer, Integer> reference = new HashMap<>();
 
   public Model(final CachePolicy policy) {
     m.setStorage(device);
@@ -81,11 +81,11 @@ public class Model {
   }
 
   private PA getAddress(final int address) {
-    return pa.newStruct(BitVector.valueOf(address, pa.getBitSize()));
+    return pa.newStruct(BitVector.valueOf(address, 32));
   }
 
   private int getLowerBit(final int address) {
-    return (address >> 2) & 7;
+    return (address & 0x1c) << 3;
   }
 
   private int getUpperBit(final int address) {
@@ -93,19 +93,34 @@ public class Model {
   }
 
   public void memset(final int start, final int end, final int fill) {
-    for (int address = (start & ~0x1f); address <= end; address += 32) {
+    for (int index = start >>> 5; index <= end >>> 5; index++) {
       final BitVector line = BitVector.valueOf(fill, 32).repeat(8);
-      storage.put(BitVector.valueOf(address, 32), line);
+      storage.put(BitVector.valueOf(index, 32), line);
+
+      for (int offset = 0; offset < 8; offset++) {
+        final int address = (index << 5) | (offset << 2);
+        reference.put(address, fill);
+      }
     }
   }
 
   public int lw(final int core, final int address) {
+    System.out.format("lw core=%d address=0x%08x\n", core, address);
+
     final L1.Entry entry = l1[core].readEntry(getAddress(address));
     return entry.asBitVector().field(getLowerBit(address), getUpperBit(address)).intValue();
   }
 
   public void sw(final int core, final int address, final int word) {
+    System.out.format("sw core=%d address=0x%08x word=0x%08x\n", core, address, word);
+
     final BitVector data = BitVector.valueOf(word, 32);
     l1[core].writeEntry(getAddress(address), getLowerBit(address), getUpperBit(address), data);
+
+    reference.put(address, word);
+  }
+
+  public int lookup(final int address) {
+    return reference.get(address);
   }
 }
