a7633deaab0358b391be9e046636a215806a06c8
Artem Kotsynyak <kotsynyak@ispras.ru>
2020-10-29 19:02:02 +0300

    [mir,style] InsnVisitor: unify method structure
    Signed-off-by: Artem Kotsynyak <kotsynyak@ispras.ru>

--- src/main/java/core/ru/ispras/microtesk/translator/mir/InsnVisitor.java
+++ src/main/java/core/ru/ispras/microtesk/translator/mir/InsnVisitor.java
@@ -19,47 +19,35 @@ package ru.ispras.microtesk.translator.mir;
 import static ru.ispras.microtesk.translator.mir.Instruction.*;
 
 public class InsnVisitor {
-  public void visit(final Assignment insn) {
-  }
+  public void visit(final Assignment insn) { }
 
-  public void visit(final Concat insn) {
-  }
+  public void visit(final Concat insn) { }
 
-  public void visit(final Extract insn) {
-  }
+  public void visit(final Extract insn) { }
 
-  public void visit(final Sext insn) {
-  }
+  public void visit(final Sext insn) { }
 
-  public void visit(final Zext insn) {
-  }
+  public void visit(final Zext insn) { }
 
-  public void visit(final Branch insn) {
-  }
+  public void visit(final Branch insn) { }
 
-  public void visit(final Return insn) {
-  }
+  public void visit(final Return insn) { }
 
-  public void visit(final Instruction.Exception insn) {
-  }
+  public void visit(final Instruction.Exception insn) { }
 
-  public void visit(final Call insn) {
-  }
+  public void visit(final Call insn) { }
 
-  public void visit(final Invoke insn) {
-  }
+  public void visit(final Invoke insn) { }
 
-  public void visit(final Load insn) {
-  }
+  public void visit(final Load insn) { }
 
-  public void visit(final Store insn) {
-  }
+  public void visit(final Store insn) { }
 
-  public void visit(final Disclose insn) {
-  }
+  public void visit(final Disclose insn) { }
 
   public void visit(final Conditional insn) { }
 
   public void visit(final GlobalNumbering.Phi insn) { }
+
   public void visit(final GlobalNumbering.SsaStore insn) { }
 }
